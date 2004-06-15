fd964ae2b2f57a80591e51e8fd30dc24135ceab0
heavy <heavy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-06-15 16:13:47 +0000

    Don't allow CMS to start if install.php exists, CMS deletes it for you with confirmation
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@22 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/index.php
+++ cms/trunk/index.php
@@ -1,4 +1,22 @@
 <?php
+if (!file_exists("config.php")) {
+    require_once("lib/misc.functions.php");
+    redirect("install.php");
+    exit;
+} ## if
+if ($_GET["deleteinstall"] == "true") {
+    @unlink("install.php");
+} ## if
+
+if (file_exists("config.php") && file_exists("install.php")) {
+    echo "You cannot start CMS until you remove the install.php<br>\n";
+    if ($_GET["deleteinstall"] == "true") {
+        echo "Looks like you tried to have CMS delete the install file but that was not sucessful.  You will have to remove it manually before you can continue<br>\n";
+        exit;
+    } ## if
+    echo "Click <a href=\"index.php?deleteinstall=true\">here</a> to have CMS try to delete it for you.  If successful you will see the CMS main page<br>\n";
+    exit;
+} ## if
 
 require_once("include.php");
 
