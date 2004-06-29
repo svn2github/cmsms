b8757bdbcdcf865199d2513a777925759c224939
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-06-29 02:05:18 +0000

    -- Release 0.1rc1 --
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@56 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ cms/trunk/release-cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo ------------------------------------
+echo Running Cleanup Script
+echo ------------------------------------
+
+rm -fr CHECKLIST
+rm -fr config.php
+rm -fr config.php.sample
+rm -fr autogen.sh
+rm -fr mpd.sql
+rm -fr mysql.sql
+rm -fr images/*.svg
+rm -fr smarty/cms/cache/*
+rm -fr smarty/cms/templates_c/*
+find -depth -type d -name .svn -exec rm -fr {} \;
+
+echo ------------------------------------
+echo Done!
+echo ------------------------------------
