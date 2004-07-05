0e2bb29df146bda5fd14d1eeaacaac94e0121c74
heavy <heavy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-07-05 11:26:53 +0000

    Removed nested <div> tags in footer
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@100 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/smarty/cms/plugins/function.footer.php
+++ cms/trunk/smarty/cms/plugins/function.footer.php
@@ -2,11 +2,9 @@
 
 function smarty_function_footer() {
     global $CMS_VERSION;
-    echo '<div class="thebody">'."\n";
     echo '<div class="bottom">'."\n";
     echo 'Site powered by <a href="http://cmsmadesimple.sf.net">CMS Made Simple</a> Version: '."$CMS_VERSION<br />\n";
     echo 'Think you\'ve found a bug? <a href="http://cmsmadesimple.sf.net/mantis">Submit</a> it'."\n";
-    echo "</div>\n";
     echo "</div>\n";
 } ## smarty_function_show_footer
 
