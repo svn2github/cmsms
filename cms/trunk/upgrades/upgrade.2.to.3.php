fefc6df60b32ea2ef0c4e7534654df2635582459
heavy <heavy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-07-08 16:01:37 +0000

    Upgrade script from ver 2 to 3
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@129 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/upgrades/upgrade.2.to.3.php
+++ cms/trunk/upgrades/upgrade.2.to.3.php
@@ -1,3 +1,44 @@
 <?php
 
+$current_schema_ver = 2;
+$new_schema_ver = 3;
+$doupgrade = false;
+
+$db = new DB($config);
+
+$query = "SELECT version from ".$config->db_prefix."version";
+$result = $db->query($query);
+while($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+	if ($row["version"] == $current_schema_ver) {
+		$doupgrade = true;
+	}
+}
+mysql_free_result($result);
+
+if ($doupgrade) {
+
+	echo "<p>Creating plugins table...";
+
+	$query  = "CREATE TABLE ".$config->db_prefix."plugins (";
+	$query .= "  plugin_id int(11) NOT NULL auto_increment,";
+	$query .= "  plugin_name varchar(50) default NULL,";
+	$query .= "  plugin_path varchar(250) default NULL,";
+	$query .= "  PRIMARY KEY (plugin_id)";
+	$query .= ") TYPE=MyISAM";
+
+	$result = $db->query($query);
+
+	echo "[done]</p>";
+
+	echo "<p>Updating schema version... ";
+
+	$query = "UPDATE ".$config->db_prefix."version SET version = ".$new_schema_ver;
+	$db->query($query);
+
+	echo "[done]</p>";
+}
+
+$db->close();
+
+# vim:ts=4 sw=4 noet
 ?>
