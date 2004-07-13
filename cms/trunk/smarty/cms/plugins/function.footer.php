3d8fa2867e85c7ed1ab78b2d34c0bac7dabb78e3
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-07-13 16:59:01 +0000

    Footer now points to trac
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@159 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/smarty/cms/plugins/function.footer.php
+++ cms/trunk/smarty/cms/plugins/function.footer.php
@@ -4,7 +4,7 @@ function smarty_function_footer() {
     global $CMS_VERSION;
     echo '<div class="bottom">'."\n";
     echo 'Site powered by <a href="http://cmsmadesimple.sf.net">CMS Made Simple</a> Version: '."$CMS_VERSION<br />\n";
-    echo 'Think you\'ve found a bug? <a href="http://cmsmadesimple.sf.net/mantis">Submit</a> it'."\n";
+    echo 'Think you\'ve found a bug? <a href="http://trac.wishy.org/cms">Submit</a> it'."\n";
     echo "</div>\n";
 } ## smarty_function_show_footer
 
