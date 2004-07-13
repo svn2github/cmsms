cf2a955e3af48196077323af64360219797bb23e
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-07-13 16:43:13 +0000

    -- Release 0.2.1rc2 --
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@157 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ cms/tags/version-0.2.1rc2/admin/deletesection.php
@@ -0,0 +1,62 @@
+<?php
+#CMS - CMS Made Simple
+#(c)2004 by Ted Kulp (wishy@users.sf.net)
+#This project's homepage is: http://cmsmadesimple.sf.net
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+require_once("../include.php");
+
+check_login($config);
+
+$dodelete = true;
+
+$section_id = -1;
+if (isset($_GET["section_id"])) {
+
+	$section_id = $_GET["section_id"];
+	$userid = get_userid();
+	$access = check_permission($config, $userid, 'Remove Section');
+
+	if ($access) {
+		$db = new DB($config);
+
+		$query = "SELECT count(*) AS count FROM ".$config->db_prefix."pages WHERE section_id = $section_id";
+		$result = $db->query($query);
+		$row = mysql_fetch_array($result, MYSQL_ASSOC);
+		if (isset($row["count"]) && $row["count"] > 0) {
+			$dodelete = false;
+		}
+		mysql_free_result($result);
+
+		if ($dodelete) {
+			$query = "DELETE FROM ".$config->db_prefix."sections where section_id = $section_id";
+			$result = $db->query($query);
+			#This is so pages will not cache the menu changes
+			$query = "UPDATE ".$config->db_prefix."templates SET modified_date = now()";
+			$db->query($query);
+		}
+		$db->close();
+	}
+}
+
+if ($dodelete == true) {
+	redirect("listsections.php");
+}
+else {
+	redirect("listsections.php?message=".GetText::gettext("Section still being used by content pages.  Please remove those first."));
+}
+
+# vim:ts=4 sw=4 noet
+?>
---
+++ cms/tags/version-0.2.1rc2/admin/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<title><?=GetText::gettext("CMS Admin System")?></title>
+
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+
+<body>
+
+<img src="../images/cmsadminbanner.png" border="0" id="logo" alt="CMS Made Simple"/>
+<div id="header" class="header">
+
+</div>
+
+<?php
+include_once("menu.php");
+?>
+
+<div class="content">
