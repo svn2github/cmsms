8b8b807ff133d19021252cb4cfbd5d1b02ebdcb2
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-07-14 02:59:07 +0000

    Added announcement in forums to CHECKLIST Updated version to 0.3-svn Updates schema version to 3
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@164 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/version.php
+++ cms/trunk/version.php
@@ -16,8 +16,8 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-$CMS_VERSION = "0.2.1";
-$CMS_SCHEMA_VERSION = "2";
+$CMS_VERSION = "0.3-svn";
+$CMS_SCHEMA_VERSION = "3";
 
 # vim:ts=4 sw=4 noet
 ?>
