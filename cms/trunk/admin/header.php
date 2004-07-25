e3540e1ffe29fdcfab0636c56f11a5a01516eb19
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-07-25 18:58:36 +0000

    Force admin pages to never cache on the browser
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@235 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/admin/header.php
+++ cms/trunk/admin/header.php
@@ -1,3 +1,17 @@
+<?
+// Date in the past
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+
+// always modified
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+ 
+// HTTP/1.1
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+
+// HTTP/1.0
+header("Pragma: no-cache");
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
