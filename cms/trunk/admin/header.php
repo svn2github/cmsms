6ffc23057d53ae0f35d37154f19d423cc103cf60
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-07-26 16:48:51 +0000

    Hopefully fixed all the encoding issues
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@237 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/admin/header.php
+++ cms/trunk/admin/header.php
@@ -1,4 +1,7 @@
 <?
+
+@ob_start();
+
 // Date in the past
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 
@@ -11,16 +14,21 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 
 // HTTP/1.0
 header("Pragma: no-cache");
+
+header("Content-type: text/html; charset=UTF-8");
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
 
 <title><?=$gettext->gettext("CMS Admin System")?></title>
 
 <link rel="stylesheet" type="text/css" href="style.css" />
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
 <?php 
 
