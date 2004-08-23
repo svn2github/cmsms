31e0e781ed1ecbe32768b7d8137a75a80989a74c
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-08-23 00:35:41 +0000

    -- Release 0.5 --
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@424 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ cms/tags/version-0.5/modules/RSS/modulefunctions.php
@@ -0,0 +1,95 @@
+<?php
+#CMS - CMS Made Simple
+#(c)2004 by Ted Kulp (wishy@users.sf.net)
+#This project's homepage is: http://cmsmadesimple.sf.net
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+function rss_module_install($cms)
+{
+	//Doesn't do anything
+}
+
+function rss_module_uninstall($cms)
+{
+	//Doesn't do anything
+}
+
+function rss_module_execute($cms, $id, $params)
+{
+	//This is the entryway into the module.  All requests from CMS will come through here.
+
+	$config = $cms->config;
+
+	define('MAGPIE_CACHE_DIR', $config["root_path"].'/smarty/cms/cache');
+
+	require_once dirname(__FILE__).('/rss_fetch.inc');
+
+	if (isset($params['url']))
+	{
+		$rss = fetch_rss($params['url']);
+		$items = array();
+		if (isset($params['numbertoshow']))
+		{
+			$items = array_slice($rss->items, 0, $params['numbertoshow']);
+		}
+		else
+		{
+			$items = $rss->items;
+		}
+
+		foreach ($items as $item ) {
+			$title = $item[title];
+			$url   = $item[link];
+			echo "<a href=\"$url\">$title</a><br />\n";
+		}
+	}
+}
+
+function rss_module_help($cms)
+{
+	//Text to show for the help box...
+	?>
+
+	<h3>What does this do?</h3>
+	<p>RSS is a module for displaying news feeds from other sites in your site.  It can be inserted into a template or content page as a tag and will display the title and url of each item from the feed given.</p>
+	<h3>Anything else I should know?</h3>
+	<p>The RSS module will cache feeds so that they are not downloaded and parsed on every refresh.  Instead, it pulls the feed down every so many page refreshes and stores the data locally so it's easily accessible.  When the local data becomes stale, it will pull fresh data.  You should notice no performance hit by using it in a template.</p>
+	<h3>How do I use it?</h3>
+	<p>As this is just a tag module, it's inserted into your page or template by using the cms_module tag.  Example syntax would be: <br /><code>{cms_module module="rss" url="http://download.freshmeat.net/backend/fm-releases.rdf" numbertofetch="5"}</code></p>
+	<h3>What parameters are there?</h3>
+	<p>
+	<ul>
+		<li>url="http://feed_url" - RSS feed URL</li>
+		<li><em>(optional)</em>numbertofetch="5" - Maximum number of items to display -- leaving empty will show all items</li>
+	</ul>
+	</p>
+
+	<?
+
+}
+
+function rss_module_about() {
+	?>
+	<p>Author: Ted Kulp &lt;tedkulp@users.sf.net&gt;</p>
+	<p>Version: 1.0</p>
+	<p>
+	Change History:<br/>
+	None
+	</p>
+	<?
+}
+
+# vim:ts=4 sw=4 noet
+?>
---
+++ cms/tags/version-0.5/upgrades/upgrade.3.to.4.php
@@ -0,0 +1,102 @@
+<?php
+
+echo "<p>Creating sequences...";
+
+
+function upgrade_create_sequence_table($db, $tablename, $idcol) {
+
+	$num = $db->GetOne("SELECT MAX($idcol) FROM $tablename");
+	$db->DropSequence($tablename."_seq");
+	$db->CreateSequence($tablename."_seq", $num + 1);
+	$dict = NewDataDictionary($db);
+	$sqlarray = $dict->AlterColumnSQL($tablename, "$idcol I"); 
+	$dict->ExecuteSQLArray($sqlarray);
+
+}
+
+upgrade_create_sequence_table($dbnew, $config->db_prefix."additional_users", "additional_users_id");
+upgrade_create_sequence_table($dbnew, $config->db_prefix."group_perms", "group_perm_id");
+upgrade_create_sequence_table($dbnew, $config->db_prefix."groups", "group_id");
+upgrade_create_sequence_table($dbnew, $config->db_prefix."pages", "page_id");
+upgrade_create_sequence_table($dbnew, $config->db_prefix."permissions", "permission_id");
+upgrade_create_sequence_table($dbnew, $config->db_prefix."templates", "template_id");
+upgrade_create_sequence_table($dbnew, $config->db_prefix."users", "user_id");
+
+echo "[done]</p>";
+
+echo "<p>Creating modules table...";
+
+$db = NewDataDictionary($dbnew);
+$flds = "
+	module_name C(255),
+	status C(255),
+	version C(255),
+	active L
+";
+$taboptarray = array('mysql' => 'TYPE=MyISAM');
+$sqlarray = $db->CreateTableSQL($config->db_prefix."modules", $flds, $taboptarray);
+$db->ExecuteSQLArray($sqlarray);
+
+echo "[done]</p>";
+
+echo "<p>Adding parent_id to pages table...";
+
+$db = NewDataDictionary($dbnew);
+$sqlarray = $db->AddColumnSQL($config->db_prefix."pages", "parent_id I NOTNULL DEFAULT 0");
+$db->ExecuteSQLArray($sqlarray);
+
+echo "[done]</p>";
+
+echo "<p>Removing section_id from pages table...";
+
+$db = NewDataDictionary($dbnew);
+$sqlarray = $db->DropColumnSQL($config->db_prefix."pages", "section_id");
+$db->ExecuteSQLArray($sqlarray);
+
+echo "[done]</p>";
+
+echo "<p>Removing sections table...";
+
+$db = NewDataDictionary($dbnew);
+$sqlarray = $db->DropTableSQL($config->db_prefix."sections");
+$db->ExecuteSQLArray($sqlarray);
+
+echo "[done]</p>";
+
+echo "<p>Adding module admin permission... ";
+
+$new_id = $dbnew->GenID($config->db_prefix."permissions_seq");
+$query = "INSERT INTO ".$config->db_prefix."permissions (permission_id, permission_name, permission_text, create_date, modified_date) VALUES ($new_id,'Modify Modules','Modify Modules',".$dbnew->DBTimeStamp(time()).",".$dbnew->DBTimeStamp(time()).")";
+$dbnew->Execute($query);
+
+echo "[done]</p>";
+
+echo "<p>Clearing cache and template directories... ";
+
+function clear_dir($dir){
+
+	$path = dirname(dirname(__FILE__))."/smarty/cms/".$dir."/";
+
+	$handle=opendir($path);
+	while ($file = readdir($handle)) {
+		if ($file != "." && $file != ".." && is_file($path.$file)) {
+			#echo ($path.$file);
+			unlink($path.$file);
+		}
+	}
+}
+
+clear_dir("templates_c");
+clear_dir("cache");
+
+echo "[done]</p>";
+
+echo "<p>Updating schema version... ";
+
+$query = "UPDATE ".$config->db_prefix."version SET version = 4";
+$dbnew->Execute($query);
+
+echo "[done]</p>";
+
+# vim:ts=4 sw=4 noet
+?>
