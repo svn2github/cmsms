a12fb72ccfb57f9ab71217c2ccc14b90b304c4c9
calexico <calexico@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-08-30 13:08:35 +0000

    updated help for start_element
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@509 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/plugins/function.bulletmenu.php
+++ cms/trunk/plugins/function.bulletmenu.php
@@ -92,7 +92,7 @@ function smarty_cms_help_function_bulletmenu() {
 	<h3>What parameters does it take?</h3>
 	<ul>
 		<li><em>(optional)</em> <tt>showadmin</tt> - 1/0, whether you want to show or not the admin link.</li>
-		<li><em>(optional)</em> <tt>start_element</tt> - a page ID. This parameter sets the root of the menu.</li>
+		<li><em>(optional)</em> <tt>start_element</tt> - the hierarchy of your element (ie : 1.2 or 3.5.1 for example). This parameter sets the root of the menu.</li>
 		<li><em>(optional)</em> <tt>number_of_levels</tt> - an integer, the number of levels you want to show in your menu.</li>
 	</ul>
 	</p>
