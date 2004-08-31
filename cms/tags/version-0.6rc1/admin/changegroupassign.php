accc131dac35963c0d5c3f980c2780f2a6d7ebf0
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-08-31 22:18:42 +0000

    -- Release 0.6rc1 --
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@527 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ cms/tags/version-0.6rc1/admin/changegroupassign.php
@@ -0,0 +1,137 @@
+<?php
+#CMS - CMS Made Simple
+#(c)2004 by Ted Kulp (wishy@users.sf.net)
+#This project's homepage is: http://cmsmadesimple.sf.net
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+$CMS_ADMIN_PAGE=1;
+
+require_once("../include.php");
+
+check_login();
+
+$group_id="";
+if (isset($_POST["group_id"])) $group_id = $_POST["group_id"];
+else if (isset($_GET["group_id"])) $group_id = $_GET["group_id"];
+
+$group_name="";
+
+if (isset($_POST["cancel"])) {
+	redirect("listgroups.php");
+	return;
+}
+
+$userid = get_userid();
+$access = check_permission($userid, 'Modify Group Assignments');
+
+if ($access) {
+
+	$query = "SELECT group_name FROM ".cms_db_prefix()."groups WHERE group_id = ".$group_id;
+	$result = $db->Execute($query);
+
+	if ($result && $result->RowCount() > 0) {
+		$row = $result->FetchRow();
+		$group_name = $row['group_name'];
+	}
+
+	if (isset($_POST["changeassign"])) {
+
+		$query = "DELETE FROM ".cms_db_prefix()."user_groups WHERE group_id = " . $group_id;
+		$result = $db->Execute($query);
+
+		foreach ($_POST as $key=>$value) {
+			if (strpos($key,"user-") == 0 && strpos($key,"user-") !== false) {
+				$query = "INSERT INTO ".cms_db_prefix()."user_groups (group_id, user_id, create_date, modified_date) VALUES (".$db->qstr($group_id).", ".$db->qstr(substr($key,5)).", ".$db->DBTimeStamp(time()).", ".$db->DBTimeStamp(time()).")";
+				$result = $db->Execute($query);
+			}
+		}
+
+		audit($_SESSION["cms_admin_user_id"], $_SESSION["cms_admin_username"], $group_id, $group_name, 'Changed Group Assignments');
+		redirect("listgroups.php");
+		return;
+
+	}
+}
+
+include_once("header.php");
+
+if (!$access) {
+	print "<h3>".lang('noaccessto',array(lang('modifygroupassignments')))."</h3>";
+}
+else {
+
+?>
+<h3><?php echo lang('usersassignedtogroup',array($group_name))?></h3>
+
+<form method="post" action="changegroupassign.php">
+
+<div class="adminformSmall">
+
+
+
+<?php
+
+	$query = "SELECT * FROM ".cms_db_prefix()."users ORDER BY username";
+	$result = $db->Execute($query);
+
+	if ($result && $result->RowCount()) {
+
+		while($row = $result->FetchRow()) {
+
+			$users[$row['username']] = false;
+			$ids[$row['username']] = $row['user_id'];
+		}
+
+	}
+
+	$query = "SELECT u.user_id, u.username FROM ".cms_db_prefix()."user_groups ug INNER JOIN ".cms_db_prefix()."users u ON u.user_id = ug.user_id WHERE group_id = " . $group_id;
+	$result = $db->Execute($query);
+
+	if ($result && $result->RowCount()) {
+
+		while($row = $result->FetchRow()) {
+
+			$users[$row['username']] = true; 
+			$ids[$row['username']] = $row['user_id'];
+		}
+
+	}
+	echo "<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" summary=\"\" align=\"center\">";
+	foreach ($users as $key => $value) {
+
+		echo "<tr><td>";
+		echo "<input type=\"checkbox\"";
+		echo ($value == true?" checked":"");
+		echo " name=\"user-".$ids[$key]."\" value=\"1\">$key</td></tr>\n";
+	}
+
+?>
+
+<tr><td><br><input type="hidden" name="group_id" value="<?php echo $group_id?>">
+<input type="submit" name="changeassign" value="<?php echo lang('submit')?>" class="button" onmouseover="this.className='buttonHover'" onmouseout="this.className='button'">
+<input type="submit" name="cancel" value="<?php echo lang('cancel')?>" class="button" onmouseover="this.className='buttonHover'" onmouseout="this.className='button'"></td></tr>
+</table>
+</div>
+
+</form>
+
+<?php
+
+}
+
+include_once("footer.php");
+
+# vim:ts=4 sw=4 noet
+?>
---
+++ cms/tags/version-0.6rc1/admin/header.php
@@ -0,0 +1,88 @@
+<?php
+
+@ob_start();
+
+// Date in the past
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+
+// always modified
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+ 
+// HTTP/1.1
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+
+// HTTP/1.0
+header("Pragma: no-cache");
+
+// Language shizzle
+header("Content-Encoding: " . get_encoding());
+header("Content-Language: " . $current_language);
+header("Content-Type: text/html; charset=" . get_encoding());
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html40/loose.dtd">
+<html>
+<head>
+
+<title><?php echo lang('adminsystemtitle')?></title>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<script type="text/javascript" language="javascript" src="helparea.js"></script>
+
+<?php if (isset($htmlarea_flag) && isset($htmlarea_replaceall)) {?>
+	<script type="text/javascript">
+		_editor_url = "<?php echo $config["root_url"]?>/htmlarea/";
+		_editor_lang = "en";
+	</script>
+
+	<script type="text/javascript" src="<?php echo $config["root_url"]?>/htmlarea/htmlarea.js"></script>
+	<script type="text/javascript" defer>
+		var editor = null;
+		function initHtmlArea() {
+			HTMLArea.replaceAll();
+		}
+	</script>
+
+<?php } else if (isset($htmlarea_flag)) { ?>
+
+	<script type="text/javascript">
+		_editor_url = "<?php echo $config["root_url"]?>/htmlarea/";
+		_editor_lang = "en";
+	</script>
+
+	<script type="text/javascript" src="<?php echo $config["root_url"]?>/htmlarea/htmlarea.js"></script>
+
+	<script type="text/javascript">
+
+		HTMLArea.loadPlugin("TableOperations");
+		HTMLArea.loadPlugin("ContextMenu");
+		HTMLArea.loadPlugin("CharacterMap");
+		var editor = null;
+		function initHtmlArea() {
+			editor = new HTMLArea("content");
+			editor.registerPlugin(TableOperations);
+			editor.registerPlugin(ContextMenu);
+			editor.registerPlugin(CharacterMap);
+			editor.config.pageStyle = '<?php echo str_replace("'", "\\'", get_stylesheet($template_id))?>';
+			editor.generate();
+		}
+	</script>
+<?php }
+$userid = get_userid();
+if (get_preference($userid, 'use_wysiwyg') == "1" && isset($htmlarea_flag)){
+	$load = "onload=\"initHtmlArea();\""; ?>
+<?php } ?>
+
+</head>
+<body <?php echo (isset($load)?$load:""); ?>>
+
+<div id="header" class="header">
+<img src="../images/cms/cmsadminbanner.png" border="0" id="logo" alt="CMS Made Simple">
+</div>
+
+<?php
+include_once("menu.php");
+?>
+
+<div class="content">
