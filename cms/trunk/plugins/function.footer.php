3924486d4f675b9dc6c6348b3f88d360d76d1830
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-09-28 16:59:13 +0000

    Make footer plugin point to the proper place for submitting bugs
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@629 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/plugins/function.footer.php
+++ cms/trunk/plugins/function.footer.php
@@ -20,7 +20,7 @@ function smarty_cms_function_footer() {
     global $CMS_VERSION;
     echo '<div class="bottom">'."\n";
     echo 'Site powered by <a href="http://cmsmadesimple.org">CMS Made Simple</a> Version: '."$CMS_VERSION<br />\n";
-    echo 'Think you\'ve found a bug? <a href="http://trac.wishy.org/cms">Submit</a> it'."\n";
+    echo 'Think you\'ve found a bug? <a href="http://bugs.cmsmadesimple.org">Submit</a> it'."\n";
     echo "</div>\n";
 } ## smarty_function_show_footer
 
