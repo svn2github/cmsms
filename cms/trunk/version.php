a829ef8c9c69f8b464bfe763eb4f6caa4af74aee
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-10-05 01:35:35 +0000

    Ready for 0.7rc2
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@668 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/version.php
+++ cms/trunk/version.php
@@ -16,7 +16,7 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-$CMS_VERSION = "0.7-rc1";
+$CMS_VERSION = "0.7-rc2";
 $CMS_SCHEMA_VERSION = "7";
 
 # vim:ts=4 sw=4 noet
