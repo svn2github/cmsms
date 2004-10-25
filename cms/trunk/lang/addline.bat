69936d0ee5e9c4443c2a5d709c3990eef186a544
buster777 <buster777@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-10-25 23:21:54 +0000

    Had a couple mistakes in addline.bat
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@742 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/lang/addline.bat
+++ cms/trunk/lang/addline.bat
@@ -1,14 +1,16 @@
 @echo off
 
-IF %1=="" GOTO NoArgs
-IF %2=="" GOTO NoArgs
+IF "%1"=="" GOTO NoArgs
+IF "%2"=="" GOTO NoArgs
 find "$lang['admin']['%~1']" en_US/admin.inc.php > nul
 if errorlevel 1 goto newString
 	echo "The variable $lang['admin']['%~1'] already exists"
 	GOTO EOF
 
 :NoArgs
-echo "usage: addline.bat 'messagename' 'new message text'"
+echo usage: addline.bat "messagename" "new message text"
+echo Note: If you need to place quotes withen "new message text" they will be escaped automatically, so you do not need to worry about them.
+
 goto EOF
 
 :newString
