be0fe4d636eac01b4b35ea08ab832e247fb05972
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-11-16 19:09:18 +0000

    -- Release 0.7.3rc2 --
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@852 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ cms/tags/version-0.7.3rc2/lib/misc.functions.php
@@ -0,0 +1,224 @@
+<?php
+#CMS - CMS Made Simple
+#(c)2004 by Ted Kulp (wishy@users.sf.net)
+#This project's homepage is: http://cmsmadesimple.sf.net
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+/**
+ * Misc functions
+ *
+ * @package CMS
+ */
+/**
+ * Redirects to relative URL on the current site
+ *
+ * @author http://www.edoceo.com/
+ * @since 0.1
+ */
+function redirect($to, $noappend=false)
+{
+	global $gCms;
+	$config = $gCms->config;
+
+	$schema = $_SERVER['SERVER_PORT'] == '443' ? 'https' : 'http';
+	$host = strlen($_SERVER['HTTP_HOST'])?$_SERVER['HTTP_HOST']:$_SERVER['SERVER_NAME'];
+	if (ini_get("session.use_trans_sid") != "0" && $noappend == false)
+	{
+		$to = $to."?".session_name()."=".session_id();
+	}
+	if (headers_sent())
+	{
+		return false;
+	}
+	else
+	{
+		if (isset($config) and $config['debug'] == true)
+		{
+			echo "Debug is on.  Redirecting disabled...  Please click this link to continue.<br />";
+			echo "<a href=\"".$to."\">".$to."</a><br />";
+			global $sql_queries;
+			if (isset($sql_queries))
+			{
+				echo $sql_queries;
+			}
+			exit();
+		}
+		else
+		{
+			header("Location: $to");
+			exit();
+		}
+	}
+}
+
+/**
+ * Shows the difference in seconds between two microtime() values
+ *
+ * @since 0.3
+ */
+function microtime_diff($a, $b) {
+	list($a_dec, $a_sec) = explode(" ", $a);
+	list($b_dec, $b_sec) = explode(" ", $b);
+	return $b_sec - $a_sec + $b_dec - $a_dec;
+}
+
+/**
+ * Shows a very close approximation of an Apache generated 404 error.
+ *
+ * Shows a very close approximation of an Apache generated 404 error.
+ * It also sends the actual header along as well, so that generic
+ * browser error pages (like what IE does) will be displayed.
+ *
+ * @since 0.3
+ */
+function ErrorHandler404($errno, $errmsg, $filename, $linenum, $vars)
+{
+	if ($errno == E_USER_WARNING) {
+		@ob_end_clean();
+		header("HTTP/1.0 404 Not Found");
+		echo '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL was not found on this server.</p>
+</body></html>';
+		exit();
+	}
+}
+
+
+/**
+ * Simple template parser
+ *
+ * @since 0.6.1
+ */
+
+	function parse_template ($template, $tpl_array, $warn=0)
+	{
+		while ( list ($key,$val) = each ($tpl_array) )
+		{
+			if (!(empty($key)))
+			{
+				if(gettype($val) != "string")
+				{
+					settype($val,"string");
+				}
+				$template = eregi_replace("\{$key\}","$val","$template");
+			}
+		}
+
+		if(!$warn)
+		{
+			// Silently remove anything not already found
+
+			$template = ereg_replace("{([A-Z0-9_]+)}","",$template);
+		}
+		else
+		{
+			// Warn about unresolved template variables
+			if (ereg("({[A-Z0-9_]+})",$template))
+			{
+				$unknown = split("\n",$template);
+				while (list ($Element,$Line) = each($unknown) )
+				{
+					$UnkVar = $Line;
+					if(!(empty($UnkVar)))
+					{
+						$this->show_unknowns($UnkVar);
+					}
+				}
+			}
+		}
+		return $template;
+
+	}	// end parse_template();
+
+function cms_htmlentities($string, $param=ENT_QUOTES, $charset="UTF-8")
+{
+	$result = "";
+	#$result = htmlentities($string, $param, $charset);
+	$result = my_htmlentities($string);
+	return $result;
+}
+
+function my_htmlentities($val)
+{
+	if ($val == "")
+	{
+		return "";
+	}
+	$val = str_replace( "&#032;", " ", $val ); 
+
+	//Remove sneaky spaces 
+	// $val = str_replace( chr(0xCA), "", $val );   
+
+	$val = str_replace( "&"            , "&amp;"         , $val ); 
+	$val = str_replace( "<!--"         , "&#60;&#33;--"  , $val ); 
+	$val = str_replace( "-->"          , "--&#62;"       , $val ); 
+	$val = preg_replace( "/<script/i"  , "&#60;script"   , $val ); 
+	$val = str_replace( ">"            , "&gt;"          , $val ); 
+	$val = str_replace( "<"            , "&lt;"          , $val ); 
+	$val = str_replace( "\""           , "&quot;"        , $val ); 
+
+	// Uncomment it if you need to convert literal newlines 
+	//$val = preg_replace( "/\n/"        , "<br>"          , $val ); 
+
+	$val = preg_replace( "/\\$/"      , "&#036;"        , $val ); 
+
+	// Uncomment it if you need to remove literal carriage returns 
+	//$val = preg_replace( "/\r/"        , ""              , $val ); 
+
+	$val = str_replace( "!"            , "&#33;"         , $val ); 
+	$val = str_replace( "'"            , "&#39;"         , $val ); 
+	 
+	// Uncomment if you need to convert unicode chars 
+	//$val = preg_replace("/&#([0-9]+);/s", "&#\1;", $val ); 
+
+	// Strip slashes if not already done so. 
+
+	//if ( get_magic_quotes_gpc() ) 
+	//{ 
+	//	$val = stripslashes($val); 
+	//} 
+
+	// Swop user inputted backslashes 
+
+	//$val = preg_replace( "/\(?!&#|?#)/", "&#092;", $val );
+
+	return $val;
+}
+
+
+//Taken from http://www.webmasterworld.com/forum88/164.htm
+function nl2pnbr( $text )
+{
+	// Use \n for newline on all systems
+	$text = preg_replace("/(\r\n|\n|\r)/", "\n", $text);
+
+	// Only allow two newlines in a row.
+	$text = preg_replace("/\n\n+/", "\n\n", $text);
+
+	// Put <p>..</p> around paragraphs
+	$text = preg_replace('/\n?(.+?)(\n\n|\z)/s', "<p>$1</p>", $text);
+
+	// Convert newlines not preceded by </p> to a <br /> tag
+	$text = preg_replace('|(?<!</p>)\s*\n|', "<br />", $text);
+
+	return $text;
+}
+
+# vim:ts=4 sw=4 noet
+?>
---
+++ cms/tags/version-0.7.3rc2/modules/News/adminform.php
@@ -0,0 +1,379 @@
+<?php
+
+$error = "";
+$hiddenfields = "";
+$db = $cms->db;
+
+//$news_cat = "";
+$news_cat = "";
+if( isset($_POST[$id."news_cat"])) $news_cat = $_POST[$id."news_cat"];
+if( strlen($news_cat) == 0 ) {
+  if( isset( $_GET[$id."news_cat"])) $news_cat = $_GET[$id."news_cat"];
+  if( strlen($news_cat) == 0 ) {
+    if (isset($_POST[$id."selcat"])) $news_cat = $_POST[$id."selcat"];
+    if( strlen($news_cat) == 0 ) {
+      if (isset($_POST[$id."addcat"]) && strlen($_POST[$id."addcat"])) $news_cat = $_POST[$id."addcat"];
+    }
+  }
+}
+$title = "";
+if (isset($_POST[$id."newstitle"])) $title = $_POST[$id."newstitle"];
+$data = "";
+if (isset($_POST[$id."newscontent"])) $data = $_POST[$id."newscontent"];
+$post_date = "";
+if (isset($_POST[$id."post_date"])) $post_date = $_POST[$id."post_date"];
+$start_date = "";
+if (isset($_POST[$id."start_date"])) $start_date = $_POST[$id."start_date"];
+$expiry = "";
+if (isset($_POST[$id."expiry"])) $expiry = $_POST[$id."expiry"];
+$end_date = "";
+if (isset($_POST[$id."end_date"])) $end_date = $_POST[$id."end_date"];
+ 
+if (isset($_POST[$id."cancelsubmit"])) {
+        redirect("moduleinterface.php?module=News");
+        return;
+}
+//$hiddenfields .= "<input type=\"hidden\" name=\"".$id."news_cat\" value=\"$newscat\" />";
+switch( $moduleaction )
+{
+  case "Add":
+  case "Select":
+    echo "<p>Error!</p>"; 
+    break;
+
+  case "edit":
+    {
+      $newsid = (isset($_GET[$id."news_id"])?$_GET[$id."news_id"]:"").(isset($_POST[$id."news_id"])?$_POST[$id."news_id"]:"");
+      $hiddenfields .= "<input type=\"hidden\" name=\"".$id."action\" value=\"completeedit\" /><input type=\"hidden\" name=\"".$id."news_id\" value=\"".$newsid."\" />";
+      $query = "SELECT * FROM ".cms_db_prefix()."module_news WHERE news_id = ?";
+      $dbresult = $db->Execute($query, array($newsid));
+      if ($dbresult && $dbresult->RowCount() > 0) 
+      {
+        $row = $dbresult->FetchRow();
+	$news_cat = $row["news_cat"];
+        $title = $row["news_title"];
+        $data = $row["news_data"];
+        if (isset($row["news_date"]))
+        {
+            $post_date = $row["news_date"];
+        }
+        if (isset($row["start_time"]))
+        {
+            $start_date = $row["start_time"];
+        }
+        if (isset($row["end_time"]))
+        {
+            $end_date = $row["end_time"];
+        }
+      }
+    }
+    break;
+ 
+  case "add":
+    {
+      $hiddenfields .= "<input type=\"hidden\" name=\"".$id."action\" value=\"completeadd\" /><input type=\"hidden\" name=\"".$id."news_id\" value=\"".$newsid."\" />";
+      $post_date = rtrim(ltrim($db->DBTimeStamp(time()), "'"), "'");
+    }
+    break;
+
+  case "delete":
+    {
+      $query = "DELETE FROM ".cms_db_prefix()."module_news WHERE news_id = ?";        $dbresult = $db->Execute($query, array((isset($_GET[$id."news_id"])?$_GET[$id."news_id"]:"").(isset($_POST[$id."news_id"])?$_POST[$id."news_id"]:"")));
+      cms_mapi_audit($cms, (isset($_GET[$id."news_id"])?$_GET[$id."news_id"]:"").(isset($_POST[$id."news_id"])?$_POST[$id."news_id"]:""), "News", "Deleted News Item");
+       redirect("moduleinterface.php?module=News");
+     } 
+     break;
+
+  case "completeadd":
+    {
+      $hiddenfields .= "<input type=\"hidden\" name=\"".$id."action\" value=\"completeadd\" /><input type=\"hidden\" name=\"".$id."news_id\" value=\"".$newsid."\" />";
+      $validinfo = true;
+
+      if ($news_cat == "")                                                            {
+	$error .= "<li>No Category given</li>";
+        $validinfo = false;
+      }
+      if ($title == "") {
+        $error .= "<li>No title given</li>";
+        $validinfo = false;
+      }
+      if ($data == "") {
+        $error .= "<li>No content given</li>";
+        $validinfo = false;
+      }
+      if ($post_date == "") {
+        $error .= "<li>No post date given</li>";
+        $validinfo = false;
+      }
+      else if ($db->DBTimeStamp($post_date) === FALSE)
+      {
+        $error .= "<li>Post date not in a valid yyyy-mm-dd hh:mm:ss format</li>";
+        $validinfo = false;
+      }
+      if ($start_date !== "" && $end_date === "")
+      {
+        $error .= "<li>Entering a start date requires an end date also.</li>";
+        $validinfo = false;
+      }
+      if ($end_date !== "" && $start_date === "")
+      {
+        $error .= "<li>Entering an end date requires a start date also.</li>";
+        $validinfo = false;
+      }
+      if ($start_date !== "" && $end_date !== "" && $validinfo)
+      {
+        if ($db->DBTimeStamp($start_date) === FALSE)
+        {
+          $error .= "<li>Start date not in a valid yyyy-mm-dd hh:mm:ss format</li>";
+          $validinfo = false;
+        }
+        if ($db->DBTimeStamp($end_date) === FALSE)
+        {
+          $error .= "<li>End date not in a valid yyyy-mm-dd hh:mm:ss format</li>";
+          $validinfo = false;
+        }
+      }
+      if ($validinfo)
+      {
+	$new_id = $db->GenID(cms_db_prefix()."module_news_seq");
+        $querystart = "INSERT INTO ".cms_db_prefix()."module_news (news_cat, news_id, news_title, news_data, news_date, create_date";
+        $queryend = ") VALUES (?,?,?,?,?,?";
+        $params = array( $news_cat, $new_id, $title, $data, $post_date, $db->DBTimeStamp(time()));
+        if (strlen($end_date) > 0)
+        {
+          $querystart .= ", end_time";
+	  $queryend .= ",?";
+          array_push($params, $db->DBTimeStamp($end_date));
+        }
+        else if( $expiry != "" && $expiry != "Never" )
+        {
+	  $querystart .= ", end_time";
+          $queryend .= ",?";
+          $time = strtotime("+".$expiry);
+          array_push($params, $db->DBTimeStamp($time));
+	  if( $start_date == "" ) 
+          {
+	    $start_date = $db->DBTimeStamp(time());
+          }
+        }
+        if ($start_date != "")
+        {
+          $querystart .= ", start_time";
+	  $queryend .= ",?";
+          array_push($params, $db->DBTimeStamp($start_date));
+        }
+        $query = $querystart . $queryend . ")";
+        $dbresult = $db->Execute($query, $params);
+        //cms_mapi_audit($cms, $new_id, "News", "Added News Item");
+        //redirect("moduleinterface.php?module=News");
+        return;
+      }
+    }
+    break;
+
+  case "completeedit":
+    {
+      $newsid = (isset($_GET[$id."news_id"])?$_GET[$id."news_id"]:"").(isset($_POST[$id."news_id"])?$_POST[$id."news_id"]:"");
+      $hiddenfields .= "<input type=\"hidden\" name=\"".$id."action\" value=\"completeedit\" /><input type=\"hidden\" name=\"".$id."news_id\" value=\"".$newsid."\" />";
+                                                                                
+      $validinfo = true;
+        
+      if ($news_cat == "")                                                            {
+        $error .= "<li>No Category given</li>";
+        $validinfo = false;
+      } 
+      if ($title == "") 
+      {
+        $error .= "<li>No title given</li>";
+        $validinfo = false;
+      }
+      if ($data == "")
+      { 
+        $error .= "<li>No content given</li>";
+        $validinfo = false;
+      }
+      if ($post_date == "")
+      { 
+        $error .= "<li>No post date given</li>";
+        $validinfo = false;
+      }
+      else if ($db->DBTimeStamp($post_date) === FALSE)
+      {
+        $error .= "<li>Post date not in a valid yyyy-mm-dd hh:mm:ss format</li>";
+        $validinfo = false;
+      }
+      if ($start_date !== "" && $end_date === "")
+      {
+        $error .= "<li>Entering a start date requires an end date also.</li>";
+        $validinfo = false;
+      }
+      if ($end_date !== "" && $start_date === "")
+      {
+        $error .= "<li>Entering an end date requires a start date also.</li>";
+        $validinfo = false;
+      }
+      if ($start_date !== "" && $end_date !== "" && $validinfo)
+      {
+        if ($db->DBTimeStamp($start_date) === FALSE)
+        {
+          $error .= "<li>Start date not in a valid yyyy-mm-dd hh:mm:ss format</li>";
+          $validinfo = false;
+        }
+        if ($db->DBTimeStamp($end_date) === FALSE)
+        {
+          $error .= "<li>End date not in a valid yyyy-mm-dd hh:mm:ss format</li>";
+          $validinfo = false;
+        }
+      }
+      if ($validinfo)
+      {
+        $query = "UPDATE ".cms_db_prefix()."module_news SET news_cat = ?, news_title = ?, news_data = ?, modified_date = ?";
+        $params = array($news_cat, $title, $data, $db->DBTimeStamp(time()));
+        if ($start_date != "")
+        {
+          $query .= ", start_time = ?";
+          array_push($params, $db->DBTimeStamp($start_date));
+        }
+        else
+        {
+          $query .= ", start_time = ?";
+          array_push($params, NULL);
+        }
+        if ($end_date != "")
+        {
+          $query .= ", end_time = ?";
+          array_push($params, $db->DBTimeStamp($end_date));
+        }
+        else
+        {
+          $query .= ", end_time = ?";
+          array_push($params, NULL);
+        }
+        if ($post_date != "")
+        {
+          $query .= ", news_date = ?";
+          array_push($params, $db->DBTimeStamp($post_date));
+        }
+        $query .= " WHERE news_id = ?";
+        array_push($params, $newsid);
+        $dbresult = $db->Execute($query, $params);
+        
+        cms_mapi_audit($cms, $newsid, "News", "Edited News Item");
+        redirect("moduleinterface.php?module=News");
+	return;
+      }
+    }
+    break;
+
+  default:
+    echo "ERROR! Unknown action!<BR>";
+    return;
+    break;
+}
+  
+
+if ( $error != "" )
+{
+  echo "<ul class=\"error\">$error</ul>";
+} 
+?>
+
+<div class="adminform">
+ 
+<?php echo cms_mapi_create_admin_form_start("News", $id)?>
+<table width="100%" border="0">
+  <tr>
+    <td width="100">Category:</td>
+    <td>
+      <SELECT NAME="<?php echo $id?>selcat" SIZE="4">
+        <?php
+	  $query = "SELECT news_cat FROM "
+	     .cms_db_prefix()."module_news GROUP BY news_cat";
+	  $dbresult = $cms->db->Execute($query);
+          if( $dbresult && $dbresult->RowCount() )
+          {
+	    while( $row = $dbresult->FetchRow() )
+            {
+	       if( strlen( $row["news_cat"] ) > 0 ) 
+               {
+	         if( $row["news_cat"] == $news_cat )
+                 {
+                   echo "<OPTION SELECTED>".$row["news_cat"]."</OPTION>\n";
+                 }
+                 else
+                 {
+                   echo "<OPTION>".$row["news_cat"]."</OPTION>\n";
+                 }
+               }
+            }
+          }
+        ?>
+      </SELECT>
+    </td>
+  </tr>
+  <tr>
+     <td>New Category:</td>
+     <td><INPUT NAME="<?php echo $id?>addcat" SIZE="40" MAXLENGTH="255">
+  </td>
+  <tr>
+    <td width="60">Title:</td>
+    <td><input name="<?php echo $id?>newstitle" maxlength="255" value="<?php echo $title?>" class="standard"/></td>
+  </tr>
+  <tr>
+    <td>Content:</td>
+    <td><textarea id="<?php echo $id?>newscontent" name="<?php echo
+$id?>newscontent" cols="80" rows="12"><?php echo $data?></textarea></td>
+  </tr>
+  <?php //if ($moduleaction == "edit" || $moduleaction == "completeedit") { ?>
+  <tr>
+    <td>Post Date:</td>
+    <td><input type="text"  name="<?php echo $id?>post_date" maxlength="20" length="12" value="<?php echo $post_date?>" /></td>
+  </tr>
+  <?php //} ?>
+  <tr>
+    <td>Start Date:</td>
+    <td><input type="text"  name="<?php echo $id?>start_date" maxlength="20" length="12" value="<?php echo $start_date?>" /></td>
+  </tr>
+  <tr>
+    <td>Expiry:</td>
+    <td>
+<?php
+   if( $moduleaction == "edit" || $moduleaction == "completeedit" ) {
+      echo "<select name=\"".$id."expiry\" disabled>";
+   }
+   else {
+      echo "<select name=\"".$id."expiry\">";
+   }
+?>
+         <option>1 Day</option>
+         <option>1 Week</option>
+         <option>2 Weeks</option>
+         <option>1 Month</option>
+         <option>3 Months</option>
+         <option selected>6 Months</option>
+         <option>1 Year</option>
+         <option>Never</option>
+       </select>
+    </td>
+  </tr>
+  <tr>
+    <td>End Date:</td>
+    <td><input type="text"  name="<?php echo $id?>end_date" maxlength="20" length="12" value="<?php echo $end_date?>" /></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td><em>Note:</em> Dates must be in a 'yyyy-mm-dd hh:mm:ss' format.</td>
+  </tr>
+  <tr>
+    <td><?php echo $hiddenfields?>&nbsp;</td>
+    <td>
+      <input type="submit" name="<?php echo $id?>submit" value="Submit" />
+      <input type="submit" name="<?php echo $id?>cancelsubmit" value="Cancel" />
+    </td>
+  </tr>
+</table>
+ 
+<?php echo cms_mapi_create_admin_form_end()?>
+ 
+</div>
+ 
