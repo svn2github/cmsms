c2460b5927f2769e7d19c6dfae917672a53e8c0e
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-11-26 19:25:31 +0000

    Moved lang to admin
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@894 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/branches/wishy/include.php
+++ cms/branches/wishy/include.php
@@ -143,67 +143,8 @@ load_modules();
 #Load all site preferences
 load_site_preferences();
 
-#Nice decent default
-$current_language = "en_US";
-
-#Only do language stuff for admin pages
 if (isset($CMS_ADMIN_PAGE)) {
-
-	#Read in all current languages...
-	$dir = dirname(__FILE__)."/lang";
-	$ls = dir($dir);
-	while (($file = $ls->read()) != "") {
-		if (is_file("$dir/$file") && strpos($file, "nls.php") != 0) {
-			include("$dir/$file");
-		}
-	}
-
-	#Check to see if there is already a language in use...
-	if (isset($_POST["change_cms_lang"]))
-	{
-		$current_language = $_POST["change_cms_lang"];
-		setcookie("cms_language", $_POST["change_cms_lang"]);
-	}
-	else if (isset($_COOKIE["cms_language"]))
-	{
-		$current_language = $_COOKIE["cms_language"];
-	}
-	else
-	{
-		#No, take a stab at figuring out the default language...
-		#Figure out default language and set it if it exists
-		if (isset($_SERVER["HTTP_ACCEPT_LANGUAGE"])) {
-			$svrstring = $_SERVER["HTTP_ACCEPT_LANGUAGE"];
-			$alllang = substr($svrstring,0,strpos($svrstring, ";"));
-			$langs = explode(",", $alllang);
-
-			foreach ($langs as $onelang) {
-				#Check to see if lang exists...
-				if (isset($nls['language'][$onelang])) {
-					$current_language = $onelang;
-					setcookie("cms_language", $onelang);
-					break;
-				}
-				#Check to see if alias exists...
-				if (isset($nls['alias'][$onelang])) {
-					$alias = $nls['alias'][$onelang];
-					if (isset($nls['language'][$alias])) {
-						$current_language = $alias;
-						setcookie("cms_language", $alias);
-						break;
-					}
-				}
-			}
-		}
-	}
-	#Ok, we have a language to load, let's load it already...
-	if (isset($nls['file'][$current_language]))
-	{
-		foreach ($nls['file'][$current_language] as $onefile)
-		{
-			include($onefile);
-		}
-	}
+	include_once(dirname(__FILE__)."/admin/lang.php");
 }
 
 #Check for HTML_BBCodeParser
