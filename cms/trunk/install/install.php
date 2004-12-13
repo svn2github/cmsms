79752f3a8dd778d02b4ae34ca9a518f5bb9fd4bd
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-12-13 11:32:24 +0000

    Fix for config.php check
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@995 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/install/install.php
+++ cms/trunk/install/install.php
@@ -16,7 +16,7 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-$config = "config.php";
+$config = dirname(dirname(__FILE__))."/config.php";
 if (!file_exists($config)) {
     $file = @fopen($config, "w");
     if ($file != 0) {
