a1d385f81901b8b339e0f246628220c51389ad14
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2004-12-14 03:38:16 +0000

    Updated the sitemap to work with new content model
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1008 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/lib/contenttypes/Content.inc.php
+++ cms/trunk/lib/contenttypes/Content.inc.php
@@ -99,7 +99,7 @@ class Content extends ContentBase
 		$text .= '<tr><td>'.lang('menutext').':</td><td><input type="text" name="menutext" value="'.$this->mMenuText.'" /></td></tr>';
 		$text .= '<tr><td>'.lang('pagealias').':</td><td><input type="text" name="alias" value="'.$this->mAlias.'" /></td></tr>';
 		$text .= '<tr><td>'.lang('template').':</td><td>'.TemplateOperations::TemplateDropdown('template_id', $this->mTemplateId).'</td></tr>';
-		$text .= '<tr><td>'.lang('content').':</td><td>'.create_textarea(true, $this->mProperties->GetValue('content_en'), 'content', 'syntaxHighlight', 'content').'</td></tr>'."\n";
+		$text .= '<tr><td>'.lang('content').':</td><td>'.create_textarea(true, $this->mProperties->GetValue('content_en'), 'content_en', 'syntaxHighlight', 'content_en').'</td></tr>'."\n";
 		$text .= '<tr><td>'.lang('headtags').':</td><td>'.create_textarea(true, $this->mProperties->GetValue('headtags'), 'headtags', 'syntaxHighlight', 'headtags').'</td></tr>'."\n";
 		$text .= '<tr><td>'.lang('active').':</td><td><input type="checkbox" name="active"'.($this->mActive?' checked="checked"':'').' /></td></tr>';
 		$text .= '<tr><td>'.lang('showinmenu').':</td><td><input type="checkbox" name="showinmenu"'.($this->mShowInMenu?' checked="checked"':'').' /></td></tr>';
--- cms/trunk/plugins/function.sitemap.php
+++ cms/trunk/plugins/function.sitemap.php
@@ -17,82 +17,116 @@
 #Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 function smarty_cms_function_sitemap($params, &$smarty) {
-	# set defaults
-	global $gCms; 
-	$thispage = $gCms->variables['page'];
 
-	$standard = '<a href="{url}">{menu_text} - {page_title}</a>';
-	$rootitem = '<h2><a href="{url}">{menu_text} - {page_title}</a></h2>';
-	$sectionheader = '<h3>{menu_text}</h3>';
-	
-	$sitemap = "";
-	$sitemap .= "\n\n<!-- sitemap start-->\n<ul>\n";
+	global $db;
 
-	# get menu data
-	$newparams["show"] = "menu";
-	$content = db_get_menu_items($newparams);	
-	$level = array(0);
-	
-	foreach ($content as $menuitem) {
-		
-		# have we changed into another level?
-		if ($menuitem->parent_id!=end($level)) {
-			if (in_array($menuitem->parent_id, $level)) {
-				do {
-					$sitemap .= "</ul>\n</li>\n";
-				} while ($menuitem->parent_id != array_pop($level) && $menuitem->parent_id >0);
-			} else {
-				array_push($level, $menuitem->parent_id);
+	$allcontent = ContentManager::GetAllContent();
+
+	# defining variables
+	$menu = "";
+	$last_level = 0;
+	$count = 0;
+	$in_hr = 0;
+
+	foreach ($allcontent as $onecontent)
+	{
+		#Handy little trick to figure out how deep in the tree we are
+		#Remember, content comes to use in order of how it should be displayed in the tree already
+		$depth = count(split('\.', $onecontent->Hierarchy()));
+
+		#If hierarchy starts with the start_element (if it's set), then continue on
+		if (isset($params['start_element']))
+		{
+			if (!(strpos($onecontent->Hierarchy(), $params['start_element']) !== FALSE && strpos($onecontent->Hierarchy(), $params['start_element']) == 0))
+			{
+				continue;
 			}
-		} 
-	
-	
-		# set class
-		if ($menuitem->page_type=='sectionheader') $class=" class=\"sectionheader\""; 
-		elseif ($menuitem->page_type=='link') $class=" class=\"link\"";
-		else $class="";
-
-		$sitemap .= "<li$class>";
-		
-		if ($menuitem->parent_id==0)
-			$a = $rootitem;
-		elseif ($menuitem->page_type=="sectionheader")
-			$a = $sectionheader;
-		else
-			$a = $standard;
-
-		// replace {url}, {menu_text}, {page_title}
-		$search = array('{url}', '{menu_text}', '{page_title}');
-		$replace = array($menuitem->url, $menuitem->menu_text, $menuitem->page_title);
-		
-		$sitemap .= str_replace($search, $replace, $a);
-
-		# any children?
-		if ($menuitem->childs!="") {
-			$sitemap .= "\n<ul>\n";
-		} else {
-			$sitemap .= "</li>\n";
 		}
-	}
-	
-	# descend to ground safely - close off all the tags
-	while (array_pop($level)) $sitemap .= "</ul>\n</li>\n";
 
-	#close off the menu
-	$sitemap .= "</ul>\n<!-- sitemap end -->\n\n";
-	
-	return $sitemap;
+		#Now check to make sure we're not too many levels deep if number_of_levels is set
+		if (isset($params['number_of_levels']))
+		{
+			$number_of_levels = $params['number_of_levels'] - 1;
+			$base_level = 1;
+			
+			#Is start_element set?  If so, reset the base_level to it's level
+			if (isset($params['start_element']))
+			{
+				$base_level = count(split('\.', $params['start_element']));
+			}
+
+			#If this element's level is more than base_level + number_of_levels, then scratch it
+			if ($base_level + $number_of_levels < $depth)
+			{
+				continue;
+			}
+		}
+
+		if (!$onecontent->Active())
+		{
+			continue;
+		}
+
+		if ($onecontent->Type() == 'separator')
+		{
+			continue;
+		}
+
+		if ($onecontent->Type() == 'sectionheader')
+		{
+			if ($in_hr == 1)
+			{
+				$menu .= "</ul>\n";
+				$in_hr = 0;
+			}
+			$menu .= "<div class=\"sectionheader\">".$onecontent->MenuText()."</div>\n";
+			if ($count > 0 && $in_hr == 0)
+			{
+				$menu .= "<ul>\n";
+				$in_hr = 1;
+			}
+		}
+		else
+		{
+			if ($depth < $last_level)
+			{
+				for ($i = $depth; $i < $last_level; $i++) $menu .= "</ul>\n";
+			}
+			if ($depth > $last_level)
+			{
+				for ($i = $depth; $i > $last_level; $i--) $menu .= "<ul>\n";
+			}
+			$menu .= "<li><h2><a href=\"".$onecontent->GetURL()."\">".$onecontent->MenuText();
+			if ($onecontent->Name() != '')
+			{
+				$menu .= ' - ' . $onecontent->Name();
+			}
+			$menu .= "</a></h2></li>\n";
+			$in_hr = 1;
+			$last_level = $depth;
+		}
+		$count++;
+	}
+
+	for ($i = 0; $i < $last_level; $i++) $menu .= "</ul>";
+
+	return $menu;
+
 }
 
 function smarty_cms_help_function_sitemap() {
 	?>
 	<h3>What does this do?</h3>
-	<p>Prints a sitemap.</p>
+	<p>Prints a bullet menu.</p>
 	<h3>How do I use it?</h3>
-	<p>Just insert the tag into your template/page like: <code>{sitemap}</code></p>
+	<p>Just insert the tag into your template/page like: <code>{bulletmenu}</code></p>
 	<h3>What parameters does it take?</h3>
 	<p>
-		Sitemap doesn't take any parameters.	
+	<ul>
+		<li><em>(optional)</em> <tt>showadmin</tt> - 1/0, whether you want to show or not the admin link.</li>
+		<li><em>(optional)</em> <tt>start_element</tt> - the hierarchy of your element (ie : 1.2 or 3.5.1 for example). This parameter sets the root of the menu.</li>
+		<li><em>(optional)</em> <tt>number_of_levels</tt> - an integer, the number of levels you want to show in your menu.</li>
+	</ul>
 	</p>
 
 	<?php
@@ -100,7 +134,7 @@ function smarty_cms_help_function_sitemap() {
 
 function smarty_cms_about_function_sitemap() {
 	?>
-	<p>Author: Marcus Deglos &lt;<a href="mailto:md@zioncore.com">md@zioncore.com</a>&gt;</p>
+	<p>Author: Julien Lancien&lt;calexico@cmsmadesimple.org&gt;</p>
 	<p>Version: 1.0</p>
 	<p>
 	Change History:<br/>
