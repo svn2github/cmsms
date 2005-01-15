5db53d7e463336a1d046455e2e6e9bd7351e353b
uptoeleven <uptoeleven@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-01-15 01:56:39 +0000

    Added sequences. sequence = series of actions to run in predefined order. You define the sequence which is saved to the database. If you don't pass currentStep=(number) in the request or the session timesout you get thrown to the panic page
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1201 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/branches/wishy/include.php
+++ cms/branches/wishy/include.php
@@ -106,7 +106,9 @@ require_once(dirname(__FILE__)."/lib/misc.functions.php");
 require_once(dirname(__FILE__)."/lib/page.functions.php");
 require_once(dirname(__FILE__)."/lib/content.functions.php");
 require_once(dirname(__FILE__)."/lib/module.functions.php");
+require_once(dirname(__FILE__)."/lib/sequence.functions.php");
 require_once(dirname(__FILE__)."/lib/classes/class.module.inc.php");
+require_once(dirname(__FILE__)."/lib/classes/class.sequence.inc.php");
 require_once(dirname(__FILE__)."/lib/translation.functions.php");
 
 #Load content types
