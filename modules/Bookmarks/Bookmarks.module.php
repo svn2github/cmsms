277fbd3f4a69c08d2e8c02a1c0b513baccb59007
akra <akra@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-02-09 17:03:22 +0000

    All working ready for the 0.9rc release.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1281 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/Bookmarks/Bookmarks.module.php
+++ modules/Bookmarks/Bookmarks.module.php
@@ -92,8 +92,8 @@ class Bookmarks extends CMSModule
 			<td> Display the category at the start of the list. <em>(optional)</em></td>
 		</tr>
 		<tr>
-			<td>display_approved</td>
-			<td>Display unapproved bookmarks. <em>(optional)</em></td>
+			<td>display_unapproved</td>
+			<td>Display unapproved bookmarks. (true or false)<em>(optional)</em></td>
 		</tr>
 		<tr>
 			<td>number</td>
@@ -137,13 +137,15 @@ class Bookmarks extends CMSModule
 		</tr>
 		<tr>
 			<td>email_to</td>
-			<td>Used with addform=true. Set to the email address to which email notifications of
-			new bookmarks will be sent. <em>(optional)</em></td>
+			<td>Used with addform=true. Set to the user id of the user to which email notifications of
+			new bookmarks will be sent. Note that you should choose an id for a user that has an email address
+			set up. By default, the admin user does not have an email address set.<em>(optional)</em></td>
 		</tr>
 		<tr>
 			<td>email_from</td>
-			<td>Used with addform=true. Set to the email address to be used in the "from" field
-			for email notifications. <em>(optional)</em></td>
+			<td>Used with addform=true. Set to the user id to be used in the "from" field
+			for email notifications. In general, you would user the admin user's id (1), but you must set an
+			email address for the admin user as initially it does not have one.<em>(optional)</em></td>
 		</tr>
 		</table>
 
@@ -328,33 +330,80 @@ EOT;
 	    return $result;
 	}	
 	
+	function AllowAccess($permission='Modify Bookmarks')
+	{
+		$access = $this->CheckPermission($permission);
+		if (!$access)  {
+			echo "<p class=\"error\">You need the '$permission' permission to perform that function.</p>";
+			return false;
+		}
+		else return true;
+	
+	}
+	
 	function DoAction($name, $id, $parameters, $returnid='')
 	{
 		switch($name)
 		{
 			case 'default':  // default user front end page
+				$this->UserDisplay($id, $parameters, $returnid);
+				break;
+				
+			case 'bookmarks_useradd':
+				$this->UserAddBookmark($id, $parameters, $returnid);
 				break;
 				
 			case 'defaultadmin': // default admin page
-				$this->AdminDisplayDefaultAdminPage($id, $parameters, $returnid);
+				if($this->AllowAccess())
+				{
+					$this->AdminDisplayDefaultAdminPage($id, $parameters, $returnid);
+				}
 				break;
 				
 			case 'admin_categories_update':
-				$this->AdminUpdateCategories($id, $parameters, $returnid);
+				if($this->AllowAccess())
+				{
+					$this->AdminUpdateCategories($id, $parameters, $returnid);
+				}
 				break;
 				
 			case 'admin_add_bookmark':
 			case 'admin_edit_bookmark':
-				$this->AdminDisplayAddBookmark($id, $parameters, $returnid);
+				if($this->AllowAccess())
+				{
+					$this->AdminDisplayAddBookmark($id, $parameters, $returnid);
+				}
 				break;
 				
 			case 'admin_manage_categories':
-				$this->AdminDisplayCategories($id, $parameters, $returnid);
+				if($this->AllowAccess())
+				{
+					$this->AdminDisplayCategories($id, $parameters, $returnid);
+				}
 				break;
 				
 			case 'admin_bookmarks_update':
-				$this->AdminUpdateBookmarks($id, $parameters, $returnid);
+				if($this->AllowAccess())
+				{
+					$this->AdminUpdateBookmarks($id, $parameters, $returnid);
+					$url = $this->CreateLink($id, 'defaultadmin', $returnid, $contents='', $params=array('result'=>'1'), '', true);
+					$url = str_replace('&amp;', '&', $url);
+					
+					redirect($url);
+				}
 				break;
+				
+			case 'admin_delete_bookmark':
+				if($this->AllowAccess())
+				{
+					$this->AdminDeleteBookmark($id, $parameters, $returnid);
+					$url = $this->CreateLink($id, 'defaultadmin', $returnid, $contents='', $params=array('result'=>'1'), '', true);
+					$url = str_replace('&amp;', '&', $url);
+					
+					redirect($url);
+				}
+				break;
+				
 				
 			default:
 				break;
@@ -630,7 +679,6 @@ EOT;
 	function AdminUpdateBookmarks($id, $parameters, $returnid)
 	{
 		$db = $this->cms->db; /* @var $db ADOConnection */
-		$db->debug = true;
 		$bookmarks_to_categories_table_name = cms_db_prefix().'module_bookmarks_to_categories';
 
 	
@@ -686,23 +734,39 @@ EOT;
 		$db->Execute($sql);
 	
 		// update bookmarks_to_categories
-		foreach($categories as $category_id)
+		if(count($categories) > 0)
 		{
-			$category_id = intval($category_id);
-			if($category_id > 0)
+			foreach($categories as $category_id)
 			{
-				$sql = "INSERT INTO " . $this->bookmarks_to_categories_table_name . "
-							(category_id, bookmark_id)
-						VALUES
-							($category_id, $bookmark_id)";
-				$db->Execute($sql);
+				$category_id = intval($category_id);
+				if($category_id > 0)
+				{
+					$sql = "INSERT INTO " . $this->bookmarks_to_categories_table_name . "
+								(category_id, bookmark_id)
+							VALUES
+								($category_id, $bookmark_id)";
+					$db->Execute($sql);
+				}
 			}
 		}		
 	
-		$url = $this->CreateLink($id, 'defaultadmin', $returnid, $contents='', $params=array('result'=>'1'), '', true);
-		$url = str_replace('&amp;', '&', $url);
-		
-		redirect($url);
+	}
+	
+	function AdminDeleteBookmark($id, $parameters, $returnid)
+	{
+		$db = $this->cms->db; /* @var $db ADOConnection */
+		$bookmarks_table_name = $this->bookmarks_table_name;
+		$bookmarks_to_categories_table_name = $this->bookmarks_to_categories_table_name;
+	
+		$bookmark_id = get_value_with_default(@$parameters['bookmark_id'], -1);
+	
+		// delete current bookmarks_to_categories records for this bookmark
+		$sql = "DELETE FROM $bookmarks_to_categories_table_name WHERE bookmark_id = $bookmark_id";
+		$db->Execute($sql);
+	
+		// delete this bookmark
+		$sql = "DELETE FROM $bookmarks_table_name WHERE bookmark_id = $bookmark_id";
+		$db->Execute($sql);
 	}
 	
 	function AdminDisplayManageBookmarks($id, $parameters, $returnid)
@@ -850,6 +914,446 @@ EOT;
 			echo "<!-- End of Bookmarks Module -->\n";
 			
 		}		
+	}
+	
+	function UserDisplay($id, $parameters, $returnid)
+	{
+		// handling of display of {cms_module module=Bookmarks}
+		if(isset($parameters['makerssbutton']))
+		{
+			echo $this->CreateLink($id, $returnid, 'default', '<img border="0" src="images/cms/xml_rss.gif" alt="RSS Feed" />',
+					array('showtemplate'=>'false','type'=>'rss'));
+			return;
+		}
+	
+		if(isset($parameters['auto_detect_link']))
+		{
+			$title = empty($parameters['title']) ? 'Bookmarks RSS' : $parameters['title'];
+			$url = $this->CreateLink($id, $returnid, 'default', '',
+					array('showtemplate'=>'false','type'=>'rss'), '', true);
+		
+			echo "<link rel='alternate' type='application/rss+xml' title='$title' href='$url' />";
+			return;
+		}
+	
+		if(isset($parameters["addform"]))
+		{
+			$this->DisplayAddBookmarkForm($id, $parameters, $returnid);
+			return;
+		}
+	
+		$type = get_value_with_default(@$parameters['type'], 'text');
+		$display_unapproved = get_value_with_default(@$parameters['display_unapproved'], false);
+		$show_category_with_title = get_value_with_default(@$parameters['show_category_with_title'], '');
+		$category = get_value_with_default(@$parameters['category'], '');
+		$number = get_value_with_default(@$parameters['number'], '');
+		$order_by_date = get_value_with_default(@$parameters['order_by_date'], false);
+		if($order_by_date)
+		{
+			$show_category_with_title = false;
+		}
+	
+		$categories_table_name = $this->categories_table_name;
+		$bookmarks_to_categories_table_name = $this->bookmarks_to_categories_table_name;
+		$bookmarks_table_name = $this->bookmarks_table_name;
+	
+		$db = $this->cms->db;
+		if($type == "text")
+		{
+			$sql = "SELECT $bookmarks_to_categories_table_name.category_id
+						,$categories_table_name.category_name
+						,$bookmarks_table_name.*
+				FROM $bookmarks_table_name
+				INNER JOIN $bookmarks_to_categories_table_name
+				   ON $bookmarks_table_name.bookmark_id = $bookmarks_to_categories_table_name.bookmark_id
+				INNER JOIN $categories_table_name
+				   ON $bookmarks_to_categories_table_name.category_id = $categories_table_name.category_id
+			";
+	
+			$where = 'WHERE';
+			if($display_unapproved == false)
+			{
+				$sql .= "$where $bookmarks_table_name.bookmark_approved <> 0 ";
+				$where = 'AND';
+			}
+			if($category)
+			{
+				$cats = explode(',', $category);
+				$sql .= $where . ' (';
+				$count = 0;
+				foreach($cats as $cat)
+				{
+					$cat = trim($cat);
+					if($count != 0)
+					{
+						$sql .= ' OR ';
+					}
+					$count++;
+					$sql .= "$categories_table_name.category_name LIKE '$cat' ";
+				}
+				$sql .=	') ';
+			}
+	
+			if($order_by_date)
+			{
+				$sql .= "ORDER BY $bookmarks_table_name.bookmark_modified_date";
+			}
+			else
+			{
+				$sql .= "ORDER BY $categories_table_name.category_order
+		    				,$categories_table_name.category_name
+		        	        ,$bookmarks_table_name.bookmark_title ";
+			}
+		}
+		else
+		{
+			$sql = "SELECT DISTINCT $bookmarks_table_name.*
+				FROM $bookmarks_table_name
+				INNER JOIN $bookmarks_to_categories_table_name
+				   ON $bookmarks_table_name.bookmark_id = $bookmarks_to_categories_table_name.bookmark_id
+				INNER JOIN $categories_table_name
+				   ON $bookmarks_to_categories_table_name.category_id = $categories_table_name.category_id
+				";
+	
+			$where = 'WHERE';
+			if($display_unapproved == false)
+			{
+				$sql .= "$where $bookmarks_table_name.bookmark_approved <> 0 ";
+				$where = 'AND';
+			}
+	
+			if($category)
+			{
+				$cats = explode(',', $category);
+				$sql .= $where . ' (';
+				$count = 0;
+				foreach($cats as $cat)
+				{
+					$cat = trim($cat);
+					if($count != 0)
+					{
+						$sql .= ' OR ';
+					}
+					$count++;
+					$sql .= "$categories_table_name.category_name LIKE '$cat' ";
+				}
+				$sql .=	') ';
+			}
+	
+			$sql .= "ORDER BY $bookmarks_table_name.bookmark_modified_date DESC";
+		}
+	
+		if($number)
+		{
+			$dbresult = $db->SelectLimit($sql,$number); /* @var $dbresult ADORecordSet */
+		}
+		else
+		{
+			$dbresult = $db->Execute( $sql ); /* @var $dbresult ADORecordSet */
+		}
+	
+		if(isset($parameters['assign']))
+		{
+			// if assigning, just return array of data
+			return $dbresult->GetArray();
+		}
+	
+		if ($dbresult && $dbresult->RowCount() > 0)
+		{
+			if ($type == "rss")
+			{
+				// find most recent date - first record as we are date_ordered
+				$date = $dbresult->fields['bookmark_modified_date'];
+				$date = date('r', strtotime($date));
+				$title = "Boomarks";
+	
+				if (isset($parameters[$id."title"]))
+					$title = $parameters[$id."title"];
+				while(@ob_end_clean());
+				header('Content-type: text/xml');
+	
+				echo "<?xml version='1.0'?>\n";
+				echo "<rss version='2.0'>\n";
+				echo "   <channel>\n";
+				echo "   <title>$title</title>\n";
+				echo "   <link>{$cms->config['root_url']}</link>\n";
+				echo "   <pubDate>$date</pubDate>\n";
+				echo "   <lastBuildDate>$date</lastBuildDate>\n";
+				if(isset($parameters["category"]))
+					echo "   <description>{$parameters["category"]} Bookmarks</description>\n";
+				else
+					echo "   <description>Bookmarks</description>\n";
+	
+			}
+			else
+			{
+				$number_of_columns = get_value_with_default(@$parameters['columns'], 3);
+	
+				$num_rows = $dbresult->RecordCount();
+				$rows_per_column = intval($num_rows / $number_of_columns) + 10; /* 10 is a fudge factor to make it look better! */
+	
+				$current_category_name = '';
+				$columns = array();
+				$col_number = 0;
+				ob_start();
+	
+			}
+			
+			$target = '';
+			if(isset($parameters['target']))
+			{
+				$target = ' target = "' . $parameters["target"] . '" ';
+			}
+	
+			$row_count = 0;
+			while( ($row = $dbresult->FetchRow()) )
+			{
+				$row_count++;
+				$category_name = $row['category_name'];
+				$bookmark_title = $row['bookmark_title'];
+				$bookmark_url = $row['bookmark_url'];
+	
+				if(strstr($bookmark_url, '//') === false)
+				{
+					$bookmark_url = 'http://' . $bookmark_url;
+				}
+	
+				if ($type == "rss")
+				{
+					$bookmark_modified_date = $row['bookmark_modified_date'];
+					$bookmark_modified_date_rfc822 = date('r', strtotime($bookmark_modified_date));
+					$bookmark_summary = $row['bookmark_summary'];
+	
+					echo "   <item>\n";
+					echo "      <title>$bookmark_title</title>\n";
+					echo "      <link>$bookmark_url</link>\n";
+					if($bookmark_summary)
+						echo "      <description>$bookmark_summary</description>\n";
+					echo "      <pubDate>$bookmark_modified_date_rfc822</pubDate>\n";
+					echo "   </item>\n";
+				}
+				else
+				{
+					$bookmark_summary = empty($parameters["summaries"]) ? '' : "<div class='cms-module-bookmarks-summary'>" . $row['bookmark_summary'] . "</div>";
+	
+					if($current_category_name != $category_name)
+					{
+						// new category - can we start a new column?
+						$current_category_name = $category_name;
+	
+						// close the list
+						if($row_count != 1)
+						{
+							echo "\t\t</ul>\n";
+							$string = ob_get_contents();
+							ob_end_clean();
+							$columns[$col_number] .= $string;
+							$col_number++;
+							if($col_number >= $number_of_columns)
+								$col_number = 0;
+						ob_start();
+						}
+	
+						// start new column
+						if($show_category_with_title)
+							echo "\t\t<div class='cms-module-bookmarks-header'>$current_category_name</div>\n";
+						echo "\t\t<ul class='cms-module-bookmarks-list'>\n";
+					}
+	
+					echo "\t\t\t<li><a class='bookmark-link' $target href='$bookmark_url'>$bookmark_title</a>$bookmark_summary</li>\n";
+				}
+			}
+	
+			if ($type == 'rss')
+			{
+				echo "   </channel>\n";
+				echo "</rss>\n";
+				exit;
+			}
+			else
+			{
+				// close off final column
+				echo "\t\t</ul>\n";
+				$string = ob_get_contents();
+				ob_end_clean();
+				$columns[$col_number] .= $string;
+	
+				// display in a table
+				echo "<!-- Bookmarks Module: number of bookmarks: $num_rows -->\n";
+				echo "<table id='cms-module-bookmarks' border='0' cellpadding='0' cellspacing='0'>\n<tr>\n";
+	
+				foreach($columns as $col)
+				{
+					echo "<td valign='top'>\n";
+					echo $col;
+					echo "</td>\n";
+				}
+				echo "</tr>\n</table>\n";
+				echo "<!-- End of Bookmarks Module -->\n";
+			}
+		}		
+	}
+	
+	function DisplayAddBookmarkForm($id, $parameters, $returnid)
+	{
+		$db = $this->cms->db; /* @var $db ADOConnection */
+		
+		$categories = $this->GetCategories();
+		$bookmark = $this->GetBookmark(-1);
+	
+		$include_back_button = get_value_with_default(@$parameters['include_back_button'], false);
+		$email_to = get_value_with_default(@$parameters['email_to'], 0);
+		$email_from = get_value_with_default(@$parameters['email_from'], 0);
+	
+		$button_text = 'Add';
+		if($bookmark_id > 0)
+		{
+			$button_text = 'Update';
+		}
+	
+		echo $this->CreateFormStart($id, 'bookmarks_useradd', $returnid, $method='post', $enctype='');
+	
+		if($email_to > 0)
+		{
+			echo $this->CreateInputHidden($id, 'email_to', $email_to);
+		}
+		if($email_from > 0)
+		{
+			echo $this->CreateInputHidden($id, 'email_from', $email_from);
+		}
+	
+		echo $this->CreateInputHidden($id, 'bookmark_id', $bookmark_id);
+		echo $this->CreateInputHidden($id, 'referer', $_SERVER['SCRIPT_NAME']);
+		echo <<<EOT
+	
+	<table border=0 cellpadding=5 cellspacing=0>
+	    <tr>
+	        <td align="right">Title:</td>
+	        <td colspan=3>
+EOT;
+		echo $this->CreateInputText($id, 'bookmark_title', $bookmark['bookmark_title'], 50);
+	    echo <<<EOT
+	        </td>
+	    </tr>
+	    <tr>
+	        <td align="right">Address:</td>
+	        <td colspan=3>
+EOT;
+		echo $this->CreateInputText($id, 'bookmark_url', $bookmark['bookmark_url'], 50);
+	    echo <<<EOT
+			</td>
+	    </tr>
+	    <tr>
+	        <td align="right">Summary:</td>
+	        <td colspan=3>
+EOT;
+		echo $this->CreateInputText($id, 'bookmark_summary', $bookmark['bookmark_summary'], 50);
+	    echo <<<EOT
+			</td>
+	    </tr>
+	    <tr>
+	        <td align="right">Categories:</td>
+	        <td>
+EOT;
+		$num_cats = count($categories);
+		$num_cols = 2;
+		$rows_per_col = intval($num_cats / $num_cols);
+		$count = 0;
+		for($i = 0; $i < $num_cats; $i++,$count ++)
+		{
+			if($count >= $rows_per_col)
+			{
+				$count = 0;
+				echo "</td><td valign='top' style='padding-left: 40px;' >\n";
+				$padding = 0;
+			}
+	
+			if($i < $num_cats)
+			{
+				$category = $categories[$i];
+				$cat_id = $category['category_id'];
+				$cat_name = $category['category_name'];
+				echo "<div>";
+				echo $this->CreateInputCheckbox($id, 'bookmark_categories[]', $cat_id);
+				echo $cat_name; 
+				echo "</div>\n";
+			}
+		}
+		echo <<<EOT
+	        </td>
+	    </tr>
+	    <tr>
+	        <td colspan=4 align="right">
+	
+EOT;
+	
+		if($include_back_button)
+		{
+			echo <<<EOT
+				<input type="button" value="Cancel" onclick='javascript:window.history.go(-1)' />
+	
+EOT;
+		}
+	
+		echo $this->CreateInputSubmit($id, '', 'Submit');
+		echo <<<EOT
+	        </td>
+	    </tr>
+	</table>
+
+EOT;
+		echo $this->CreateFormEnd();
+	
+	
+	}
+	
+	function UserAddBookmark($id, $parameters, $returnid)
+	{
+		$parameters['bookmark_approved']=0;
+		$this->AdminUpdateBookmarks($id, $parameters, $returnid);
+		$email_to = get_value_with_default(@$parameters['email_to'], 0);
+		if($email_to > 0)
+		{
+			// get email address to send email to:
+			$sql = 'SELECT email FROM ' .cms_db_prefix().'users WHERE user_id=' . $email_to;
+			$rs = $this->cms->db->SelectLimit($sql, 1);
+			$email_to = '';
+			if($rs)
+			{
+				$email_to = $rs->fields['email'];
+			}
+			if($email_to != '')
+			{
+				// get from email address.
+				$email_from = get_value_with_default(@$parameters['email_from'], 0);
+				if($email_from <= 0)
+				{
+					// none set - pick up first email we can find.
+					$email_from = 'bookmarksmodule@example.com';
+					$sql = 'SELECT email FROM ' .cms_db_prefix().'users WHERE email IS NOT NULL ORDER BY user_id ASC';
+					$rs = $this->cms->db->SelectLimit($sql, 1);
+					if($rs)
+						$email_from = $rs->fields['email'];
+				}
+		
+				$subject = 'New Bookmark Submitted';
+				$body = "A new bookmark has been submitted.\n\n";
+				$body .= "Title: {$parameters[$module_id.'title']}\n";
+				$body .= "Url: {$parameters[$module_id.'url']}\n";
+				$body .= "Summary: {$parameters[$module_id.'summary']}\n";
+		
+				$headers = "From: \"CMS Boomarks Module\" <$email_from>\r\n"
+		    	    ."Return-Path: \"CMS Boomarks Module\" <$email_from>\r\n"
+			        ."X-Mailer: PHP/" . phpversion();
+			        
+			    $result = @mail($email_to, $subject, $body, $headers);
+			}
+		}
+
+		$link = $this->CreateReturnLink($id, $returnid, 'Return');
+		echo "<p class='cms-module-bookmarks-submitted'>Thank you for your submission. $link.</p>";
+		
+		
 	}
 	
 };
