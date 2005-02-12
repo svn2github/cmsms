f1285a0196f608863949ea61bb6380161c0d50d7
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-02-12 03:37:03 +0000

    Set lazy loading on for listcontent.php
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1293 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/admin/listcontent.php
+++ cms/trunk/admin/listcontent.php
@@ -38,7 +38,7 @@ if (isset($_GET["message"])) {
 
 	$modifyall = check_permission($userid, 'Modify Any Content');
 
-	$content_array = ContentManager::GetAllContent();
+	$content_array = ContentManager::GetAllContent(false);
 
 	if ($modifyall)
 	{
