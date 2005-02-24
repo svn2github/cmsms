a5cc6988393892dcd7288790fc004e2baf7f1d4a
akra <akra@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-02-24 23:30:05 +0000

    Remove check for the advanced CSS option
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1390 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/admin/listtemplates.php
+++ cms/trunk/admin/listtemplates.php
@@ -101,9 +101,7 @@ if (isset($_GET["message"])) {
 					echo "<td align=\"center\"><a href=\"listtemplates.php?action=setallcontent&amp;template_id=".$onetemplate->id."\" onclick=\"return confirm('".lang('setallcontentconfirm')."');\">".lang('setallcontent')."</a></td>\n";
 
 				# view css association
-				if (get_site_preference('useadvancedcss') == "1") {
-					echo "<td width=\"16\"><a href=\"listcssassoc.php?type=template&amp;id=".$onetemplate->id."\"><img src=\"../images/cms/css.gif\" width=\"16\" height=\"16\" border=\"0\" alt=\"".lang("CSS")."\" title=\"".lang("CSS")."\" /></a></td>\n";
-				}
+				echo "<td width=\"16\"><a href=\"listcssassoc.php?type=template&amp;id=".$onetemplate->id."\"><img src=\"../images/cms/css.gif\" width=\"16\" height=\"16\" border=\"0\" alt=\"".lang("CSS")."\" title=\"".lang("CSS")."\" /></a></td>\n";
 
 				# add new template
 				if ($add)
