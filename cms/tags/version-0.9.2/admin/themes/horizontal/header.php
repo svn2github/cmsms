d54b8ab9a6eb281b5b7228f74a0ec24b124ec65d
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-02-25 14:39:23 +0000

    -- Release 0.9.2 --
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1402 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ cms/tags/version-0.9.2/admin/themes/horizontal/header.php
@@ -0,0 +1,6 @@
+<div id="header" class="header">&nbsp;</div>
+<div id="sloganWrapper"><div id="slogan">CMS Made Simple -- <?php echo lang('slogan'); ?></div></div>
+<?php
+include_once("themes/horizontal/menu.php");
+?>
+<div class="content">
