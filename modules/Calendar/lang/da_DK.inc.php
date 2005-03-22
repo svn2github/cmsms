490938ed22ef3ad0c3e985940adb7af35bfac905
akra <akra@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-03-22 18:10:18 +0000

    Add Danish translation from esmann
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1450 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ modules/Calendar/lang/da_DK.inc.php
@@ -0,0 +1,14 @@
+<?php
+# Simple Language file for the Calendar module.
+# Copyright (c) 2004 by Rob Allen <rob@akrabat.com>
+
+# Language: da_DK
+
+$calendar_language_strings = array();
+$calendar_language_strings['Date'] = 'Dato';
+$calendar_language_strings['Details'] = 'Detaljer';
+$calendar_language_strings['Summary'] = 'Oversigt';
+$calendar_language_strings['Return'] = 'Tilbage';
+$calendar_language_strings['to'] = 'til';
+
+?>
