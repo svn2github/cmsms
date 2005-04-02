a8e9e7d0f3b5a546a431f495eb2fc2a25f6e7292
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-04-02 21:22:48 +0000

    Content titles and menu text have htmlentities encoded.  Fixes #000113.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1516 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/lib/contenttypes/Link.inc.php
+++ cms/trunk/lib/contenttypes/Link.inc.php
@@ -118,9 +118,9 @@ class link extends ContentBase
 	{
 		$text = "";
 
-		$text .= '<tr><th>'.lang('title').':</th><td><input type="text" name="title" value="'.$this->mName.'" /></td></tr>';
-		$text .= '<tr><th>'.lang('menutext').':</th><td><input type="text" name="menutext" value="'.$this->mMenuText.'" /></td></tr>';
-		$text .= '<tr><th>'.lang('url').':</th><td><input type="text" name="url" value="'.$this->GetPropertyValue('url').'" /></td></tr>';
+		$text .= '<tr><th>'.lang('title').':</th><td><input type="text" name="title" value="'.htmlentities($this->mName).'" /></td></tr>';
+		$text .= '<tr><th>'.lang('menutext').':</th><td><input type="text" name="menutext" value="'.htmlentities($this->mMenuText).'" /></td></tr>';
+		$text .= '<tr><th>'.lang('url').':</th><td><input type="text" name="url" value="'.htmlentities($this->GetPropertyValue('url')).'" /></td></tr>';
 		$text .= '<tr><th>'.lang('active').':</th><td><input type="checkbox" name="active"'.($this->mActive?' checked="checked"':'').' /></td></tr>';
 		$text .= '<tr><th>'.lang('showinmenu').':</th><td><input type="checkbox" name="showinmenu"'.($this->mShowInMenu?' checked="checked"':'').' /></td></tr>';
 		$text .= '<tr><th>'.lang('parent').':</th><td>'.ContentManager::CreateHierarchyDropdown($this->mId, $this->mParentId).'</td></tr>';
