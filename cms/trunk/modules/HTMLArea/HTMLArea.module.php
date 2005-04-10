d1cc2f3e201dd1c591f8828081582a811d4ac171
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-04-10 01:38:19 +0000

    Make help text for HTMLArea into lang file
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1540 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/modules/HTMLArea/HTMLArea.module.php
+++ cms/trunk/modules/HTMLArea/HTMLArea.module.php
@@ -197,12 +197,7 @@ class HTMLArea extends CMSModule
 
 	function GetHelp($lang='en_US')
 	{
-		return "
-		<h3>What does this do?</h3>
-    <p>Enables HTMLArea to be used as a WYSIWYG.</p>
-    <h3>How do I use it?</h3>
-    <p>Install it, then go to User Preferences and Set HTMLArea  to be your wysiwyg of choice.</p>
-		";
+		return $this->Lang('help');
 	}
 
 	function GetAuthor()
