af9bfcd3cc4f4134f6b6cf6933ef839d1a6f4f20
sjg <sjg@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-04-20 05:53:26 +0000

    Fixed login style path
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1583 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/admin/loginstyle.php
+++ cms/trunk/admin/loginstyle.php
@@ -1,17 +1,11 @@
 <?php
-
-//CHANGED
 Header ("Content-type: text/css");
 require_once("../include.php");
 $theme=get_site_preference('logintheme', 'default');
 //
-if (file_exists(dirname(__FILE__)."/themes/".$theme."/style.css")) {  
-	readfile(dirname(__FILE__)."/themes/".$theme."/style.css");
-	//include(dirname(__FILE__)."/themes/".$theme."/style.css");
+if (file_exists(dirname(__FILE__)."/themes/".$theme."/css/style.css")) {  
+	readfile(dirname(__FILE__)."/themes/".$theme."/css/style.css");
 } else {
-	readfile(dirname(__FILE__)."/themes/default/style.css");
-	//include(dirname(__FILE__)."/themes/default/style.css");
+	readfile(dirname(__FILE__)."/themes/default/css/style.css");
 }
-//STOP
-
 ?>
