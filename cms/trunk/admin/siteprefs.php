0e7b368253c800bdd6a1c5326477c7fdc3e64c74
sjg <sjg@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-05-06 01:11:18 +0000

    Admin preference update now leaves you on the admin preference page
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1620 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/admin/siteprefs.php
+++ cms/trunk/admin/siteprefs.php
@@ -87,8 +87,9 @@ else if (isset($_POST["editsiteprefs"]))
 		#set_site_preference('useadvancedcss', $useadvancedcss);
 		set_site_preference('logintheme', $logintheme);
 		audit(-1, '', 'Edited Site Preferences');
-		redirect("index.php");
-		return;
+		//redirect("siteprefs.php");
+		//return;
+		$error .= "<li>".lang('prefsupdated').'</li>';
 	}
 	else
 	{
