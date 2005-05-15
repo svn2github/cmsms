137645dfd5eee148ee38db8da92a972414032334
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-05-15 16:06:10 +0000

    Added pagecheckbox to checkboxes
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1666 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/modules/News/News.module.php
+++ cms/trunk/modules/News/News.module.php
@@ -748,7 +748,7 @@ class News extends CMSModule
 				$this->smarty->assign_by_ref('inputsummary', $this->CreateTextArea(false, $id, $summary, 'summary', '', '', '', '', '80', '3'));
 				$this->smarty->assign_by_ref('postdate', $postdate);
 				$this->smarty->assign('postdateprefix', $id.'postdate_');
-				$this->smarty->assign_by_ref('inputexp', $this->CreateInputCheckbox($id, 'useexp', '1', $useexp));
+				$this->smarty->assign_by_ref('inputexp', $this->CreateInputCheckbox($id, 'useexp', '1', $useexp, 'class="pagecheckbox"));
 				$this->smarty->assign_by_ref('startdate', $startdate);
 				$this->smarty->assign('startdateprefix', $id.'startdate_');
 				$this->smarty->assign_by_ref('enddate', $enddate);
@@ -891,7 +891,7 @@ class News extends CMSModule
 				$this->smarty->assign_by_ref('inputtitle', $this->CreateInputText($id, 'title', $title, 30, 255));
 				$this->smarty->assign_by_ref('inputcontent', $this->CreateTextArea(false, $id, $content, 'content'));
 				$this->smarty->assign_by_ref('inputsummary', $this->CreateTextArea(false, $id, $summary, 'summary', '', '', '', '', '80', '3'));
-				$this->smarty->assign_by_ref('inputexp', $this->CreateInputCheckbox($id, 'useexp', '1', $useexp));
+				$this->smarty->assign_by_ref('inputexp', $this->CreateInputCheckbox($id, 'useexp', '1', $useexp, 'class="pagecheckbox"));
 				$this->smarty->assign_by_ref('postdate', $postdate);
 				$this->smarty->assign('postdateprefix', $id.'postdate_');
 				$this->smarty->assign_by_ref('startdate', $startdate);
