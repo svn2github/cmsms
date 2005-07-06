7e3fe5afb9bf94840f7f83b0d8703f6521796e3a
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-07-06 01:08:37 +0000

    Last time
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1826 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/tags/version-0.10/modules/HTMLArea/lang/da_DK.php
+++
@@ -1,8 +0,0 @@
-<?php
-$lang['help'] = <<<EOF
-	<h3>Hvad gør dette module?</h3>
-	<p>Tillader at HTMLArea kan bruges som WYSIWYG-editor.</p>
-	<h3>Hvordan bruger jeg det?</h3>
-	<p>Installér det, gå til bruger indstillinger og vælg HTMLArea som det ønskede WYSIWYG-system.</p>
-EOF;
-?>
--- cms/tags/version-0.10/modules/HTMLArea/lang/en_US.php
+++
@@ -1,8 +0,0 @@
-<?php
-$lang['help'] = <<<EOF
-	<h3>What does this do?</h3>
-	<p>Enables HTMLArea to be used as a WYSIWYG.</p>
-	<h3>How do I use it?</h3>
-	<p>Install it, then go to User Preferences and Set HTMLArea  to be your wysiwyg of choice.</p>
-EOF;
-?>
