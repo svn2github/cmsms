61151d738ab36c67731499d64720bdc88ed4168a
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-07-06 00:01:17 +0000

    Remove and try again
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1823 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/tags/version-0.10/modules/HTMLArea/lang/es_MX.php
+++
@@ -1,8 +0,0 @@
-<?php
-$lang['help'] = <<<EOF
-	<h3>¿Que hace esto?</h3>
-	<p>Habilita a HTMLArea como editor WYSIWYG de tu pagina.</p>
-	<h3>¿Como lo uso?</h3>
-	<p>Instalalo, entonces ve a las preferencias de usuario y elige a HTMLArea como tu editor wysiwyg.</p>
-EOF;
-?>
