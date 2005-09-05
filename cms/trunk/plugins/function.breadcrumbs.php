fee6d91d929439af5591fa7a4b1455e599249430
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-09-05 13:50:55 +0000

    Updated breakcrumbs plugin
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1900 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/plugins/function.breadcrumbs.php
+++ cms/trunk/plugins/function.breadcrumbs.php
@@ -15,9 +15,9 @@
 #You should have received a copy of the GNU General Public License
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 
-function smarty_cms_function_breadcrumbs($params, &$smarty)
-{
+function smarty_cms_function_breadcrumbs($params, &$smarty) {
 	global $gCms; 
 
 	$thispage = $gCms->variables['page'];
@@ -25,20 +25,15 @@ function smarty_cms_function_breadcrumbs($params, &$smarty)
 	$trail = "";
 
 	#Check if user has specified a delimiter, otherwise use default
-	if (isset($params['delimiter'])) 
-	{
+	if (isset($params['delimiter'])) {
 		$delimiter = $params['delimiter'];
-	}
-	else
-	{
+	} else {
 		$delimiter = "&gt;&gt;";
 	}
 
 	#Check if user has requested an initial delimiter
-	if (isset($params['initial'])) 
-	{
-		if ($params['initial'] == "1")
-		{
+	if (isset($params['initial']))  {
+		if ($params['initial'] == "1") 	{
 			$trail .= $delimiter . " ";
 		}
 	}
@@ -50,65 +45,60 @@ function smarty_cms_function_breadcrumbs($params, &$smarty)
 	$onecontent = ContentManager::LoadContentFromId($thispage, false);
 
 	#Check if user has requested the list to start with a specific page
-	if (isset($params['root'])) 
-	{
-		if (strtolower($onecontent->Alias()) != strtolower($params['root']))
-		{
+	if (isset($params['root'])) {
+		if (strtolower($onecontent->Alias()) != strtolower($params['root'])) {
 			$rootcontent = ContentManager::LoadContentFromAlias($params['root'], false);
-			if ($rootcontent) 
-			{
-				$trail .= "<a href=\"".$rootcontent->getURL()."\">".$rootcontent->MenuText()."</a> ".$delimiter." ";
+			if ($rootcontent) {
+				// tdh add / modify next 5 lines:
+				if (isset($params['classid'])) {
+					$trail .= "<a href=\"" . $rootcontent->getURL() . "\" class=\"" . $params['classid'] . "\">".$rootcontent->MenuText()."</a> ".$delimiter." ";
+				} else {
+					$trail .= "<a href=\"".$rootcontent->getURL()."\">".$rootcontent->MenuText()."</a> ".$delimiter." ";
+				}
 			}
 		}
 	}
-	
-	if ($onecontent !== FALSE)
-	{
+
+	if ($onecontent !== FALSE) {
 		array_push($allcontent, $onecontent);
 
 		#Grab all parents and put them into the array as well
-		while ($onecontent->ParentId() > 0) 
-		{
+		while ($onecontent->ParentId() > 0) {
 			$onecontent = ContentManager::LoadContentFromId($onecontent->ParentId(), false);
-			array_push($allcontent, $onecontent);
+			// tdh add / modify next 5 lines
+			if (isset($params['root'])) {
+				if (strtolower($onecontent->Alias()) != strtolower($params['root'])) {
+					array_push($allcontent, $onecontent);
+				}
+			}
 		}
 
 		#Pull them one by one in reverse order to construct a breadcrumb list
-		while ($onecontent = array_pop($allcontent))
-		{
-			if ($onecontent->Id() != $thispage && $onecontent->Type() != 'seperator')
-			{
-				if ($onecontent->Type() == 'sectionheader')
-				{
-					if (getURL($thispage)!="")
-					{
+		while ($onecontent = array_pop($allcontent)) {
+			if ($onecontent->Id() != $thispage && $onecontent->Type() != 'seperator') {
+				if ($onecontent->Type() == 'sectionheader') {
+					if (getURL($thispage)!="") {
+						$trail .= $onecontent->MenuText()." ".$delimiter."\n";
+					} else {
 						$trail .= $onecontent->MenuText()." ".$delimiter."\n";
 					}
-					else
-					{
-						$trail .= $onecontent->MenuText()." ".$delimiter."\n";
-					}
-				}
-				else
-				{
-					if (getURL($thispage)!="")
-					{
-						$trail .= "<a href=\"".$onecontent->getURL()."\">".($onecontent->MenuText()!=''?$onecontent->MenuText():$onecontent->Name())."</a> ".$delimiter."\n";
-					}
-					else
-					{
+				} else {
+					if (getURL($thispage)!="") {
+						// tdh add / modify next 5 lines
+						if (isset($params['classid'])) {
+							$trail .= "<a href=\"".$onecontent->getURL()."\" class=\"" . $params['classid'] . "\">".($onecontent->MenuText()!=''?$onecontent->MenuText():$onecontent->Name())."</a> ".$delimiter."\n";
+						} else {
+							$trail .= "<a href=\"".$onecontent->getURL()."\">".($onecontent->MenuText()!=''?$onecontent->MenuText():$onecontent->Name())."</a> ".$delimiter."\n";
+						}
+					} else {
 						$trail .= ($onecontent->MenuText()!=''?$onecontent->MenuText():$onecontent->Name())." ".$delimiter."\n";
 					}
 				}
-			}
-			else
-			{
-				$trail .= "<strong>".($onecontent->MenuText()!=''?$onecontent->MenuText():$onecontent->Name())."</strong>\n";
+			} else {
+				$trail .= ($onecontent->MenuText()!=''?$onecontent->MenuText():$onecontent->Name())."\n";
 			}
 		}
-	}
-	else
-	{
+	} else {
 		$trail = "No pages";
 	}
 
@@ -117,34 +107,37 @@ function smarty_cms_function_breadcrumbs($params, &$smarty)
 }
 
 function smarty_cms_help_function_breadcrumbs() {
-	?>
-	<h3>What does this do?</h3>
-	<p>Prints a breadcrumb trail .</p>
-	<h3>How do I use it?</h3>
-	<p>Just insert the tag into your template/page like: <code>{breadcrumbs}</code></p>
-	<h3>What parameters does it take?</h3>
-	<p>
-	<ul>
-		<li><em>(optional)</em> <tt>delimiter</tt> - Text to seperate entries in the list (default "&gt;&gt;").</li>
-		<li><em>(optional)</em> <tt>initial</tt> - 1/0 If set to 1 start the breadcrumbs with a delimiter (default 0).</li>
-		<li><em>(optional)</em> <tt>root</tt> - Page alias of a page you want to always appear as the first page in
-		    the list. Can be used to make a page (e.g. the front page) appear to be the root of everything even though it is not.</li>
-	</ul>
-	</p>
-	<?php
+// tdh added the classid help text
+?>
+<h3>What does this do?</h3>
+<p>Prints a breadcrumb trail .</p>
+<h3>How do I use it?</h3>
+<p>Just insert the tag into your template/page like: <code>{breadcrumbs}</code></p>
+<h3>What parameters does it take?</h3>
+<p>
+<ul>
+<li><em>(optional)</em> <tt>delimiter</tt> - Text to seperate entries in the list (default "&gt;&gt;").</li>
+<li><em>(optional)</em> <tt>initial</tt> - 1/0 If set to 1 start the breadcrumbs with a delimiter (default 0).</li>
+<li><em>(optional)</em> <tt>root</tt> - Page alias of a page you want to always appear as the first page in
+    the list. Can be used to make a page (e.g. the front page) appear to be the root of everything even though it is not.</li>
+<li><em>(optional)</em> <tt>classid</tt> - The CSS class for the <a href> tags.</li>
+</ul>
+</p>
+<?php
 }
 
 function smarty_cms_about_function_breadcrumbs() {
-	?>
-	<p>Author: Marcus Deglos &lt;<a href="mailto:md@zioncore.com">md@zioncore.com</a>&gt;</p>
-	<p>Version: 1.2</p>
-	<p>
-	Change History:<br/>
-	1.1 - Modified to use new content rewrite (wishy)<br />
-	1.2 - Added parameters: delimiter, initial, and root (arl)<br />
-	</p>
-	<?php
+?>
+<p>Author: Marcus Deglos &lt;<a href="mailto:md@zioncore.com">md@zioncore.com</a>&gt;</p>
+<p>Version: 1.2</p>
+<p>
+Change History:<br/>
+1.1 - Modified to use new content rewrite (wishy)<br />
+1.2 - Added parameters: delimiter, initial, and root (arl)<br />
+1.3 - Added parameter: classid (tdh / perl4ever)<br />
+</p>
+<?php
 }
-
 # vim:ts=4 sw=4 noet
 ?>
+
