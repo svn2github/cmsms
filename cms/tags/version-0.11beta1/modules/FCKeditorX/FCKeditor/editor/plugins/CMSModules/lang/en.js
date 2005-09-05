32de628414eca298e45ab653c0c3efb7ab1204a8
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-09-05 16:47:02 +0000

    Need to redo this
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1908 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/tags/version-0.11beta1/modules/FCKeditorX/FCKeditor/editor/plugins/CMSModules/lang/en.js
+++
@@ -1,4 +0,0 @@
-FCKLang.CMSModulesBtn			= 'Insert/Edit CMS Content' ;
-FCKLang.CMSModulesDlgTitle		= 'CMS Link - Insert internal link' ;
-FCKLang.CMSModulesDlgName		= 'CMS Link insert' ;
-FCKLang.CMSModulesDlgSelection		= 'Please make a selection of text in order to create a link' ;
--- cms/tags/version-0.11beta1/modules/FCKeditorX/FCKeditor/editor/plugins/CMSModules/lang/nl.js
+++
@@ -1,4 +0,0 @@
-FCKLang.CMSContentBtn			= 'Interne CMS link toevoegen' ;
-FCKLang.CMSModulesDlgTitle		= 'CMS Link - voeg interne pagina link toe' ;
-FCKLang.CMSModulesDlgName		= 'CMS Link toevoegen' ;
-FCKLang.CMSModulesDlgSelection		= 'Eerst een selectie maken voordat u een link wilt toevoegen' ;
