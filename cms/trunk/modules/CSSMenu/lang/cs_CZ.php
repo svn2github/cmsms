038f695810ee1af37b33b380342c25f404ab4336
peca <peca@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-09-18 22:09:26 +0000

    an vsprintf error displaying listmodules.php in czech caused due an bad character in lang file
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1956 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/modules/CSSMenu/lang/cs_CZ.php
+++ cms/trunk/modules/CSSMenu/lang/cs_CZ.php
@@ -5,6 +5,7 @@ $lang['helpshowadmin'] = '1/0, zda chcete nebo nechcete zobrazit link na adminis
 $lang['helpstart_element'] = 'hierarchie položek (např.: 1.2 nebo 3.5.1). Tento parametr nastaví kořen menu.';
 $lang['helpnumber_of_levels'] = 'celé číslo, počet úrovní, které chcete zobrazit v menu.';
 $lang['helphorizontal'] = '1/0, pokud chcete radši horizontální menu místo vertikálního.';
+$lang['helprelative'] = '1/0, zda chcete v menu generovat pouze podpoložky aktuální stránky. Je to velmi užitečné pokud chcete menu citlivé na kontext.';
 $lang['help'] = <<<EOF
   <h3>Co dělá tento modul?</h3>
   <p>Vytváří vertikální standardům odpovídající CSS menu. Trochu JavaScriptu je potřebné pro IE.</p>
@@ -12,6 +13,6 @@ $lang['help'] = <<<EOF
   <h3>Jak se používá?</h3>
   <p>Stačí pouze vložit tento modul do Vaší šablony/stránky, např.: <code>{cms_module module='cssmenu'}</code></p>
   <p>Upravte /modules/CSSMenu/CSSMenu.css pro změnu stylu. Pro více informací viz články výše.</p>
-  <p>Upravte /modules/CSSMenu/CSSMenuHorizontal.css pro změnu stylu. Na řádce 14 nastavte  width: 600px; na vhodnou hodnotu pro šířku. (zkuste '100%')
+  <p>Upravte /modules/CSSMenu/CSSMenuHorizontal.css pro změnu stylu. Na řádce 14 nastavte  width: 600px; na vhodnou hodnotu pro šířku. (zkuste '100
 EOF;
 ?>
