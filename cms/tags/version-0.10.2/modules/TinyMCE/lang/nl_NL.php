dc582308e4b879514456c1917c863ebaf4cae9ea
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-09-24 18:08:28 +0000

    Setup for 0.10.2
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@1979 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ cms/tags/version-0.10.2/modules/TinyMCE/lang/nl_NL.php
@@ -0,0 +1,12 @@
+<?php
+$lang['stripbackgroundtags'] = 'Strip Background Tags From CSS';
+$lang['false'] = 'False';
+$lang['true'] = 'True';
+$lang['submit'] = 'Submit';
+$lang['help'] = <<<EOF
+	<h3>Wat doet het?</h3>
+	<p>Enables TinyMCE to be used as a WYSIWYG.</p>
+	<h3>Hoe gebruik ik het?</h3>
+	<p>Install it, then go to User Preferences and Set TinyMCE  to be your wysiwyg of choice.</p>
+EOF;
+?>
