b5d5072ff0f73ae39d3e26657da37032f146e03d
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-10-21 03:33:22 +0000

    Fix for section headers in bulletmenu.  Utter, that's for you.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@2058 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/plugins/function.bulletmenu.php
+++ cms/trunk/plugins/function.bulletmenu.php
@@ -235,7 +235,7 @@ function smarty_cms_function_bulletmenu($params, &$smarty) {
 			}
 			else if ($onecontent->Type() == 'sectionheader')
 			{
-				$menu .= '<li><a href="#" class="sectionheader">'.$onecontent->MenuText()."</a>\n";
+				$menu .= '<li class="bullet_sectionheader">'.$onecontent->MenuText()."\n";
 			}
 			else
 			{
