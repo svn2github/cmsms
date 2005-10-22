3c88258e6cde6aaeff8f956944e2bfffecb21eeb
peca <peca@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-10-22 16:22:07 +0000

    updated czech lang files for admin and News module
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@2067 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/modules/News/lang/cs_CZ.php
+++ cms/trunk/modules/News/lang/cs_CZ.php
@@ -14,11 +14,12 @@ $lang['dateformat'] = '%s není ve správném yyyy-mm-dd hh:mm:ss formátu';
 $lang['delete'] = 'Smazat';
 $lang['description'] = 'Vložit, upravit nebo smazat novinky';
 $lang['detailtemplate'] = 'Šablona detailu';
-$lang['displaytemplate'] = 'Display Template';
+$lang['displaytemplate'] = 'Zobrazit šablonu';
 $lang['edit'] = 'Upravit';
 $lang['enddate'] = 'Končí';
 $lang['endrequiresstart'] = 'Vložení data konce potřebuje také datum počátku';
 $lang['entries'] = '%s položek';
+$lang['status'] = 'Stav';
 $lang['expiry'] = 'Vyprší';
 $lang['filter'] = 'Filtr';
 $lang['more'] = 'Více';
@@ -39,6 +40,7 @@ $lang['postdate'] = 'Datum vložení';
 $lang['postinstall'] = 'Nastavte "Modify News" oprávnění uživatelům, kteří budou spravovat novinky.';
 $lang['rsstemplate'] = 'RSS šablona';
 $lang['selectcategory'] = 'Vybrat kategorii';
+$lang['showchildcategories'] = 'Zobrazit podřízené kategorie';
 $lang['sortascending'] = 'Třídit vzestupně';
 $lang['startdate'] = 'Začíná';
 $lang['startrequiresend'] = 'Vložení data začátku potřebuje také datum ukončení';
@@ -47,6 +49,15 @@ $lang['summary'] = 'Souhrn';
 $lang['summarytemplate'] = 'Šablona souhrnu';
 $lang['title'] = 'Nadpis';
 $lang['useexpiration'] = 'Použít datum konce';
+$lang['helpnumber'] = 'Maximální počet položek k zobrazení - ponecháno prázdné zobrazí všechny položky.';
+$lang['helpmakerssbutton'] = 'Vytvořit tlačítko s odkazem na RSS feed novinek.';
+$lang['helpcategory'] = 'Zobrazit pouze položky této kategorie a jejích podpoložek. Ponecháno prázdné zobrazí všechny kategorie.';
+$lang['helpmoretext'] = 'Text pro zobrazení na konci novinky pokud jde přes délku souhrnu. Základní je "more..."';
+$lang['helpsummarytemplate'] = 'Používat oddělenou šablonu pro souhrn.  Tato šablona má být v  modules/News/templates.';
+$lang['helpdetailtemplate'] = 'Používat oddělenou šablonu pro detail.  Tato šablona má být v  modules/News/templates.';
+$lang['helpsortby'] = 'Pole pro třídění. Možnosti jsou: "news_date", "summary", "news_data", "news_category", "news_title".  Defaults to "news_date".';
+$lang['helpsortasc'] = 'Třídit novinky vzestupně.';
+$lang['helpdateformat'] = 'Formát rozbrazení data novinky. Toto vychází z <a href="http://php.net/strftime" target="_blank">strftime</a> funkce a může být použit v šabloně s $entry->formatpostdate.';
 $lang['help'] = <<<EOF
 	<h3>Co dělá tento modul?</h3>
 	<p>Novinky jsou modul pro zobrazování událostí na Vašich stránkách, podobně jako blogové zápisy, pouze s více možnostmi! Po instalaci modulu je vložena další položka do administračního menu a to Vám umožní vybrat nebo vložit kategorii novinek. Po vytvoření nebo zvolení kategorie je zobrazen seznam novinek pro tuto kategorii. Odsud můžete přidávat, upravovat nebo mazat novinky dané kategorie.</p>
@@ -54,18 +65,5 @@ $lang['help'] = <<<EOF
 	<p>Aby uživatel mohl přidávat, upravovat nebo mazat novinky, musí mít oprávnění skupiny 'Modify News'.</p>
 	<h3>Jak se používá?</h3>
 	<p>Nejjednodušší použití je ve využití tagu cms_module. Toto vloží modul do šablony nebo stránky a zobrazí novinky jak si přejete. Samotný kó bude vypadat nějak následovně: <code>{cms_module module="news" number="5" category="beer"}</code></p>
-	<h3>Jaké jsou parametry?</h3>
-	<p>
-	<ul>
-	<li><em>(volitelné)</em> number="5" - Maximání počet položek k zobrazení =- ponecháno prázdné zobrazí všechny položky</li>
-	<li><em>(volitelné)</em> makerssbutton="true" - Vytvořit tlačítko odkazující na  RSS zdroj novinek.</li>
-	<li><em>(volitelné)</em> category="category" - Zobrazit položky pouze dané kategorie. Ponecháno prázdné zobrazí všechny kategorie</li>
-	<li><em>(volitelné)</em> moretext="more..." - Text zobrazený na konci novinky, pokud překračuje délku souhrnu. Standardně "more...".</li>
-	<li><em>(volitelné)</em> summarytemplate="sometemplate.tpl" - Použít zvláštní šablonu pro zobrazení souhrnu. Má být v modules/News/templates.
-	<li><em>(volitelné)</em> detailtemplate="sometemplate.tpl" - Použít zvláštní šablonu pro zobrazení detailu.  Má být v modules/News/templates.
-	<li><em>(volitelné)</em> sortby="news_date" - Pole pro třídění. Volby jsou: "news_date", "summary", "news_data", "news_category", "news_title".  Základní je "news_date".</li>
-	<li><em>(volitelné)</em> sortasc="true" - Seřadit novinky vzestupně místo sestupně.</li>
-	</ul>
-	</p>
 EOF;
 ?>
