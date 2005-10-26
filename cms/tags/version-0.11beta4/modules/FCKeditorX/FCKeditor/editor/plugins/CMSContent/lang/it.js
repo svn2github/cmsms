84b51542848dbfb239d79784b87e7684448adb08
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-10-26 01:22:07 +0000

    Going to redo beta4
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@2091 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/tags/version-0.11beta4/modules/FCKeditorX/FCKeditor/editor/plugins/CMSContent/lang/it.js
+++
@@ -1,4 +0,0 @@
-FCKLang.CMSContentBtn                   = 'Inserisci/Edita Contenuto CMS' ;
-FCKLang.CMSContentDlgTitle              = 'CMS Link - Inserisci link interno' ;
-FCKLang.CMSContentDlgName               = 'CMS Link inserimento' ;
-FCKLang.CMSContentDlgSelection          = 'Perfavore seleziona del testo in modo da applicare il link' ;
--- cms/tags/version-0.11beta4/modules/HTMLArea/lang/nb_NO.php
+++
@@ -1,8 +0,0 @@
-<?php
-$lang['help'] = <<<EOF
-	<h3>Hva gjør denne modulen?</h3>
-	<p>Denne modulen gjør at HTMLArea kan benyttes som WYSIWYG tekstbehandler</p>
-	<h3>Hvordan bruker jeg modulen?</h3>
-	<p>Installer den, gå deretter til System -> Brukervalg og velg HTMLArea som din WYSIWYG tekstbehandler.</p>
-EOF;
-?>
