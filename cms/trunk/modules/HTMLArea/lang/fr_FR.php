09d7a92ad334a5e143e23f8cac8d02d0ea7ba4af
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-11-01 19:35:41 +0000

    Removed HTMLArea from the trunk
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@2107 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/modules/HTMLArea/lang/fr_FR.php
+++
@@ -1,8 +0,0 @@
-<?php
-$lang['help'] = <<<EOF
-	<h3>Que fait ce module?</h3>
-	<p>Active HTMLArea pour être utilisé en tant que WYSIWYG.</p>
-	<h3>Comment l'utiliser?</h3>
-	<p>Installez le module, puis allez à "Préférences utilisateur" et définissez HTMLArea comme wysiwyg de votre choix.</p>
-EOF;
-?>
