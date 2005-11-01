cef31289d4d435ae4795b8ebaa4cd8ddeebb2358
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-11-01 16:32:48 +0000

    Removed FCKeditorX from the repository
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@2105 3d254a34-79dc-0310-9e5f-be208747d8a0

--- cms/trunk/modules/FCKeditorX/FCKeditor/editor/plugins/CMSContent/lang/en.js
+++
@@ -1,4 +0,0 @@
-﻿FCKLang.CMSContentBtn			= 'Insert/Edit CMS Content' ;
-FCKLang.CMSContentDlgTitle		= 'CMS Link - Insert internal link' ;
-FCKLang.CMSContentDlgName		= 'CMS Link insert' ;
-FCKLang.CMSContentDlgSelection		= 'Please make a selection of text in order to create a link' ;
