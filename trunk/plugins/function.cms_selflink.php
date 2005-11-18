e1d732667eaeddcdc744f47575090e4bce763c0a
westis <westis@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-11-18 00:39:33 +0000

    Replaced cms_selflink with Silmarillion's Improved version of cms_selflink
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@2190 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/plugins/function.cms_selflink.php
+++ trunk/plugins/function.cms_selflink.php
@@ -25,6 +25,7 @@ function smarty_cms_function_cms_selflink($params, &$smarty) {
 	if (isset($params['page']))
 	{
 		$page = $params['page'];
+		$name = $params['page']; //mbv - 21-06-2005
 
 		# check if the page exists in the db
 		$content = ContentManager::LoadContentFromAlias($page);
@@ -32,47 +33,124 @@ function smarty_cms_function_cms_selflink($params, &$smarty) {
 		{
 			$pageid = $content->Id();
 			$alias = $content->Alias();
+			$name = $content->Name(); //mbv - 21-06-2005 
 			$url = $content->GetUrl();
 		}
+			$Prev_label = "";
+			$Next_label = "";
 	}
-	
+	elseif (isset($params['dir'])) // this part is by mbv built on a proposal by 100rk
+	{
+		if (isset($params['lang'])){
+			switch (strtolower($params['lang']))
+			{
+				case 'dk':
+				case 'da':
+					$Prev_label = "Forrige side: ";
+					$Next_label = "N&aelig;ste side: ";
+					break;
+				case 'en':
+					$Prev_label = "Previous page: ";
+					$Next_label = "Next page: ";
+					break;
+				case '0':
+					$Prev_label = "";
+					$Next_label = "";
+					break;
+				default:
+					$Prev_label = "Previous page: ";
+					$Next_label = "Next page: ";
+			}
+		}
+		else
+		{
+			$Prev_label = "Previous page: ";
+			$Next_label = "Next page: ";
+		}
+		$condition = $order_by = false;
+		switch (strtolower($params['dir']))
+		{
+			case 'next':
+				$condition = '>';
+				$order_by = 'hierarchy';
+				$label=$Next_label;
+				break;
+			case 'prev':
+			case 'previous':
+				$condition = '<';
+				$order_by = 'hierarchy DESC';
+				$label=$Prev_label;
+				break;
+		}
+		if ($condition && $order_by)
+		{
+			$query  = "SELECT DISTINCT t1.content_id, t1.content_alias, t1.content_name, t1.menu_text ";
+			$query .= "FROM ".cms_db_prefix()."content AS t1, ".cms_db_prefix()."content AS t2 ";
+			$query .= "WHERE t1.active = 1 ";
+			$query .= "AND t1.type = 'content' "; // Added by Teemu Koistinen
+			$query .= "AND t2.content_id = ".$gCms->variables['content_id']." ";
+			$query .= "AND t1.hierarchy ".$condition." t2.hierarchy ";
+			$query .= "ORDER BY t1.".$order_by." LIMIT 1";
+			$dbresult = $db->Execute($query);
+			if ($dbresult && $dbresult->RowCount() > 0)
+			{
+				while ($row = $dbresult->FetchRow())
+				{
+					$content = new content();
+					$pageid = $content->mId = $row['content_id'];
+					$alias = $content->mAlias = $row['content_alias'];
+					$name = $content->mName = $row['content_name'];
+					$menu_text = $content->mMenu_text = $row['menu_text'];
+					$url = $content->GetUrl();
+					unset($content);
+				}
+			}
+		}
+		unset($condition);
+		unset($order_by);
+	} // end of next-prev code
 	if (isset($alias) && $alias != "") {
 		if ($config["assume_mod_rewrite"]) 
-			$url = $config["root_url"]."/".$alias.$config["page_extension"];
+			$url = $config["root_url"]."/".$alias.".shtml";
 		else 
 			$url = $config["root_url"]."/index.php?".$config["query_var"]."=".$alias;
 	} else if (isset ($pageid)) {
 		if ($config["assume_mod_rewrite"])
-			$url = $config["root_url"]."/".$pageid.$config["page_extension"];
+			$url = $config["root_url"]."/".$pageid.".shtml";
 		else 
 			$url = $config["root_url"]."/index.php?".$config["query_var"]."=".$pageid;
 	} else {
 		$url="";
 	}
 
-	if ($url != '') {
-		$result = '';
-		$anchor = '';
-		if (isset($params['anchor'])) {
-			$anchor .= '#'.$params['anchor'];
-		}
-		$result .= '<a href="'.$url.$anchor.'"';
+
+	if ($url != "") {
+		$result = "";
+		$result .= $label.'<a href="'.$url.'"';
 
 		if (isset($params['target'])) {
 			$result .= ' target="'.$params['target'].'"';
 		}
+		
 		if (isset($params['id'])) {
 			$result .= ' id="'.$params['id'].'"';
 		}
+
 		if (isset($params['class'])) {
 			$result .= ' class="'.$params['class'].'"';
+		}
+
+		if (isset($params['more'])) {
+			$result .= ' '.$params['more'];
 		}
 		$result .= '>';
 		
 		if (isset($params['text']))	{
 			$result .= $params['text'];
+		} elseif (isset($params['menu']) && $params['menu'] == "1")	{ // mbv 
+			$result .= $menu_text;
 		} else {
-			$result .= $params['page'];
+			$result .= $name; // mbv - 21-06-2005 
 		}
 
 		$result .= '</a>';
@@ -93,12 +171,16 @@ function smarty_cms_help_function_cms_selflink() {
 	<h3>What parameters does it take?</h3>
 	<p>
 	<ul>
-		<li><tt>page</tt> - Page ID or alias to link to.</li>
-		<li><em>(optional)</em> <tt>text</tt> - Text to show for the link.  If not given, the page variable is used instead.</li>
-		<li><em>(optional)</em> <tt>anchor</tt> - Will make the link go to a particular anchor on the target page.</li>
+		<li><em>(optional)</em> <tt>page</tt> - Page ID or alias to link to.</li>
+		<li><em>(optional)</em> <tt>dir next/prev (previous)</tt> - If this is used <tt>page</tt> should not be set.</li> <!-- mbv - 21-06-2005 -->
+			<B>Note!</B> Only one of the above may be used in the same cms_selflink statement!!
+		<li><em>(optional)</em> <tt>text</tt> - Text to show for the link.  If not given, the Page Name is used instead.</li>
+		<li><em>(optional)</em> <tt>menu 1/0</tt> - If 1 the Menu Text is used for the link text in stead of the Page Name</li> <!-- mbv - 21-06-2005 -->
 		<li><em>(optional)</em> <tt>target</tt> - Optional target for the a link to point to.  Useful for frame and javascript situations.</li>
-		<li><em>(optional}</em> <tt>class</tt> - Optional class for the a tag.
-		<li><em>(optional}</em> <tt>id</tt> - Optional id for the a tag.
+		<li><em>(optional)</em> <tt>class</tt> - Class for the &lt;a&gt; link.  Useful for styling the link.</li> <!-- mbv - 21-06-2005 -->
+		<li><em>(optional)</em> <tt>lang</tt> - Display link-labels  ("Next Page"/"Previous Page") in different languages (0 for no label.) Danish (dk) or English (en), for now.</li> <!-- mbv - 21-06-2005 -->
+		<li><em>(optional)</em> <tt>id</tt> - Optional css_id for the &lt;a&gt; link.</li> <!-- mbv - 29-06-2005 -->
+		<li><em>(optional)</em> <tt>more</tt> - place additional options inside the &lt;a&gt; link.</li> <!-- mbv - 29-06-2005 -->
 	</ul>
 	</p>
 
@@ -109,8 +191,20 @@ function smarty_cms_about_function_cms_selflink() {
 	?>
 	<p>Author: Ted Kulp &lt;tedkulp@users.sf.net&gt;</p>
 	<p>Version: 1.1</p>
+	<p>Modified: Martin B. Vestergaard &lt;mbv@nospam.dk&gt;</p>
+	<p>Version: 1.4</p>
 	<p>
 	Change History:<br/>
+	1.4 - fixed bug next/prev linking to non-content pages. (Thanks Teemu Koistinen for this fix)
+	1.3 - added option "more"<br />
+	1.2 - by Martin B. Vestergaard
+	<ul>
+		<li>changed default text to Page Name (was Page Alias)</li>
+		<li>added option dir=next/prev to display next or previous item in the hirachy - thanks to 100rk</li>
+		<li>added option class to add a class= statement to the a-tag.</li>
+		<li>added option menu to display menu-text in sted of Page Name</li>
+		<li>added option lang to display link-labels in different languages</li>
+	</ul>
 	1.1 - Changed to new content system<br />
 	1.0 - Initial release
 	</p>
