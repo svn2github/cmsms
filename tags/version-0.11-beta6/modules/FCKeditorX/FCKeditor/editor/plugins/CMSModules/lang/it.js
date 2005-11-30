1daa780f302cd228ef215788de6fdb2ef43268ab
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-11-30 00:46:51 +0000

    Wrong naming scheme
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@2227 3d254a34-79dc-0310-9e5f-be208747d8a0

--- tags/version-0.11-beta6/modules/FCKeditorX/FCKeditor/editor/plugins/CMSModules/lang/it.js
+++
@@ -1,4 +0,0 @@
-FCKLang.CMSModulesBtn                   = 'Inserisci/Edita Contenuto CMS' ;
-FCKLang.CMSModulesDlgTitle              = 'CMS Link - Inserisci link interno' ;
-FCKLang.CMSModulesDlgName               = 'CMS Link inserimento' ;
-FCKLang.CMSModulesDlgSelection          = 'Perfavore seleziona del testo in modo da applicare il link' ;
