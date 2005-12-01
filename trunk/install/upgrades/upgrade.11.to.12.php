3732a7f868c8d0b58c882fa2a692c433f954026f
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2005-12-01 10:48:08 +0000

    Changed upgrade script to collapse all nodes by default
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@2232 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/install/upgrades/upgrade.11.to.12.php
+++ trunk/install/upgrades/upgrade.11.to.12.php
@@ -8,6 +8,13 @@ $dbdict->ExecuteSQLArray($sqlarray);
 
 echo "[done]</p>";
 
+echo "<p>Setting all content to collapsed...";
+
+$query = "UPDATE ".cms_db_prefix()."content SET collapsed = 1";
+$db->Execute($query);
+
+echo "[done]</p>";
+
 echo '<p>Updating schema version... ';
 
 $query = "UPDATE ".cms_db_prefix()."version SET version = 12";
