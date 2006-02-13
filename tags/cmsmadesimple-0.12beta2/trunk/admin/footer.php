33dcedcab45448258c6d68deb374f6a54648af24
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-02-13 02:08:55 +0000

    -- Release 0.12beta2 --  * http://svn.cmsmadesimple.org/svn/cmsmadesimple/tags/cmsmadesimple-0.12beta2/trunk/admin/lang: update svn:externals    from 'old_external' to 'ext -r 559 http://svn.cmsmadesimple.org/svn/translatecenter/admin/lang'
     * http://svn.cmsmadesimple.org/svn/cmsmadesimple/tags/cmsmadesimple-0.12beta2/trunk/modules: update svn:externals
       from 'old_external' to 'ThemeManager -r 13 http://svn.cmsmadesimple.org/svn/thememanager'
    
     * http://svn.cmsmadesimple.org/svn/newsmodule/trunk/lang: update svn:externals
       from 'old_external' to 'ext -r 558 http://svn.cmsmadesimple.org/svn/translatecenter/modules/News/lang/ext'
    
     * http://svn.cmsmadesimple.org/svn/fckeditorx/trunk/lang: update svn:externals
       from 'old_external' to 'ext -r 510 http://svn.cmsmadesimple.org/svn/translatecenter/modules/FCKeditorX/lang/ext'
    
     * http://svn.cmsmadesimple.org/svn/menumanager/lang: update svn:externals
       from 'old_external' to 'ext -r 545 http://svn.cmsmadesimple.org/svn/translatecenter/modules/MenuManager/lang/ext'
    
    
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@2576 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ tags/cmsmadesimple-0.12beta2/trunk/admin/footer.php
@@ -0,0 +1,65 @@
+<?php
+$themeObject->DisplayMainDivEnd();
+$themeObject->OutputFooterJavascript();
+$themeObject->DisplayFooter();
+
+if ($gCms->config["debug"] == true)
+{
+	echo '<div id="DebugFooter">';
+	global $sql_queries;
+	echo "<div>".$sql_queries."</div>\n";
+	foreach ($gCms->errors as $error)
+	{
+		echo $error;
+	}
+	echo '</div> <!-- end DebugFooter -->';
+}
+
+?>
+
+</body>
+</html>
+
+<?php
+
+#Pull the stuff out of the buffer...
+$htmlresult = @ob_get_contents();
+@ob_end_clean();
+
+#Do any header replacements (this is for WYSIWYG stuff)
+$footertext = '';
+$formtext = '';
+$bodytext = '';
+
+$userid = get_userid();
+$wysiwyg = get_preference($userid, 'wysiwyg');
+
+foreach($gCms->modules as $key=>$value)
+{
+	if ($gCms->modules[$key]['installed'] == true &&
+		$gCms->modules[$key]['active'] == true &&
+		$gCms->modules[$key]['object']->IsWYSIWYG()
+		)
+	{
+		$loadit=false;
+		if ($gCms->modules[$key]['object']->WYSIWYGActive()) {
+			$loadit=true;
+		} else {
+		  if (get_preference(get_userid(), 'wysiwyg')==$gCms->modules[$key]['object']->GetName()) {
+		  	$loadit=true;
+		  }
+		}
+		if ($loadit) {
+		  $bodytext.=$gCms->modules[$key]['object']->WYSIWYGGenerateBody();
+		  $footertext.=$gCms->modules[$key]['object']->WYSIWYGGenerateHeader($htmlresult);
+		  $formtext.=$gCms->modules[$key]['object']->WYSIWYGPageForm();
+		}
+	}
+}
+
+$htmlresult = str_replace('<!-- THIS IS WHERE HEADER STUFF SHOULD GO -->', $footertext, $htmlresult);
+$htmlresult = str_replace('##FORMSUBMITSTUFFGOESHERE##', ' '.$formtext, $htmlresult);
+$htmlresult = str_replace('##BODYSUBMITSTUFFGOESHERE##', ' '.$bodytext, $htmlresult);
+
+echo $htmlresult;
+
