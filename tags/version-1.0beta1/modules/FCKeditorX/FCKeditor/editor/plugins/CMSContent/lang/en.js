847e0baaaee4bd5fcf1b8010e3605e004662dbe7
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-07-27 01:12:28 +0000

    Try again...
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@3168 3d254a34-79dc-0310-9e5f-be208747d8a0

--- tags/version-1.0beta1/modules/FCKeditorX/FCKeditor/editor/plugins/CMSContent/lang/en.js
+++
@@ -1,4 +0,0 @@
-FCKLang.CMSContentBtn			= 'Insert/Edit CMSMS link' ;
-FCKLang.CMSContentDlgTitle		= 'CMSMS Link - Insert internal link' ;
-FCKLang.CMSContentDlgName		= 'CMSMS Link insert' ;
-FCKLang.CMSContentDlgSelection		= 'Please make a selection of text in order to create a link' ;
--- tags/version-1.0beta1/modules/FCKeditorX/FCKeditor/editor/plugins/CMSModules/lang/en.js
+++
@@ -1,3 +0,0 @@
-FCKLang.CMSModulesBtn			= 'Insert modules call' ;
-FCKLang.CMSModulesDlgTitle		= 'CMS Modules - Insert modules call' ;
-FCKLang.CMSModulesDlgName		= 'CMS Modules' ;
