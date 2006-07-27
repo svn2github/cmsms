57fb66c9697c0fa4ef64d61fddfd0d0a7fe67206
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-07-27 01:08:01 +0000

    Try again...
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@3166 3d254a34-79dc-0310-9e5f-be208747d8a0

--- tags/version-1.0beta1/modules/FCKeditorX/FCKeditor/editor/plugins/CMSContent/lang/it.js
+++
@@ -1,4 +0,0 @@
-FCKLang.CMSContentBtn                   = 'Inserisci/Edita link CMSMS' ;
-FCKLang.CMSContentDlgTitle              = 'CMSMS Link - Inserisci link interno' ;
-FCKLang.CMSContentDlgName               = 'CMSMS Link inserimento' ;
-FCKLang.CMSContentDlgSelection          = 'Perfavore seleziona del testo in modo da applicare il link' ;
