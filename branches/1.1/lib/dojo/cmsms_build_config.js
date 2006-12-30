d6300f1bbc06612be86101de7f22ce61c3920a5b
elijahlofgren <elijahlofgren@3d254a34-79dc-0310-9e5f-be208747d8a0>
2006-12-30 21:24:47 +0000

    Add dojo cmsms build config file.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@3677 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ branches/1.1/lib/dojo/cmsms_build_config.js
@@ -0,0 +1,13 @@
+var dependencies = [ 
+	"dojo.widget.*",
+	"dojo.io.*",
+	"dojo.event.*",
+	"dojo.lfx.*",
+	"dojo.widget.Editor",
+	"dojo.widget.Editor2",
+	"dojo.widget.PageContainer",
+	"dojo.widget.TabContainer",
+	"dojo.widget.ContentPane",
+];
+
+load("getDependencyList.js");
