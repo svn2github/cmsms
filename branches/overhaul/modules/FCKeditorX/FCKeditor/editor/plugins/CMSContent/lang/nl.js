be3a4e98c4ab612ad135bbfda7b542d3acf04d1a
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2007-05-06 14:46:44 +0000

    Removing old branches
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@3907 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/overhaul/modules/FCKeditorX/FCKeditor/editor/plugins/CMSContent/lang/nl.js
+++
@@ -1,4 +0,0 @@
-﻿FCKLang.CMSContentBtn			= 'Interne CMS link toevoegen' ;
-FCKLang.CMSContentDlgTitle		= 'CMS Link - voeg interne pagina link toe' ;
-FCKLang.CMSContentDlgName		= 'CMS Link toevoegen' ;
-FCKLang.CMSContentDlgSelection		= 'Eerst een selectie maken voordat u een link wilt toevoegen' ;
--- branches/overhaul/modules/HTMLArea/lang/cs_CZ.php
+++
@@ -1,8 +0,0 @@
-<?php
-$lang['help'] = <<<EOF
-	<h3>Co dělá tento modul?</h3>
-	<p>Umožní používat HTMLArea WYSIWYG editor pro úpravu obsahu.</p>
-	<h3>Jak se používá?</h3>
-	<p>Nainstalovat, poté jít do Uživatelských nastavení a zvolit HTMLArea jako váš wysiwyg editor.</p>
-EOF;
-?>
