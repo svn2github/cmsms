98af672e8e33237e66cb6b21f8d79574e749c2e9
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2007-05-06 14:46:56 +0000

    Removing old branches
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@3909 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/0.4.x/locale/generatemo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-all_dir=$(find . -type d -iname ??_??)
-for po_file in $all_dir ; do
-	msgfmt -f -o $po_file/LC_MESSAGES/cmsmadesimple.mo $po_file/LC_MESSAGES/cmsmadesimple.po 
-done
--- branches/0.4.x/smarty/cms/plugins/function.cms_version.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function smarty_function_cms_version() {
-    global $CMS_VERSION;
-    echo $CMS_VERSION;
-} ## smarty_function_cms_version
-
-?>
