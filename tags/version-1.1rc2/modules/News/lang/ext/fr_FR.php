9b8a637ee59922f98b33d1baf7edbcb2ca5c2077
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2007-05-22 17:41:29 +0000

    -- Release 1.1rc2 --
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@3977 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ tags/version-1.1rc2/modules/News/lang/ext/fr_FR.php
@@ -0,0 +1,236 @@
+<?php
+$lang['uploadscategory'] = 'Cat&eacute;gorie Uploads';
+$lang['title_available_templates'] = 'Gabarits disponibles';
+$lang['resettodefault'] = 'Restaurer les param&egrave;tres par d&eacute;faut';
+$lang['title_detail_template'] = 'Editeur du gabarit du d&eacute;tail';
+$lang['title_summary_template'] = 'Editeur du gabarit du sommaire';
+$lang['prompt_templatename'] = 'Nom du gabarit';
+$lang['prompt_template'] = 'Source gabarit';
+$lang['title_summary_sysdefault'] = 'Gabarit du sommaire par d&eacute;faut';
+$lang['title_detail_sysdefault'] = 'Gabarit du d&eacute;tail par d&eacute;faut';
+$lang['info_sysdefault'] = '<em>(le gabarit utilis&eacute; par d&eacute;faut quand un nouveau gabarit est selectionn&eacute;)</em>';
+$lang['template'] = 'Gabarit&nbsp;';
+$lang['prompt_name'] = 'Nom';
+$lang['prompt_default'] = 'D&eacute;faut';
+$lang['prompt_newtemplate'] = 'Cr&eacute;er un nouveau gabarit';
+$lang['help_pagelimit'] = 'Nombre maximal d&#039;articles affich&eacute;s (par page). Si ce param&egrave;tre n&#039;est pas indiqu&eacute; tous les articles sont affich&eacute;s. Si ce param&egrave;tre est indiqu&eacute;, et que le nombres d&#039;articles est sup&eacute;rieur, les textes et les liens seront affich&eacute;s pour permettre le d&eacute;filement des r&eacute;sultats.';
+$lang['prompt_page'] = 'Page';
+$lang['firstpage'] = '<<';
+$lang['prevpage'] = '<';
+$lang['nextpage'] = '>';
+$lang['lastpage'] = '>>';
+$lang['prompt_of'] = 'sur';
+$lang['prompt_pagelimit'] = 'Nombre de pages&nbsp;';
+$lang['prompt_sorting'] = 'Tri&eacute; par&nbsp;';
+$lang['title_filter'] = 'Filtres';
+$lang['published'] = 'Publi&eacute;';
+$lang['draft'] = 'Brouillon';
+$lang['expired'] = 'Expir&eacute;';
+$lang['author'] = 'Auteur';
+$lang['sysdefaults'] = 'Restaurer les param&egrave;tres par d&eacute;faut';
+$lang['restoretodefaultsmsg'] = 'Cette op&eacute;ration restaurera les gabarits par d&eacute;faut. &Ecirc;tes-vous s&ucirc;r de vouloir continuer&nbsp;?';
+$lang['addarticle'] = 'Ajouter un article';
+$lang['articleadded'] = 'L&#039;article a &eacute;t&eacute; ajout&eacute; avec succ&egrave;s.';
+$lang['articleupdated'] = 'L&#039;article a &eacute;t&eacute; mis &agrave; jour avec succ&egrave;s.';
+$lang['articledeleted'] = 'L&#039;article a &eacute;t&eacute; supprim&eacute; avec succ&egrave;s.';
+$lang['addcategory'] = 'Ajouter une cat&eacute;gorie';
+$lang['categoryadded'] = 'La cat&eacute;gorie a &eacute;t&eacute; ajout&eacute;e avec succ&egrave;s.';
+$lang['categoryupdated'] = 'La cat&eacute;gorie a &eacute;t&eacute; mise &agrave; jour avec succ&egrave;s.';
+$lang['categorydeleted'] = 'La cat&eacute;gorie a &eacute;t&eacute; supprim&eacute;e avec succ&egrave;s.';
+$lang['addnewsitem'] = 'Ajouter un article';
+$lang['allcategories'] = 'Toutes les cat&eacute;gories';
+$lang['allentries'] = 'Toutes les entr&eacute;es';
+$lang['areyousure'] = '&Ecirc;tes-vous s&ucirc;r de vouloir supprimer&nbsp;?';
+$lang['articles'] = 'Articles';
+$lang['cancel'] = 'Annuler';
+$lang['category'] = 'Cat&eacute;gorie&nbsp;';
+$lang['categories'] = 'Cat&eacute;gories';
+$lang['default_category'] = 'Cat&eacute;gorie par d&eacute;faut&nbsp;';
+$lang['changelog'] = '<ul>
+<li>
+<p>Version: 1.0</p>
+<p>Ce module est une version modifi&eacute;e et am&eacute;lior&eacute;e du module News (Articles) par <em>Ted Kulp&#039;s</em>. Un champ a &eacute;t&eacute; ajout&eacute; &agrave; la base de donn&eacute;es, et un peu de code pour que ce champ fonctionne... J&#039;ai &eacute;galement nettoy&eacute; un peu le code, pour le rendre plus facile &agrave; lire. A part &ccedil;a, c&#039;est le code de Ted.</p>
+</li> 
+<li> 
+<p>Version: 1.1</p> 
+<p>Ajout&eacute; la possibilit&eacute; de mettre une date d&#039;expiration par d&eacute;faut avec un menu d&eacute;roulant, d&eacute;plac&eacute; la s&eacute;lection de cat&eacute;gorie, et sur la page principale, vous pouvez maintenant filtrer les articles que vous voulez voir.</p> 
+</li> 
+<li> 
+<p>Version: 1.2</p> 
+<p>Ajout&eacute; le sommaire, et les param&egrave;tres no_anchor et de longueur. En mode sommaire, les liens pointent maintenant aux articles, et les liens sont ins&eacute;r&eacute;s &agrave; la page des articles et dans l&#039;article sp&eacute;cifi&eacute;.</p> 
+</li> 
+<li> 
+<p>Version: 1.3</p> 
+<p>Petits changements visuels</p> 
+</li> 
+<li> 
+<p>Version 1.5</p> 
+<p>Combin&eacute; avec le module existant</p> 
+</li> 
+<li> 
+<p>Version 1.6</p> 
+<p>Pagination ajout&eacute;e, et d&eacute;placement du bouton d&#039;ajout en haut de page (calguy)</p>
+</li>
+<li>
+<p>Version 2.0</p>
+<p>R&eacute;-&eacute;crit pour l&#039;utilisation des gabarits smarty, et plusieurs autres grosses am&eacute;liorations.</p>
+</li>
+<li>
+<p>Version 2.0.1</p>
+<p>Petits changements pour la g&eacute;n&eacute;ration de flux RDD, afin de permettre ce flux de fonctionner sur diff&eacute;rents navigateurs, et pour le support des caract&egrave;res non alpha-num&eacute;riques dans la description.</p> 
+</li> 
+<li>
+<p>Version 2.0.2</p>
+<p>- Ajout&eacute; un param&egrave;tre &quot;start&quot; pour sp&eacute;cifier un butoir de d&eacute;part pour les articles</p>
+<p>- Les onglets de gabarits ont maintenant un bouton &quot;restaurer les valeurs par d&eacute;faut.</p>
+<p>- Une date de d&eacute;but est maintenant requise, mais la date de fin est toujours optionnelle, m&ecirc;me lorsque la coche &quot;utiliser l&#039;espiration&quot; est activ&eacute;e. 
+<p>- Le syst&egrave;me des permissions a &eacute;t&eacute; consid&eacute;rablement modifi&eacute;, la permission &quot;Modify News&quot; ne vaut maintenant que pour les articles et cat&eacute;gories. La permission &quot;Modify Templates&quot; est requise pour &eacute;diter les gabarits, et la permission &quot;Modify Site Preferences&quot; est requise pour &eacute;diter les options.</p> 
+<p>- D&eacute;plac&eacute; le titre de RSS dans les fichiers de langues.</p>
+</li> 
+<p>Version 2.0.3</p>
+<p>- Ajout de l&#039;identification de l&#039;auteur original d&#039;un article</p>
+</li>
+<li>
+<p>Version 2.2</p>
+<p>- Ajout du param&egrave;tre de cat&eacute;gorie de navigation
+</li>
+</ul> ';
+$lang['content'] = 'Contenu&nbsp;';
+$lang['dateformat'] = '%s pas dans un format valide yyyy-mm-dd hh:mm:ss';
+$lang['delete'] = 'Effacer';
+$lang['description'] = 'Ajout, &eacute;dition et suppression des articles';
+$lang['detailtemplate'] = 'Gabarit du d&eacute;tail de l&#039;article';
+$lang['detailtemplateupdated'] = 'Le gabarit de l&#039;affichage du d&eacute;tail de l&#039;article a &eacute;t&eacute; sauvegard&eacute; dans la base de donn&eacute;es.';
+$lang['displaytemplate'] = 'Afficher le gabarit';
+$lang['edit'] = '&Eacute;diter';
+$lang['enddate'] = 'Date de fin&nbsp;';
+$lang['endrequiresstart'] = 'Entrer une date de fin n&eacute;cessite qu&#039;une date de d&eacute;but soit &eacute;galement entr&eacute;e';
+$lang['entries'] = '%s entr&eacute;es';
+$lang['status'] = 'Statut';
+$lang['expiry'] = 'Expiration';
+$lang['filter'] = 'Filtre';
+$lang['more'] = 'Plus';
+$lang['category_label'] = 'Cat&eacute;gorie&nbsp;:';
+$lang['author_label'] = 'Post&eacute; par&nbsp;:';
+$lang['moretext'] = 'Texte pour ';
+$lang['name'] = 'Nom';
+$lang['news'] = 'Articles';
+$lang['news_return'] = 'Retour';
+$lang['newcategory'] = 'Nouvelle cat&eacute;gorie';
+$lang['needpermission'] = 'Vous devez avoir la permission &#039;%s&#039; pour ex&eacute;cuter cette action.';
+$lang['nocategorygiven'] = 'Aucune cat&eacute;gorie entr&eacute;e';
+$lang['nocontentgiven'] = 'Aucun contenu entr&eacute;';
+$lang['noitemsfound'] = '<strong>Aucun</strong> objet trouv&eacute; pour cette cat&eacute;gorie: %s';
+$lang['nopostdategiven'] = 'Il manque la date &agrave; laquelle l&#039;article sera post&eacute;';
+$lang['note'] = '<em>Note:</em> Les dates doivent &ecirc;tre entr&eacute;es dans ce format &#039;yyyy-mm-dd hh:mm:ss&#039;.';
+$lang['notitlegiven'] = 'Aucun titre entr&eacute;';
+$lang['nonamegiven'] = 'Aucun nom donn&eacute;';
+$lang['numbertodisplay'] = 'Nombre &agrave; afficher (vide = toutes les entr&eacute;es)';
+$lang['print'] = 'Imprimer';
+$lang['postdate'] = 'Date &agrave; laquelle l&#039;article sera post&eacute;&nbsp;';
+$lang['postinstall'] = 'Assurez-vous que les utilisateurs qui administeront les articles aient la permission &quot;Modify News&quot;.';
+$lang['rssfeedtitle'] = 'Flux RSS CMS Made Simple';
+$lang['rsstemplate'] = 'Gabarit RSS';
+$lang['selectcategory'] = 'S&eacute;lection de cat&eacute;gorie';
+$lang['showchildcategories'] = 'Afficher les sous-cat&eacute;gories&nbsp;';
+$lang['sortascending'] = 'Tri ascendant';
+$lang['startdate'] = 'Date de d&eacute;but&nbsp;';
+$lang['startoffset'] = 'Commence l&#039;affichage au &eacute;ni&egrave;me article';
+$lang['startrequiresend'] = 'Entrer une date de d&eacute;but n&eacute;cessite qu&#039;une date de fin soit &eacute;galement entr&eacute;e';
+$lang['submit'] = 'Envoyer';
+$lang['summary'] = 'Sommaire&nbsp;';
+$lang['summarytemplate'] = 'Gabarit du sommaire de l&#039;article';
+$lang['summarytemplateupdated'] = 'Le gabarit de l&#039;affichage du sommaire de l&#039;article a &eacute;t&eacute; mis &agrave; jour avec succ&egrave;s';
+$lang['title'] = 'Titre&nbsp;';
+$lang['options'] = 'Options';
+$lang['optionsupdated'] = 'Les options ont &eacute;t&eacute; mises &agrave; jour avec succ&egrave;s';
+$lang['useexpiration'] = 'Utiliser la date d&#039;expiration&nbsp;';
+$lang['showautodiscovery'] = 'Affiche le lien Auto-Discovery du flux RSS&nbsp;';
+$lang['autodiscoverylink'] = 'URL du flux RSS Auto-Discovery (vide par defaut)&nbsp;';
+$lang['eventdesc-NewsArticleAdded'] = 'Envoy&eacute; quand un article est ajout&eacute;';
+$lang['eventhelp-NewsArticleAdded'] = '<p>Envoy&eacute; quand un article est ajout&eacute;</p>
+<h4>Param&egrave;tres</h4>
+<ul>
+<li>\&quot;news_id\&quot; - Id de l&#039;article</li>
+<li>\&quot;category_id\&quot; - Id de la cat&eacute;gorie de cet article</li>
+<li>\&quot;title\&quot; - Titre de l&#039;article</li>
+<li>\&quot;content\&quot; - Contenu de l&#039;article</li>
+<li>\&quot;summary\&quot; - Sommaire de l&#039;article</li>
+<li>\&quot;status\&quot; - Statut de l&#039; article (&quot;draft&quot; or &quot;published&quot;)</li>
+<li>\&quot;start_time\&quot; - Date de d&eacute;but de publication de l&#039;article</li>
+<li>\&quot;end_time\&quot; - Date de fin de publication de l&#039;article</li>
+<li>\&quot;useexp\&quot; - Si la date d&#039;expiration doit &ecirc;tre ignor&eacute;e ou pas</li>
+</ul>
+';
+$lang['eventdesc-NewsArticleEdited'] = 'Envoy&eacute; quand un article est &eacute;dit&eacute;';
+$lang['eventhelp-NewsArticleEdited'] = '<p>Envoy&eacute; quand un article est &eacute;dit&eacute;</p>
+<h4>Param&egrave;tres</h4>
+<ul>
+<li>\&quot;news_id\&quot; - Id de l&#039;article</li>
+<li>\&quot;category_id\&quot; - Id de la cat&eacute;gorie de cet article</li>
+<li>\&quot;title\&quot; - Titre de l&#039;article</li>
+<li>\&quot;content\&quot; - Contenu de l&#039;article</li>
+<li>\&quot;summary\&quot; - Sommaire de l&#039;article</li>
+<li>\&quot;status\&quot; - Statut de l&#039; article (&quot;draft&quot; or &quot;published&quot;)</li>
+<li>\&quot;start_time\&quot; - Date de d&eacute;but de publication de l&#039;article</li>
+<li>\&quot;end_time\&quot; - Date de fin de publication de l&#039;article</li>
+<li>\&quot;useexp\&quot; - Si la date d&#039;expiration doit &ecirc;tre ignor&eacute;e ou pas</li>
+</ul>
+';
+$lang['eventdesc-NewsArticleDeleted'] = 'Envoy&eacute; quand un article est supprim&eacute;';
+$lang['eventhelp-NewsArticleDeleted'] = '<p>Envoy&eacute; quand un article est supprim&eacute;</p>
+<h4>Param&egrave;tres</h4>
+<ul>
+<li>\&quot;news_id\&quot; - Id de l&#039;article</li>
+</ul>
+';
+$lang['eventdesc-NewsCategoryAdded'] = 'Envoy&eacute; quand une cat&eacute;gorie est ajout&eacute;e';
+$lang['eventhelp-NewsCategoryAdded'] = '<p>Envoy&eacute; quand une cat&eacute;gorie est ajout&eacute;e</p>
+<h4>Param&egrave;tres</h4>
+<ul>
+<li>\&quot;category_id\&quot; - Id de la cat&eacute;gorie</li>
+<li>\&quot;name\&quot; - Nom de la cat&eacute;gorie</li>
+</ul>
+';
+$lang['eventdesc-NewsCategoryEdited'] = 'Envoy&eacute; quand une cat&eacute;gorie est &eacute;dit&eacute;e';
+$lang['eventhelp-NewsCategoryEdited'] = '<p>Envoy&eacute; quand une cat&eacute;gorie est &eacute;dit&eacute;e</p>
+<h4>Param&egrave;tres</h4>
+<ul>
+<li>\&quot;category_id\&quot; - Id de la cat&eacute;gorie</li>
+<li>\&quot;name\&quot; - Nom de la cat&eacute;gorie</li>
+<li>\&quot;origname\&quot; - Nom original de la cat&eacute;gorie</li>
+</ul>
+';
+$lang['eventdesc-NewsCategoryDeleted'] = 'Envoy&eacute; quand une cat&eacute;gorie est supprim&eacute;e';
+$lang['eventhelp-NewsCategoryDeleted'] = '<p>Envoy&eacute; quand une cat&eacute;gorie est supprim&eacute;e</p>
+<h4>Param&egrave;tres</h4>
+<ul>
+<li>\&quot;category_id\&quot; - Id de la cat&eacute;gorie</li>
+<li>\&quot;name\&quot; - Nom de la cat&eacute;gorie supprim&eacute;e</li>
+</ul>
+';
+$lang['helpnumber'] = 'Le nombre maximal d&#039;articles &agrave; afficher -- laisser ce param&egrave;tre vide affichera tous les articles.';
+$lang['helpstart'] = 'Commence au &eacute;ni&egrave;me article -- laisser ce param&egrave;tre vide commencera l&#039;affichage au premier article';
+$lang['helpmakerssbutton'] = 'Affiche un bouton pour cr&eacute;er un flux RSS des articles.';
+$lang['helpcategory'] = 'Affiche les articles de cette cat&eacute;gorie seulement. Utiliser * pour afficher les sous-cat&eacute;gories. Des cat&eacute;gories multiples peuvent &ecirc;tre affich&eacute;es en les s&eacute;parant par une virgule. Laisser ce param&egrave;tre vide affichera tous les articles.';
+$lang['helpmoretext'] = 'Texte &agrave; afficher &agrave; la fin d&#039;un article qui d&eacute;passe la longueur d&eacute;finie du sommaire.  D&eacute;faut = &quot;more...&quot;';
+$lang['helpsummarytemplate'] = 'Utilise un gabarit s&eacute;par&eacute; pour l&#039;affichage du sommaire des articles.  Il doit se trouver dans modules/News/templates.';
+$lang['helpdetailtemplate'] = 'Utilise un gabarit s&eacute;par&eacute; pour l&#039;affichage du d&eacute;tail des articles.  Il doit se trouver dans modules/News/templates.';
+$lang['helpsortby'] = 'Champ sur lequel trier les articles.  Les options sont: &quot;news_date&quot;, &quot;summary&quot;, &quot;news_data&quot;, &quot;news_category&quot;, &quot;news_title&quot;.  Par d&eacute;faut: &quot;news_date&quot;.';
+$lang['helpsortasc'] = 'Trie les articles dans un ordre de date ascendant plut&ocirc;t que descendant. Par d&eacute;faut: descendant.';
+$lang['helpdetailpage'] = 'Page dans laquelle afficher le d&eacute;tail des articles. Vous pouvez entrer soit un alias, soit un ID de page. Utile pour permettre d&#039;afficher le d&eacute;tail de l&#039;article dans un gabarit de page diff&eacute;rent de celui du sommaire.';
+$lang['helpdateformat'] = 'Format d&#039;affichage de la date de l&#039;article.  Ceci est bas&eacute; sur la fonction <a href=&quot;http://php.net/strftime&quot; target=&quot;_blank&quot;>strftime</a> et peut &ecirc;tre utilis&eacute; dans votre gabarit avec $entry->formatpostdate. %x par d&eacute;faut, qui est le format par d&eacute;faut des dates dans la configuration des &quot;locales&quot; du serveur&nbsp;';
+$lang['helpshowarchive'] = 'Afficher seulement les articles expir&eacute;s.';
+$lang['helpbrowsecat'] = 'Afficher une liste navigable de cat&eacute;gories';
+$lang['help'] = '	<h3>Que fait ce module&nbsp;?</h3>
+	<p>Article est un module pour afficher des articles sur vos pages, de fa&ccedil;on similaire &agrave; un blog, mais avec plus de fonctions!  D&egrave;s que le module est install&eacute;, une page de gestion des articles est ajout&eacute;e au menu d&#039;administration qui vous permettra de s&eacute;lectionner ou ajouter des cat&eacute;gories d&#039;articles.  D&egrave;s qu&#039;une cat&eacute;gorie d&#039;article est s&eacute;lectionn&eacute;e ou cr&eacute;&eacute;e, une liste des articles pour cette cat&eacute;gorie est affich&eacute;e.  Depuis l&agrave;, vous pouvez ajouter, &eacute;diter ou supprimer les articles dans cette cat&eacute;gorie.</p>
+	<h3>Variables de gabarit</h3>
+	<ul>
+		<li><b>itemcount</b> - Le nombre d&#039;articles qui doit &ecirc;tre affich&eacute;.</li>
+	</ul>
+	<h3>S&eacute;curit&eacute;</h3>
+	<p>L&#039;utilisateur doit faire partie d&#039;un groupe avec la permission &#039;Modify News&#039; pour pouvoir ajouter, &eacute;diter ou supprimer des articles.</p>
+	<p>Pour modifier les pr&eacute;f&eacute;rences globales du module, l&#039;utilisateur doit faire partie d&#039;un groupe avec la permission &#039;Modify Site Preferences&#039;.</p>
+	<h3>Comment l&#039;utiliser&nbsp;?</h3>
+	<p>La fa&ccedil;on la plus facile de l&#039;utiliser est avec la balise wrapper {news} (englobe le module dans une simple balise pour simplifier la synthaxe).  Cela ins&eacute;rera votre module dans votre gabarit ou votre page &agrave; l&#039;endroit d&eacute;sir&eacute;, et y affichera les articles.  Exemple de synthaxe: <code>{news number=&#039;5&#039;}</code></p>';
+?>
---
+++ tags/version-1.1rc2/plugins/function.search.php
@@ -0,0 +1,49 @@
+<?php
+#CMS - CMS Made Simple
+#(c)2004 by Ted Kulp (wishy@users.sf.net)
+#This project's homepage is: http://cmsmadesimple.sf.net
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+function smarty_cms_function_search($params, &$smarty)
+{
+  $params['module'] = 'Search';
+  return cms_module_plugin($params,$smarty);
+}
+
+function smarty_cms_help_function_search() {
+	?>
+	<h3>What does this do?</h3>
+	<p>This is actually just a wrapper tag for the <a href="listmodules.php?action=showmodulehelp&module=Search">Search module</a> to make the tag syntax easier. 
+	Instead of having to use <code>{cms_module module='Search'}</code> you can now just use <code>{search}</code> to insert the module in a template.
+	</p>
+	<h3>How do I use it?</h3>
+	<p>Just put <code>{search}</code> in a template where you want the search input box to appear. For help about the Search module, please refer to the <a href="listmodules.php?action=showmodulehelp&module=Search">Search module help</a>.
+
+	<?php
+}
+
+function smarty_cms_about_function_search() {
+	?>
+	<p>Author: Ted Kulp&lt;tedkulp@users.sf.net&gt;</p>
+	<p>Version: 1.0</p>
+	<p>
+	Change History:<br/>
+	None
+	</p>
+	<?php
+}
+
+
+?>
