52182319888c1e6512a9c3b40fe4a37881ace34d
tsw <tsw@3d254a34-79dc-0310-9e5f-be208747d8a0>
2007-05-27 19:08:37 +0000

    only load unit tests if we are running tests, cms_user tests added
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@3985 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/2.0/lib/classes/class.cms_unit_test.php
+++ branches/2.0/lib/classes/class.cms_unit_test.php
@@ -1,114 +1,123 @@
 <?php 
-class CmsUnitTest {
-  var $run = 0;
-  var $failed = 0;
-  /**
-   * Class name to be tested
-   * @returns Class name to be tested
-   */
-  public function getClassName() {
-    return "";
-  }
-  /**
-   * Number of tests to run
-   * @returns int number of tests
-   */
-  public function getNumberOfTests() {
-    return 0;
-  }
-  public function getTestDirectory() {
-    return "";
-  }
-  /**
-   * Should we die on fail?
-   * @returns bool true to die on first failure
-   */
-  public function DieOnFail() {
-    return false;
-  }
-
-  /**
-   * specify your tests here
-   */ 
-  public function run_tests() {
-
-  }
-
-  final protected function results() {
-    if($this->getNumberOfTests() == $this->run) {
-      echo "Ran all tests ";
-      if($this->failed == 0) {
-	echo "and everything seems ok\n";
-      }
-    } else {
-      echo "I was supposed to run ".$this->getNumberOfTests()." tests, but ran $this->run \n";
-      echo "ran $this->run tests from which $this->failed failed\n";
+if(CMS_TEST_RUN) {
+  class CmsUnitTest {
+    var $run = 0;
+    var $failed = 0;
+    /**
+     * Class name to be tested
+     * @returns Class name to be tested
+     */
+    public function getClassName() {
+      return "";
     }
-  }
-  
-  final protected function test_eq($value, $expected, $explanation = "") {
-    $this->run++;
-    if($value == $expected) {
-      $this->doresult(true, $explanation);
-    } else {
-      $this->doresult(false, $explanation);
+    /**
+     * Number of tests to run
+     * @returns int number of tests
+     */
+    public function getNumberOfTests() {
+      return 0;
     }
-  }
-  
-  final protected function test_isset($value, $explanation ="") {
-    $this->run++;
-    if(isset($value)) {
-      $this->doresult(true, $explanation);
-    } else {
-      $this->doresult(false, $explanation);
+    public function getTestDirectory() {
+      return "";
     }
-  }
-
-  public function doresult($result, $explanation) {
-    $flag = 0;
-    if($result == true) {
-      echo "test $this->run ok";
-    } else {
-      $this->failed++;
-      echo "test $this->run failed";
-      if($this->DieOnFail() == true) {
-	$flag = 1;
-      }
+    /**
+     * Should we die on fail?
+     * @returns bool true to die on first failure
+     */
+    public function DieOnFail() {
+      return false;
     }
-    if($explanation!="")
-      echo " - $explanation";
-    echo "\n";
-    if($flag==1)
-      die("### dying as requested on failure ### \n");
-  }
-
-  final protected function listFiles($dir) {
-    $files = array();
-    $dir = opendir($dir);
-    while (($file = readdir($dir)) !== false) {
-      if ($file != "." && $file != ".." && !preg_match('/~/',$file)) {
-	$files[] = $file;
-      }
+    
+    /**
+     * specify your tests here
+     */ 
+    public function run_tests() {
+      
     }
-    sort($files);
-    closedir($dir);
-    return $files;
-  }
-
-  final protected function rundir($dir) {
-    $files = listFiles($dir);
-    foreach ($files as $file) {
-      if ( is_dir( "$dir/$file" ) )
-	{
-	  rundir("$dir/$file");
+    
+    final protected function results() {
+      if($this->getNumberOfTests() == $this->run) {
+	echo "Ran all tests ";
+	if($this->failed == 0) {
+	  echo "and everything seems ok\n";
 	}
-      else
-	{
-	  include("$dir/$file");
+      } else {
+	echo "I was supposed to run ".$this->getNumberOfTests()." tests, but ran $this->run \n";
+	echo "ran $this->run tests from which $this->failed failed\n";
+      }
+    }
+    
+    final protected function test_eq($value, $expected, $explanation = "") {
+      $this->run++;
+      if($value == $expected) {
+	$this->doresult(true, $explanation);
+      } else {
+	$this->doresult(false, $explanation);
+      }
+    }
+    
+    final protected function test_isset($value, $explanation ="") {
+      $this->run++;
+      if(isset($value)) {
+	$this->doresult(true, $explanation);
+      } else {
+	$this->doresult(false, $explanation);
+      }
+    }
+    
+    final protected function test_isa($obj, $class, $explanation="") {
+      $this->run++;
+      if($obj instanceof $class) {
+	$this->doresult(true, $explanation);
+      } else {
+	$this->doresult(false, $explanation);
+      }
+    }
+    
+    public function doresult($result, $explanation) {
+      $flag = 0;
+      if($result == true) {
+	echo "test $this->run ok";
+      } else {
+	$this->failed++;
+	echo "test $this->run failed";
+	if($this->DieOnFail() == true) {
+	  $flag = 1;
 	}
+      }
+      if($explanation!="")
+	echo " - $explanation";
+      echo "\n";
+      if($flag==1)
+	die("### dying as requested on failure ### \n");
+    }
+    
+    final protected function listFiles($dir) {
+      $files = array();
+      $dir = opendir($dir);
+      while (($file = readdir($dir)) !== false) {
+	if ($file != "." && $file != ".." && !preg_match('/~/',$file)) {
+	  $files[] = $file;
+	}
+      }
+      sort($files);
+      closedir($dir);
+      return $files;
+    }
+    
+    final protected function rundir($dir) {
+      $files = listFiles($dir);
+      foreach ($files as $file) {
+	if ( is_dir( "$dir/$file" ) )
+	  {
+	    rundir("$dir/$file");
+	  }
+	else
+	  {
+	    include("$dir/$file");
+	  }
+      }
     }
   }
-
-
 }
 ?>
--- branches/2.0/lib/tests/cms_user.php
+++ branches/2.0/lib/tests/cms_user.php
@@ -1,10 +1,12 @@
 <?php
+// define that we are on test run
+DEFINE(CMS_TEST_RUN,1);
 // get the api
 include(dirname(__FILE__).'/../cmsms.api.php');
 // our test class
 class CmsUserTests extends CmsUnitTest {
   public function getNumberOfTests() {
-    return 5;
+    return 10;
   }
   
   public function getTestDirectory() {
@@ -12,14 +14,49 @@ class CmsUserTests extends CmsUnitTest {
   }
   
   public function run_tests() {
-    $user = CmsUserOperations::load_user_by_id(1);
-
+    // load first user and check the values
+    $user = CmsUserOperations::load_user_by_id(1);    
+    // save old username
+    $old_username = $user->username;
     $this->test_isset($user, "user is set and loaded");
-
+    $this->test_isa($user, "CmsUser", "object is instance of class user");
+    $this->test_eq($user->id,1,"id is one");
     $this->test_isset($user->username,"username is set");
     $this->test_eq($user->username="asdf",true,"set username to asdf");
+    $user->save();
+    unset($user);
+    $user = CmsUserOperations::load_user_by_id(1);
     $this->test_eq($user->username,"asdf","username is asdf");
-    $this->test_eq($user->id,1,"id is one");
+    // return original username :)
+    $user->username=$old_username;
+    $user->save();
+    unset($user);
+
+    //create new user
+    $newuser = new CmsUser();
+    $newuser->username = "testuser";
+    $newuser->active = 0; //just in case
+    $newuser->first_name = "testuser_firstname";
+    $newuser->last_name = "testuser_lastname";
+    $newuser->email = "testuser@cmsmadesimple.org";
+    $newuser->admin_access = 0; //just in case
+    $newuser->password = "testpass";
+
+    $this->test_eq($newuser->save(), true, "user created");    
+    $loadeduser = CmsUserOperations::load_user_by_id($newuser->id);
+
+    var_dump($loadeduser);
+    var_dump($newuser);
+
+    //check that users match
+    $this->test_eq($newuser, $loadeduser, "users match");
+    unset($newuser);
+
+    // delete user
+    $this->test_eq($loadeduser->delete(), true, "user deleted");
+
+    $this->test_eq(CmsUserOperations::load_user_by_id($loadeduser->id), false, "user cant be found anymore");
+    
 
     $this->results();
   }
