bd55278948f75332a27e7e89e51e327d25ce9ade
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2007-06-09 02:55:19 +0000

    Revoking 1.1rc3.  TinyMCE needs to be stripped down a bit.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@3997 3d254a34-79dc-0310-9e5f-be208747d8a0

--- tags/version-1.1rc3/modules/TinyMCE/action.savexconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!$this->VisibleToAdminUser()) $this->Redirect($id,"defaultadmin",$returnid);
-
-if (isset($params['extraconfig']))
-  $this->SetPreference('extraconfig', $params['extraconfig'] );
-
-$this->Redirect($id,"defaultadmin",$returnid,array("module_message"=>$this->Lang("xconfigsaved"),"tab"=>"xconfig"));
-?>
