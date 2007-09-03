063cca55d3b3983ed703994c7efe456ac307067f
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2007-09-03 18:24:19 +0000

    Trying again
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@4116 3d254a34-79dc-0310-9e5f-be208747d8a0

--- tags/version-1.1.2/admin/loginstyle.php
+++
@@ -1,7 +0,0 @@
-<?php 
-header("Content-type: text/css");
-if (file_exists(dirname(__FILE__)."/themes/default/css/style.css"))
-	{
-	readfile(dirname(__FILE__)."/themes/default/css/style.css");
-}
-?>
