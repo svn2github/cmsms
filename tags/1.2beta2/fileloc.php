40c12c2e77e83169d0c756e813242df390b888d7
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2007-09-30 14:10:02 +0000

    Tag 1.2beta2
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@4206 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ tags/1.2beta2/fileloc.php
@@ -0,0 +1,5 @@
+<?php
+define("CONFIG_FILE_LOCATION", dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config.php');
+define("TMP_CACHE_LOCATION", dirname(__FILE__) . DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR . 'cache');
+define("TMP_TEMPLATES_C_LOCATION", dirname(__FILE__) . DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR . 'templates_c');
+?>
