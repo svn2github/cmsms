004d01917af4bae29272a4aaccc560f3d2348796
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2008-12-01 15:07:55 +0000

    delete the tag.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@5286 3d254a34-79dc-0310-9e5f-be208747d8a0

--- tags/version-1.5-beta2/modules/Printing/method.install.php
+++
@@ -1,7 +0,0 @@
-<?php
-	  $this->ResetLinkTemplate();
-	  $this->ResetPrintTemplate();
-	  $this->ResetPDFTemplate();
-	  $this->ResetOverrideStyle();
-	  //$this->CreatePermission('modifyprintingsettings',$this->Lang("permission"));
-?>
--- tags/version-1.5-beta2/modules/TinyMCE/method.install.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $this->ResetSettings("all");
-  $this->Audit( 0, $this->Lang('friendlyname'), $this->Lang('installed',$this->GetVersion()));
-?>
