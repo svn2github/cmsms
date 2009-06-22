028df9cdb8b658f7b9bcbfe4dd4d4627ba675295
wishy <wishy@3d254a34-79dc-0310-9e5f-be208747d8a0>
2009-06-22 16:07:25 +0000

    Removed tag.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@5787 3d254a34-79dc-0310-9e5f-be208747d8a0

--- tags/version-1.6/modules/TinyMCE/method.install.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $this->ResetSettings("all"); 
-
-  $this->CreatePermission('allowadvancedprofile', $this->Lang('allowadvancedprofile'));
-
-  $this->Audit( 0, $this->Lang('friendlyname'), $this->Lang('installed',$this->GetVersion()));
-?>
