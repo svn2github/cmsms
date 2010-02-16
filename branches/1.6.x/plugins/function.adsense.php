9ababfbad5dabda15fa580166b5872503a05ef78
ronnyk <ronnyk@3d254a34-79dc-0310-9e5f-be208747d8a0>
2010-02-16 09:44:37 +0000

    Rolfs modification inserted, including additional parameter (google_ad_slot) and rewrite.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@6113 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.6.x/plugins/function.adsense.php
+++ branches/1.6.x/plugins/function.adsense.php
@@ -17,60 +17,13 @@
 #Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 function smarty_cms_function_adsense($params, &$smarty) {
-	$ad_client = "";
-	$ad_width = "120";
-	$ad_height = "600";
-	$ad_format = "120x600_as";
-	$ad_channel = "";
-	$ad_type = "";
-	$color_border = "";
-	$color_bg = "";
-	$color_link = "";
-	$color_url = "";
-	$color_text = "";
-	
-	if(!empty($params['ad_client']))
-		$ad_client = $params['ad_client'];
-	if(!empty($params['ad_width']))
-		$ad_width = $params['ad_width'];
-	if(!empty($params['ad_height']))
-		$ad_height = $params['ad_height'];
-	if(!empty($params['ad_format']))
-		$ad_format = $params['ad_format'];
-	if(!empty($params['ad_channel']))
-		$ad_channel = $params['ad_channel'];
-	if(!empty($params['ad_type']))
-		$ad_type = $params['ad_type'];
-	if(!empty($params['color_border']))
-		$color_border = $params['color_border'];
-	if(!empty($params['color_bg']))
-		$color_bg = $params['color_bg'];
-	if(!empty($params['color_link']))
-		$color_link = $params['color_link'];
-	if(!empty($params['color_url']))
-		$color_url = $params['color_url'];
-	if(!empty($params['color_text']))
-		$color_text = $params['color_text'];
-
-	
-	//$result = "\n<!-- Begin Google AdSense Ad -->\n";
-	$result .= "\n<script type=\"text/javascript\"><!--\n";
-	$result .= "google_ad_client = \"$ad_client\";\n";
-	$result .= "google_ad_width = \"$ad_width\";\n";
-	$result .= "google_ad_height = \"$ad_height\";\n";
-	$result .= "google_ad_format = \"$ad_format\";\n";
-	$result .= "google_ad_type = \"$ad_type\";\n";
-	$result .= "google_ad_channel = \"$ad_channel\";\n";
-	$result .= "google_color_border = \"$color_border\";\n";
-	$result .= "google_color_bg = \"$color_bg\";\n";
-	$result .= "google_color_link = \"$color_link\";\n";
-	$result .= "google_color_url = \"$color_url\";\n";
-	$result .= "google_color_text = \"$color_text\";\n";
-	$result .= "//--></script>\n";
-	$result .= "<script type=\"text/javascript\" src=\"http://pagead2.googlesyndication.com/pagead/show_ads.js\"></script>\n";
-	//$result .= "\n<!-- End Google AdSense Ad -->\n";
-
-	return $result;
+	$parts = array("ad_client","ad_width","ad_height","ad_format","ad_format","ad_channel",
+			"ad_slot","ad_type","color_border","color_bg","color_link","color_url","color_text");
+	$result = "\n<script type='text/javascript'><!--\n";
+	foreach( $parts as $part ) 
+		if( isset( $params[ $part ] ) and !empty( $params[ $part ] ) ) 
+			$result .= "google_{$part} = \"".$params[$part]."\";\n";
+	return "{$result}//--></script>\n<script type='text/javascript' src='http://pagead2.googlesyndication.com/pagead/show_ads.js'></script>\n";
 }
 
 function smarty_cms_help_function_adsense() {
@@ -83,9 +36,10 @@ function smarty_cms_about_function_adsense() {
 	<p>Version: 1.0</p>
 	<p>
 	Change History:<br/>
-	11/11/2005 Hacked by basicus (http://basicus.com) Added the missing configuration options that AdSense provides.
+       11/22/2009 Mark Giblin: Rewrote function.<br />
+       11/22/2009 Rolf Tjassens: Added google_ad_slot and deleted default parameter values<br />
+       11/11/2005 Hacked by basicus (http://basicus.com) Added the missing configuration options that AdSense provides.
 	</p>
 	<?php
 }
-
 ?>
