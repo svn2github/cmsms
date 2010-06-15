526f52c464f2c60f66a8f4b3165dc5f299213af5
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2010-06-15 18:24:52 +0000

    Revert previous change Now 'Modify All Pages' cannot be used to change template.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@6389 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.7.x/admin/listcontent.php
+++ branches/1.7.x/admin/listcontent.php
@@ -62,8 +62,7 @@ function content_list_ajax()
 
 function check_modify_all($userid)
 {
-  //return check_permission($userid, 'Modify Any Page') || check_permission($userid,'Manage All Content');
-  return check_permission($userid,'Manage All Content');
+  return check_permission($userid, 'Modify Any Page') || check_permission($userid,'Manage All Content');
 }
 
 function setdefault($contentid)
@@ -920,7 +919,7 @@ function display_hierarchy(&$root, &$userid, $modifyall, &$templates, &$users, &
 
 	  $remove    = check_permission($userid, 'Remove Pages')?1:0;
 	  $structure = check_permission($userid, 'Manage All Content')?1:0;
-	  $editperms = (check_permission($userid, 'Manage All Content') ||
+	  $editperms = (check_permission($userid, 'Modify Any Page') ||
 			quick_check_authorship($one->Id(),$mypages) ||
 			check_ownership($userid,$one->Id()))?1:0;
 	  if ( (($structure == 1) || (($remove == 1) && ($editperms == 1))) &&
@@ -1005,8 +1004,7 @@ function display_content_list($themeObject = null)
 	$columnstodisplay['edit'] = 1;
 	$columnstodisplay['delete'] = check_permission($userid, 'Remove Pages') || check_permission($userid,'Manage All Content');
 	$columnstodisplay['multiselect'] = check_permission($userid, 'Remove Pages') ||
-	  //	  check_permission($userid,'Manage All Content') || check_permission($userid,'Modify Any Page');
-  check_permission($userid,'Manage All Content');
+	  check_permission($userid,'Manage All Content') || check_permission($userid,'Modify Any Page');
 	
 	$page = 1;
 	if (isset($_GET['page']))
@@ -1168,7 +1166,7 @@ function display_content_list($themeObject = null)
 	$opts = array();
 	if( check_permission($userid, 'Remove Pages') || check_permission($userid, 'Manage All Content') )
 	  {
-    bulkcontentoperations::register_function(lang('delete'),'delete');
+	    bulkcontentoperations::register_function(lang('delete'),'delete');
 	  }
 	if (check_permission($userid, 'Manage All Content')) 
 	  {
@@ -1180,9 +1178,6 @@ function display_content_list($themeObject = null)
 	    bulkcontentoperations::register_function(lang('hidefrommenu'),'hidefrommenu');
 	    bulkcontentoperations::register_function(lang('secure'),'secure');
 	    bulkcontentoperations::register_function(lang('insecure'),'insecure');
-	  }
-	if (check_permission($userid, 'Manage All Content'))
-	  {
 	    bulkcontentoperations::register_function(lang('settemplate'),'settemplate');
 	  }
 	$opts = bulkcontentoperations::get_operation_list();
