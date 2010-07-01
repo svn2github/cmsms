72582d99f5691b2d87a2161ff441d7ece41e0b6a
sjg <sjg@3d254a34-79dc-0310-9e5f-be208747d8a0>
2010-07-01 03:26:27 +0000

    Added more documentation
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@6423 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.7.x/lib/classes/class.global.inc.php
+++ branches/1.7.x/lib/classes/class.global.inc.php
@@ -26,7 +26,7 @@
 /**
  * Simple global object to hold references to other objects
  *
- * Global object that holds references to various data sctructures
+ * Global object that holds references to various data structures
  * needed by classes/functions in CMS.  Initialized in include.php
  * as $gCms for use in every page.
  *
@@ -36,86 +36,103 @@ class CmsObject {
 
 	/**
 	 * Config object - hash containing variables from config.php
+	 *	@access private
 	 */
 	var $config;
 
 	/**
 	 * Database object - adodb reference to the current database
+	 *	@access private
 	 */
 	var $db;
 
 	/**
 	 * Variables object - various objects and strings needing to be passed 
+	 *	@access private
 	 */
 	var $variables;
 
 	/**
 	 * Modules object - holds references to all registered modules
+	 *	@access private
 	 */
 	var $cmsmodules;
 
 	/**
 	 * System Modules - a list (hardcoded) of all system modules
+	 *	@access private
 	 */
 	var $cmssystemmodules;
 
 	/**
 	 * Plugins object - holds list of all registered plugins 
+	 *	@access private
 	 */
 	var $cmsplugins;
 
 	/**
 	 * User Plugins object - holds list and function pointers of all registered user plugins
+	 *	@access private
 	 */
 	var $userplugins;
 
 	/**
 	 * BBCode object - for use in bbcode parsing
+	 *	@access private
 	 */
 	var $bbcodeparser;
 
 	/**
 	 * Site Preferences object - holds all current site preferences so they're only loaded once
+	 *	@access private
 	 */
 	var $siteprefs;
 
 	/**
 	 * User Preferences object - holds user preferences as they're loaded so they're only loaded once
+	 *	@access private
 	 */
 	var $userprefs;
 
 	/**
 	 * Smarty object - holds reference to the current smarty object -- will not be set in the admin
+	 *	@access private
 	 */
 	var $smarty;
 
 	/**
 	 * Internal error array - So functions/modules can store up debug info and spit it all out at once
+	 *	@access private
 	 */
 	var $errors;
 
 	/**
      * nls array - This holds all of the nls information for different languages
+	 *	@access private
 	 */
 	var $nls;
 
 	/**
      * template cache array - If something's called LoadTemplateByID, we keep a copy around
+	 *	@access private
 	 */
 	var $TemplateCache;
 
 	/**
      * template cache array - If something's called LoadTemplateByID, we keep a copy around
+	 *	@access private
 	 */
 	var $StylesheetCache;
 
 	/**
      * html blob cache array - If something's called LoadHtmlBlobByID, we keep a copy around
+	 *	@access private
 	 */
 	var $HtmlBlobCache;
 	
 	/**
 	 * content types array - List of available content types
+	 *	@access private
 	 */
 	var $contenttypes;
 
@@ -146,25 +163,36 @@ class CmsObject {
 		register_shutdown_function(array(&$this, 'dbshutdown'));
 	}
 
+	/**
+	* Get a handle to the ADODB database object. You can then use this
+	* to perform all kinds of database operations.
+	*
+	* @link http://phplens.com/lens/adodb/docs-adodb.htm
+	* @final
+	* @return a handle to the ADODB database object
+	*/
 	function & GetDb()
 	{
-		#static $dbinstance;
-
-		//Check to see if it hasn't been
-		//instantiated yet.  If not, connect
-		//and return it
-		#if (!isset($dbinstance) && !isset($this->db))
 		global $DONT_LOAD_DB;
+
+		/* Check to see if we have a valid instance.
+		 * If not, build the connection */
 		if (!isset($this->db) && !isset($DONT_LOAD_DB))
 		{
 			$this->db =& adodb_connect();
 		}
-
-		#return $dbinstance;
+		
 		$db =& $this->db;
 		return ($db);
 	}
 
+	/**
+	* Get a handle to the global CMS config. This is mostly values that are
+	* defined in config.php
+	*
+	* @final
+	* @return an associative array of configuration values
+	*/
 	function & GetConfig()
 	{
         if (!isset($this->config))
@@ -176,6 +204,14 @@ class CmsObject {
 		return $this->config;
 	}
 	
+	/**
+	* Get a handle to the CMS ModuleLoader object. If it does not yet,
+	* exist, this method will instantiate it.
+	*
+	* @final
+	* @see ModuleLoader
+	* @return handle to the ModuleLoader object
+	*/
 	function & GetModuleLoader()
 	{
         if (!isset($this->moduleloader))
@@ -188,6 +224,14 @@ class CmsObject {
 		return $this->moduleloader;
 	}
 	
+	/**
+	* Get a handle to the CMS ModuleOperations object. If it does not yet
+	* exist, this method will instantiate it.
+	*
+	* @final
+	* @see ModuleOperations
+	* @return handle to the ModuleOperations object
+	*/
 	function & GetModuleOperations()
 	{
         if (!isset($this->moduleoperations))
@@ -200,6 +244,14 @@ class CmsObject {
 		return $this->moduleoperations;
 	}
 	
+	/**
+	* Get a handle to the CMS UserOperations object. If it does not yet
+	* exist, this method will instantiate it.
+	*
+	* @final
+	* @see UserOperations
+	* @return handle to the UserOperations object
+	*/
 	function & GetUserOperations()
 	{
         if (!isset($this->useroperations))
@@ -212,6 +264,16 @@ class CmsObject {
 		return $this->useroperations;
 	}
 	
+	/**
+	* Get a handle to the CMS ContentOperations object. If it does not yet
+	* exist, this method will instantiate it. To disambiguate, this is a globally-available
+	* object with methods for dealing with Content -- it should not to be confused with
+	* the GlobalContentOperations object.
+	*
+	* @final
+	* @see ContentOperations
+	* @return handle to the ContentOperations object
+	*/
 	function & GetContentOperations()
 	{
         if (!isset($this->contentoperations))
@@ -225,7 +287,15 @@ class CmsObject {
 
 		return $this->contentoperations;
 	}
-	
+
+	/**
+	* Get a handle to the CMS Admin BookmarkOperations object. If it does not yet
+	* exist, this method will instantiate it.
+	*
+	* @final
+	* @see BookmarkOperations
+	* @return handle to the BookmarkOperations object, useful only in the admin
+	*/	
 	function & GetBookmarkOperations()
 	{
         if (!isset($this->bookmarkoperations))
@@ -238,6 +308,14 @@ class CmsObject {
 		return $this->bookmarkoperations;
 	}
 	
+	/**
+	* Get a handle to the CMS TemplateOperations object. If it does not yet
+	* exist, this method will instantiate it.
+	*
+	* @final
+	* @see TemplateOperations
+	* @return handle to the TemplateOperations object
+	*/
 	function & GetTemplateOperations()
 	{
         if (!isset($this->templateoperations))
@@ -249,7 +327,15 @@ class CmsObject {
 
 		return $this->templateoperations;
 	}
-	
+
+	/**
+	* Get a handle to the CMS StylesheetOperations object. If it does not yet
+	* exist, this method will instantiate it.
+	*
+	* @final
+	* @see StylesheetOperations
+	* @return handle to the StylesheetOperations object
+	*/	
 	function & GetStylesheetOperations()
 	{
         if (!isset($this->stylesheetoperations))
@@ -262,6 +348,14 @@ class CmsObject {
 		return $this->stylesheetoperations;
 	}
 	
+	/**
+	* Get a handle to the CMS GroupOperations object. If it does not yet
+	* exist, this method will instantiate it.
+	*
+	* @final
+	* @see GroupOperations
+	* @return handle to the GroupOperations object
+	*/
 	function & GetGroupOperations()
 	{
         if (!isset($this->groupoperations))
@@ -274,6 +368,16 @@ class CmsObject {
 		return $this->groupoperations;
 	}
 	
+	/**
+	* Get a handle to the CMS GlobalContentOperations object. If it does not yet
+	* exist, this method will instantiate it. To disambiguate, this object has methods
+	* for dealing with "Global Content Blocks," not to be confused with the ContentOperations
+	* object available on a global basis from this class.
+	*
+	* @final
+	* @see GlobalContentOperations
+	* @return handle to the GlobalContentOperations object
+	*/
 	function & GetGlobalContentOperations()
 	{
         if (!isset($this->globalcontentoperations))
@@ -286,6 +390,14 @@ class CmsObject {
 		return $this->globalcontentoperations;
 	}
 	
+	/**
+	* Get a handle to the CMS UserTagOperations object. If it does not yet
+	* exist, this method will instantiate it.
+	*
+	* @final
+	* @see UserTagOperations
+	* @return handle to the UserTagOperations object
+	*/
 	function & GetUserTagOperations()
 	{
         if (!isset($this->usertagoperations))
@@ -298,11 +410,20 @@ class CmsObject {
 		return $this->usertagoperations;
 	}
 
+	/**
+	* Get a handle to the CMS Smarty object. If it does not yet
+	* exist, this method will instantiate it. As of version 1.8,
+	* CMS Made Simple uses version 2.6.25 of Smarty.
+	*
+	* @final
+	* @see Smarty_CMS
+	* @link http://www.smarty.net/manual/en/
+	* @return handle to the Smarty object
+	*/
 	function & GetSmarty()
 	{
-		//Check to see if it hasn't been
-		//instantiated yet.  If not, connect
-		//and return it
+		/* Check to see if a Smarty object has been instantiated yet,
+		  and, if not, go ahead an create the instance. */
 		if (!isset($this->smarty))
 		{
 			$conf =& $this->GetConfig();
@@ -312,24 +433,27 @@ class CmsObject {
 				define('SMARTY_DIR', cms_join_path($dirname,'lib','smarty') . DIRECTORY_SEPARATOR);
 			}
 
-			#Setup global smarty object
 			$this->smarty = new Smarty_CMS($conf);
 		}
 
         return $this->smarty;
 	}
 
+	/**
+	* Get a handle to the CMS HierarchyManager object. If it does not yet
+	* exist, this method will instantiate it.
+	*
+	* @final
+	* @see HierarchyManager
+	* @return handle to the HierarchyManager object
+	*/
 	function & GetHierarchyManager()
 	{
-		//Check to see if it hasn't been
-		//instantiated yet.  If not, connect
-		//and return it
+		/* Check to see if a HierarchyManager has been instantiated yet,
+		  and, if not, go ahead an create the instance. */
         if (!isset($this->hrinstance))
 		{
 			debug_buffer('', 'Start Loading Hierarchy Manager');
-			#require_once(dirname(__FILE__).'/class.contenthierarchymanager.inc.php');
-
-			#Setup global smarty object
 			$contentops =& $this->GetContentOperations();
 			$this->hrinstance =& $contentops->GetAllContentAsHierarchy(false, array());
 			debug_buffer('', 'End Loading Hierarchy Manager');
@@ -338,6 +462,12 @@ class CmsObject {
         return $this->hrinstance;
 	}
 
+	/**
+	* Disconnect from the database.
+	*
+	* @final
+	* @access private
+	*/
 	function dbshutdown()
 	{
 		if (isset($this->db))
@@ -349,9 +479,15 @@ class CmsObject {
 	}
 
 
+	/**
+	* Clear out cached files from the CMS tmp/cache and tmp/templates_c
+	* directories.
+	*
+	* @final
+	* @access private
+	*/
 	function clear_cached_files($age_days = -100)
 	{
-	  //if( $age_days <= 0 ) return;
 	  $the_time = time() - $age_days * 24*60*60;
 
 	  $dirs = array(TMP_CACHE_LOCATION,TMP_TEMPLATES_C_LOCATION);
@@ -363,7 +499,6 @@ class CmsObject {
 		{
 		  if( $one->isFile() && $one->getMTime() <= $the_time )
 		    {
-		      //echo $one->getPathname().' '.$one->getMTime().'<br/>';
  		      @unlink($one->getPathname());
 		    }
 		}
@@ -371,6 +506,9 @@ class CmsObject {
 	}
 }
 
+/**
+ * Simple global convenience object to hold CMS route information.
+ */
 class CmsRoute
 {
 	var $module;
@@ -378,6 +516,9 @@ class CmsRoute
 	var $defaults;
 }
 
+/**
+ * Simple global convenience object to hold CMS Content Type structure.
+ */
 class CmsContentTypePlaceholder
 {
 	var $type;
