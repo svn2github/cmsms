034557c57bf662d0f9f2b41dfa225a563af5fd2a
ronnyk <ronnyk@3d254a34-79dc-0310-9e5f-be208747d8a0>
2011-01-19 19:54:24 +0000

    update CHANGELOG.txt
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@6865 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.9.x/version.php
+++ branches/1.9.x/version.php
@@ -18,8 +18,8 @@
 #
 #$Id$
 
-$CMS_VERSION = "1.9.2";
-$CMS_VERSION_NAME = "Tevairoa";
+$CMS_VERSION = "1.9.3";
+$CMS_VERSION_NAME = "No-Name-Set-Yet";
 $CMS_SCHEMA_VERSION = "34";
 
 define('CMS_VERSION', $CMS_VERSION);
