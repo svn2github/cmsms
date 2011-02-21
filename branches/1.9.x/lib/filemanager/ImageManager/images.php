89b92b2759eb43dceac5f53e5b6d415f2af99b6d
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2011-02-21 18:11:12 +0000

    Absolutely do not cache image manager iframe.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@6907 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.9.x/lib/filemanager/ImageManager/images.php
+++ branches/1.9.x/lib/filemanager/ImageManager/images.php
@@ -191,6 +191,12 @@ function drawErrorBase(&$manager)
 <?php
 }
 
+// absolutely disable caching.
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
