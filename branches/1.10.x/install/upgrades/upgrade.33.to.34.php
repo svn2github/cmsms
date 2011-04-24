7223d42e5759d195715a358105d60f3ab8346a05
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2011-04-24 00:21:37 +0000

    Adds new permission to upgrade routine.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@7003 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.10.x/install/upgrades/upgrade.33.to.34.php
+++ branches/1.10.x/install/upgrades/upgrade.33.to.34.php
@@ -1,16 +1,21 @@
 <?php
 global $gCms;
+$dbdict = NewDataDictionary($db);
 
 echo '<p>Adding new field to content table...';
-$dbdict = NewDataDictionary($db);
 $sqlarray = $dbdict->AddColumnSQL(cms_db_prefix().'content','page_url C(255)');
 $dbdict->ExecuteSQLArray($sqlarray);
 echo '[done]</p>';
 
 echo '<p>Adding more fields to CGB table...';
-$dbdict = NewDataDictionary($db);
 $sqlarray = $dbdict->AddColumnSQL(cms_db_prefix().'htmlblobs','description X,use_wysiwyg I1');
 $dbdict->ExecuteSQLArray($sqlarray);
+echo '[done]</p>';
+
+echo '<p>Adding &quot;Reorder Content&quot; permission';
+$nid = $db->GenId()(cms_db_prefix().'permissions_seq');
+$db->Execute('Insert INTO '.cms_db_prefix()."permissions (permission_id,permission_name,permission_text,create_date,modified_date)
+              VALUES (?,?,?,NOW(),NOW())",array($nid,'Reorder Content','Reorder Content'));
 echo '[done]</p>';
 
 echo '<p>Updating schema version... ';
