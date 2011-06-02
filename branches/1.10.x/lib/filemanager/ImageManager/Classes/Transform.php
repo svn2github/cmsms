96f6445a56ea7b070ee8899215deef0ab7ab935b
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2011-06-02 19:05:33 +0000

    Fix E_DEPRECATED error.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@7086 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.10.x/lib/filemanager/ImageManager/Classes/Transform.php
+++ branches/1.10.x/lib/filemanager/ImageManager/Classes/Transform.php
@@ -130,7 +130,7 @@ Class Image_Transform
         include_once (dirname(__FILE__)."/$driver.php");
 
         $classname = "Image_Transform_Driver_{$driver}";
-        $obj =& new $classname;
+        $obj = new $classname;
         return $obj;
     }
 
