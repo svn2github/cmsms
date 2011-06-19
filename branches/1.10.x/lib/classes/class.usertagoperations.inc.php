67c01343136e38017e878a76bb46eaca87f1162f
rolf1 <rolf1@3d254a34-79dc-0310-9e5f-be208747d8a0>
2011-06-19 18:31:50 +0000

    fix typo
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@7115 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.10.x/lib/classes/class.usertagoperations.inc.php
+++ branches/1.10.x/lib/classes/class.usertagoperations.inc.php
@@ -253,7 +253,7 @@ final class UserTagOperations
 		$row = $this->_get_from_cache($name);
 		if( !$row ) return;
 		$functionname = 'cms_user_tag_'.$name;
-		if( !function_exists($unctionname) )
+		if( !function_exists($functionname) )
 		{
 			$code = 'function '.$functionname.'($psrams,&$smarty) {'.$row['code']."\n}";
 			@eval($code);
