5cfd72c3c8161e811afb04676940a6b295f7ca25
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2011-07-29 00:44:46 +0000

    A better summarize modifier (the old one just irritated me).
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@7224 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.10.x/plugins/modifier.summarize.php
+++ branches/1.10.x/plugins/modifier.summarize.php
@@ -10,24 +10,15 @@
  *        http://dev.cmsmadesimple.org/users/marks/
  * ----------------------------------------------------------------
  **/
-function smarty_cms_modifier_summarize($string,$numwords='5',$etc='...'){
-
-//=Put each word (any character or group of characters seperated by a space) into the field's array
-$stringarray = explode(" ",$string);
-
-//=While loop to add int ($numwords) words to the summary string ($returnstring)
-$i = 0;
-$returnstring = '';
-while($i < $numwords){
-    $returnstring .= " ".$stringarray[$i];
-    $i++;
-}
-
-//If set, the suffix (by default "...") will now be added to the summary ($returnstring)
-$returnstring .= $etc;
-
-//Return the summary!
-return trim($returnstring);
-    
+function smarty_cms_modifier_summarize($string,$numwords='5',$etc='...')
+{
+  $stringarray = explode(" ",$str);
+  if( $numwords >= count($stringarray) )
+    {
+      return $str;
+    }
+  $tmp = array_slice($stringarray,0,$numwords);
+  $tmp = implode(' ',$tmp).$etc;
+  return $tmp;    
 }//end of function
 ?>
