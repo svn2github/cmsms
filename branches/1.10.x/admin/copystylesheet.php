669eada48008c38730ff1d47dabfcbcb890f03d2
rolf1 <rolf1@3d254a34-79dc-0310-9e5f-be208747d8a0>
2011-08-28 19:19:06 +0000

    fix typo
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@7365 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.10.x/admin/copystylesheet.php
+++ branches/1.10.x/admin/copystylesheet.php
@@ -78,7 +78,7 @@ if ($access)
 			{
 				// put mention into the admin log
 				audit($onestylesheet->id, 'Stylesheet: '.$onestylesheet->name, 'Copied');
-				redirect("listcss.php".$urlext.'&messagkeye=stylesheetcopied');
+				redirect("listcss.php".$urlext.'&messagekey=stylesheetcopied');
 				return;
 			}
 			else
