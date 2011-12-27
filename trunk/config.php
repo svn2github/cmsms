fb8252d6169ca745b6adf230147f2c9fb8b386ee
Silmarillion <Silmarillion@3d254a34-79dc-0310-9e5f-be208747d8a0>
2011-12-27 09:57:20 +0000

    A bit of work on systemmaintenance.php
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@7605 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/config.php
+++ trunk/config.php
@@ -5,11 +5,12 @@
 $config['dbms'] = 'mysql';
 $config['db_hostname'] = 'localhost';
 $config['db_username'] = 'root';
-$config['db_password'] = 'root';
-$config['db_name'] = '_demos_1.11.x';
+$config['db_password'] = '';
+$config['db_name'] = 'cmstrunk';
 $config['db_prefix'] = 'cms_';
 $config['db_port'] = 0;
-$config['root_url'] = 'http://uniqu3.dyndns-server.com/demos/cms_trunk';
-$config['timezone'] = 'Europe/Vienna';
+$config['root_url'] = 'http://localhost/cms-trunk';
+$config['query_var'] = 'side';
+$config['timezone'] = 'UTC';
 $config['default_encoding'] = 'utf-8';
 ?>
