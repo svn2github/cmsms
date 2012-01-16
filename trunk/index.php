6e8aed7921c84ee69e94e9c10e3edf1932ec1486
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2012-01-16 15:27:01 +0000

    Fixes #7465
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@7679 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/index.php
+++ trunk/index.php
@@ -257,7 +257,7 @@ else
 
 Events::SendEvent('Core', 'ContentPostRender', array('content' => &$html));
 
-$ct = cmsms()->get_variable['content-type'];
+$ct = cmsms()->get_variable('content-type');
 if( !$ct ) $ct = 'text/html';
 header("Content-Type: $ct; charset=" . get_encoding());
 
