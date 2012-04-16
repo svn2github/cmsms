756c34de4966fb3e1e96fcff6e11a299f5fdf7d1
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2012-04-16 15:07:11 +0000

    More checks for loadPlugin
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@7897 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/lib/classes/class.Smarty_CMS.php
+++ trunk/lib/classes/class.Smarty_CMS.php
@@ -101,7 +101,7 @@ class Smarty_CMS extends SmartyBC
 	$this->registerResource('content',new CMSContentTemplateResource());
 	$this->registerResource('htmlblob',new CMSGlobalContentTemplateResource());
 	$this->registerResource('globalcontent',new CMSGlobalContentTemplateResource());
-		
+
 	// just for frontend actions.
 	$this->registerPlugin('function','content','CMS_Content_Block::smarty_fetch_contentblock',false);
 	$this->registerPlugin('function','content_image','CMS_Content_Block::smarty_fetch_imageblock',false);
@@ -377,6 +377,80 @@ class Smarty_CMS extends SmartyBC
 		
 		return $output;
 	}	
+
+
+    /**
+     * Takes unknown classes and loads plugin files for them
+     * class name format: Smarty_PluginType_PluginName
+     * plugin filename format: plugintype.pluginname.php
+     *
+     * Note: this method overrides the one in the smarty base class and provides more testing.
+     *
+     * @param string $plugin_name    class plugin name to load
+     * @param bool   $check          check if already loaded
+     * @return string |boolean filepath of loaded file or false
+     */
+    public function loadPlugin($plugin_name, $check = true)
+    {
+        // if function or class exists, exit silently (already loaded)
+        if ($check && (is_callable($plugin_name) || class_exists($plugin_name, false))) {
+            return true;
+        }
+        // Plugin name is expected to be: Smarty_[Type]_[Name]
+        $_name_parts = explode('_', $plugin_name, 3);
+        // class name must have three parts to be valid plugin
+        // count($_name_parts) < 3 === !isset($_name_parts[2])
+        if (!isset($_name_parts[2]) || strtolower($_name_parts[0]) !== 'smarty') {
+            throw new SmartyException("plugin {$plugin_name} is not a valid name format");
+            return false;
+        }
+        // if type is "internal", get plugin from sysplugins
+        if (strtolower($_name_parts[1]) == 'internal') {
+            $file = SMARTY_SYSPLUGINS_DIR . strtolower($plugin_name) . '.php';
+            if (file_exists($file)) {
+                require_once($file);
+                return $file;
+            } else {
+                return false;
+            }
+        }
+        // plugin filename is expected to be: [type].[name].php
+        $_plugin_filename = "{$_name_parts[1]}.{$_name_parts[2]}.php";
+        
+        $_stream_resolve_include_path = function_exists('stream_resolve_include_path');
+
+        // loop through plugin dirs and find the plugin
+        foreach($this->getPluginsDir() as $_plugin_dir) {
+            $names = array(
+                $_plugin_dir . $_plugin_filename,
+                $_plugin_dir . strtolower($_plugin_filename),
+            );
+            foreach ($names as $file) {
+                if (file_exists($file)) {
+                    require_once($file);
+		    if( is_callable($plugin_name) || class_exists($plugin_name, false) )
+		      return $file;
+                }
+                if ($this->use_include_path && !preg_match('/^([\/\\\\]|[a-zA-Z]:[\/\\\\])/', $_plugin_dir)) {
+                    // try PHP include_path
+                    if ($_stream_resolve_include_path) {
+                        $file = stream_resolve_include_path($file);
+                    } else {
+                        $file = Smarty_Internal_Get_Include_Path::getIncludePath($file);
+                    }
+                    
+                    if ($file !== false) {
+                        require_once($file);
+			if( is_callable($plugin_name) || class_exists($plugin_name, false) )
+			  return $file;
+                    }
+                }
+            }
+        }
+        // no plugin loaded
+        return false;
+    }
+
 } // end of class
 
 ?>
