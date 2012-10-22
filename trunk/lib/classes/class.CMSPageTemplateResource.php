2931aaf79c5d8dc81464c62e47eae67585cf287e
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2012-10-22 19:35:09 +0000

    Fixes to editing templates when site down is enabled.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@8402 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/lib/classes/class.CMSPageTemplateResource.php
+++ trunk/lib/classes/class.CMSPageTemplateResource.php
@@ -82,33 +82,28 @@ class CMSPageTemplateResource extends CMS_Fixed_Resource_Custom
 
   protected function fetch($name,&$source,&$mtime)
   {
-    if( is_sitedown() )
-      {
-	$source = '';
-	$mtime = time();
-	if( $this->_section == 'body' ) {
-	  header('HTTP/1.0 503 Service Unavailable');
-	  header('Status: 503 Service Unavailable');
-	  
-	  $source = get_site_preference('sitedownmessage');
-	}
-	return;
+    if( is_sitedown() && cmsms()->is_frontend_request() ) {
+      $source = '';
+      $mtime = time();
+      if( $this->_section == 'body' ) {
+	header('HTTP/1.0 503 Service Unavailable');
+	header('Status: 503 Service Unavailable');
+	$source = get_site_preference('sitedownmessage');
       }
+      return;
+    }
 
-    if( $name == 'notemplate' )
-      {
-	$source = '{content}';
-	$mtime = time(); // never cache...
-	return;
-      }
-    else if( startswith($name,'appdata;') )
-      {
-	$name = substr($name,8);
-	$source = cms_utils::get_app_data($name);
-	$mtime = time();
-	return;
-      }
-
+    if( $name == 'notemplate' ) {
+      $source = '{content}';
+      $mtime = time(); // never cache...
+      return;
+    }
+    else if( startswith($name,'appdata;') ) {
+      $name = substr($name,8);
+      $source = cms_utils::get_app_data($name);
+      $mtime = time();
+      return;
+    }
 
     $source = '';
     $mtime = null;
@@ -116,41 +111,38 @@ class CMSPageTemplateResource extends CMS_Fixed_Resource_Custom
     $tpl = $this->get_template($name);
     if( !is_object($tpl) ) return;
 
-    switch( $this->_section )
-      {
-      case 'top':
-	$mtime = $tpl->modified_date;
-	$pos = stripos($tpl->content,'<head');
-	if( $pos === FALSE ) return;
-	$source = substr($tpl->content,0,$pos);
-	return;
+    switch( $this->_section ) {
+    case 'top':
+      $mtime = $tpl->modified_date;
+      $pos = stripos($tpl->content,'<head');
+      if( $pos === FALSE ) return;
+      $source = substr($tpl->content,0,$pos);
+      return;
 
-      case 'head':
-	$mtime = $tpl->modified_date;
-	$pos1 = stripos($tpl->content,'<head');
-	$pos2 = stripos($tpl->content,'</head>');
-	if( $pos1 === FALSE || $pos2 === FALSE ) return;
-	$source = substr($tpl->content,$pos1,$pos2-$pos1+7);
-	return;
+    case 'head':
+      $mtime = $tpl->modified_date;
+      $pos1 = stripos($tpl->content,'<head');
+      $pos2 = stripos($tpl->content,'</head>');
+      if( $pos1 === FALSE || $pos2 === FALSE ) return;
+      $source = substr($tpl->content,$pos1,$pos2-$pos1+7);
+      return;
 
-      case 'body':
-	$mtime = $tpl->modified_date;
-	$pos = stripos($tpl->content,'</head>');
-	if( $pos !== FALSE )
-	  {
-	    $source = substr($tpl->content,$pos+7);
-	  }
-	else
-	  {
-	    $source = $tpl->content;
-	  }
-	return;
-
-      default:
-	$source = $tpl->content;
-	$mtime = $tpl->modified_date;
-	return;
+    case 'body':
+      $mtime = $tpl->modified_date;
+      $pos = stripos($tpl->content,'</head>');
+      if( $pos !== FALSE ) {
+	$source = substr($tpl->content,$pos+7);
       }
+      else {
+	$source = $tpl->content;
+      }
+      return;
+
+    default:
+      $source = $tpl->content;
+      $mtime = $tpl->modified_date;
+      return;
+    }
   }
 } // end of class
 
