376c840885958dace221a6cdf51dfc75146b5f6b
stikki <stikki@3d254a34-79dc-0310-9e5f-be208747d8a0>
2012-12-15 14:41:32 +0000

    Fixes problem with Events listing none active modules.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@8571 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/lib/classes/class.Events.php
+++ trunk/lib/classes/class.Events.php
@@ -196,7 +196,7 @@ final class Events
 		$gCms = cmsms();
 		$db = $gCms->GetDb();
 
-		$q = "SELECT * FROM ".cms_db_prefix()."events ORDER BY originator,event_name";
+		//$q = "SELECT * FROM ".cms_db_prefix()."events ORDER BY originator,event_name";
 		$q = 'SELECT e.*, count(eh.event_id) as usage_count FROM '.cms_db_prefix().
 			'events e left outer join '.cms_db_prefix().
 			'event_handlers eh on e.event_id=eh.event_id GROUP BY e.event_id ORDER BY originator,event_name';
@@ -210,6 +210,9 @@ final class Events
 		$result = array();
 		while( $row = $dbresult->FetchRow() )
 		{
+			if(!cms_utils::module_available($row['originator']) && $row['originator'] !== 'Core')
+				continue;
+				
 			$result[] = $row;
 		}
 		return $result;
