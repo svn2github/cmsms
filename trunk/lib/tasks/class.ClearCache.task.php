f78a109f1b3ddb4feca27a0e002334600f9591d3
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2012-12-30 16:33:20 +0000

    Nope... put em back.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@8610 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/lib/tasks/class.ClearCache.task.php
+++ trunk/lib/tasks/class.ClearCache.task.php
@@ -26,7 +26,7 @@ class ClearCacheTask implements CmsRegularTask
     // we only do it daily.
     if( !$time ) $time = time();
     $last_execute = get_site_preference(self::LASTEXECUTE_SITEPREF,0);
-    if( ($time - 24*60*60) <= $last_execute ) {
+    if( ($time - 24*60*60) >= $last_execute ) {
       return TRUE;
     } 
     return FALSE;
