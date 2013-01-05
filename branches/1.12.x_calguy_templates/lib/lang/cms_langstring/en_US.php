cd0f5c285b70bff0548d5ecbc5b931f8b0298b06
rolf1 <rolf1@3d254a34-79dc-0310-9e5f-be208747d8a0>
2013-01-05 13:38:42 +0000

    Global CMSMS Languagestrings
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@8619 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ branches/1.12.x_calguy_templates/lib/lang/cms_langstring/en_US.php
@@ -0,0 +1,376 @@
+<?php
+$lang['no']='No';
+$lang['yes']='Yes';
+$lang['none'] = 'None';
+
+$lang['left'] = 'Left';
+$lang['right'] = 'Right';
+$lang['up'] = 'Up';
+$lang['down'] = 'Down';
+
+$lang['add'] = 'Add';
+$lang['edit'] = 'Edit';
+$lang['delete'] = 'Delete';
+$lang['areyousure'] = 'Are you sure?';
+$lang['submit'] = 'Submit';
+$lang['apply'] = 'Apply';
+$lang['save'] = 'Save';
+$lang['cancel'] = 'Cancel';
+$lang['select_one'] = 'Select One';
+$lang['available'] = 'Available';
+$lang['selected'] = 'Selected';
+
+$lang['image'] = 'Image';
+$lang['file'] = 'File';
+$lang['folder'] = 'Folder';
+
+$lang['group'] = 'Group';
+
+/**
+ * Modules
+ */
+$lang['installed'] = 'The module is installed';
+$lang['upgraded'] = 'The module is upgraded';
+$lang['uninstalled'] = 'The module is uninstalled';
+
+/**
+ * Templates
+ */
+$lang['template'] = 'Template';
+$lang['templatename'] = 'Template Name';
+$lang['edittemplate'] = 'Edit Template';
+$lang['deletetemplate'] = 'Delete Template';
+$lang['newtemplate'] = 'New Template';
+$lang['default'] = 'Default';
+$lang['resettofactory'] = 'Reset to factory defaults';
+
+/**
+ * Errors
+ */
+$lang['error_missingparam'] = 'A required parameter is missing or invalid';
+
+/**
+ * Time
+ */
+$lang['second']="second";
+$lang['Second']="Second";
+$lang['seconds']="seconds";
+$lang['Seconds']="Seconds";
+$lang['minute']="minute";
+$lang['Minute']="Minute";
+$lang['minutes']="minutes";
+$lang['Minutes']="Minutes";
+$lang['hour']="hour";
+$lang['Hour']="Hour";
+$lang['hours']="hours";
+$lang['Hours']="Hours";
+$lang['day']="day";
+$lang['Day']="Day";
+$lang['days']="days";
+$lang['Days']="Days";
+$lang['week']="week";
+$lang['Week']="Week";
+$lang['weeks']="weeks";
+$lang['Weeks']="Weeks";
+$lang['month']="month";
+$lang['Month']="Month";
+$lang['months']="months";
+$lang['Months']="Months";
+$lang['year']="year";
+$lang['Year']="Year";
+$lang['years']="years";
+$lang['Years']="Years";
+
+/**
+ * Days
+ */
+$lang['monday']="monday";
+$lang['tuesday']="tuesday";
+$lang['wednesday']="wednesday";
+$lang['thursday']="thursday";
+$lang['friday']="friday";
+$lang['saturday']="saturday";
+$lang['sunday']="sunday";
+
+/**
+ * Months
+ */
+$lang['january']='january';
+$lang['february']='february';
+$lang['march']='march';
+$lang['april']='april';
+$lang['may']='may';
+$lang['june']='june';
+$lang['july']='july';
+$lang['august']='august';
+$lang['september']='september';
+$lang['october']='october';
+$lang['november']='november';
+$lang['december']='december';
+
+/**
+ * Top-level Domains
+ */
+$lang["country_COM"]="Commercial (.com)";
+$lang["country_GOV"]="Governmental (.gov)";
+$lang["country_ORG"]="Organization (.org)";
+$lang["country_EDU"]="Educational (.edu)";
+$lang["country_MIL"]="Millitary (.mil)";
+$lang["country_NAME"]="Name (.name)";
+$lang["country_NET"]="Network (.net)";
+$lang["country_BIZ"]="Business (.biz)";
+$lang["country_INFO"]="Information (.info)";
+$lang["country_EU"]="European (.eu)";
+$lang["country_LOCALHOST"]="Localhost";
+
+/**
+ * Countries
+ */
+$lang["country_AD"]="Andorra";
+$lang["country_AE"]="United Arab Emirates";
+$lang["country_AF"]="Afghanistan";
+$lang["country_AG"]="Antigua and Barbuda";
+$lang["country_AI"]="Anguilla";
+$lang["country_AL"]="Albania";
+$lang["country_AM"]="Armenia";
+$lang["country_AN"]="Netherlands Antilles";
+$lang["country_AO"]="Angola";
+$lang["country_AQ"]="Antarctica";
+$lang["country_AR"]="Argentina";
+$lang["country_AS"]="American Samoa";
+$lang["country_AT"]="Austria";
+$lang["country_AU"]="Australia";
+$lang["country_AW"]="Aruba";
+$lang["country_AX"]="Aland Islands";
+$lang["country_AZ"]="Azerbaijan";
+$lang["country_BA"]="Bosnia and Herzegovina";
+$lang["country_BB"]="Barbados";
+$lang["country_BD"]="Bangladesh";
+$lang["country_BE"]="Belgium";
+$lang["country_BF"]="Burkina Faso";
+$lang["country_BG"]="Bulgaria";
+$lang["country_BH"]="Bahrain";
+$lang["country_BI"]="Burundi";
+$lang["country_BJ"]="Benin";
+$lang["country_BL"]="Saint Barthelemy";
+$lang["country_BM"]="Bermuda";
+$lang["country_BN"]="Brunei Darussalam";
+$lang["country_BO"]="Bolivia";
+$lang["country_BR"]="Brazil";
+$lang["country_BS"]="Bahamas";
+$lang["country_BT"]="Bhutan";
+$lang["country_BV"]="Bouvet Island";
+$lang["country_BW"]="Botswana";
+$lang["country_BY"]="Belarus";
+$lang["country_BZ"]="Belize";
+$lang["country_CA"]="Canada";
+$lang["country_CC"]="Cocos (Keeling) Islands";
+$lang["country_CD"]="Congo, Democratic Republic of the";
+$lang["country_CF"]="Central African Republic";
+$lang["country_CG"]="Congo";
+$lang["country_CH"]="Switzerland";
+$lang["country_CI"]="Cote d\'Ivoire";
+$lang["country_CK"]="Cook Islands";
+$lang["country_CL"]="Chile";
+$lang["country_CM"]="Cameroon";
+$lang["country_CN"]="China";
+$lang["country_CO"]="Colombia";
+$lang["country_CR"]="Costa Rica";
+$lang["country_CU"]="Cuba";
+$lang["country_CV"]="Cape Verde";
+$lang["country_CX"]="Christmas Island";
+$lang["country_CY"]="Cyprus";
+$lang["country_CZ"]="Czech Republic";
+$lang["country_DE"]="Germany";
+$lang["country_DJ"]="Djibouti";
+$lang["country_DK"]="Denmark";
+$lang["country_DM"]="Dominica";
+$lang["country_DO"]="Dominican Republic";
+$lang["country_DZ"]="Algeria";
+$lang["country_EC"]="Ecuador";
+$lang["country_EE"]="Estonia";
+$lang["country_EG"]="Egypt";
+$lang["country_EH"]="Western Sahara";
+$lang["country_ER"]="Eritrea";
+$lang["country_ES"]="Spain";
+$lang["country_ET"]="Ethiopia";
+$lang["country_FI"]="Finland";
+$lang["country_FJ"]="Fiji";
+$lang["country_FK"]="Falkland Islands (Malvinas)";
+$lang["country_FM"]="Micronesia, Federated States of";
+$lang["country_FO"]="Faroe Islands";
+$lang["country_FR"]="France";
+$lang["country_GA"]="Gabon";
+$lang["country_GB"]="United Kingdom";
+$lang["country_GD"]="Grenada";
+$lang["country_GE"]="Georgia";
+$lang["country_GF"]="French Guiana";
+$lang["country_GG"]="Guernsey";
+$lang["country_GH"]="Ghana";
+$lang["country_GI"]="Gibraltar";
+$lang["country_GL"]="Greenland";
+$lang["country_GM"]="Gambia";
+$lang["country_GN"]="Guinea";
+$lang["country_GP"]="Guadeloupe";
+$lang["country_GQ"]="Equatorial Guinea";
+$lang["country_GR"]="Greece";
+$lang["country_GS"]="South Georgia and the South Sandwich Islands";
+$lang["country_GT"]="Guatemala";
+$lang["country_GU"]="Guam";
+$lang["country_GW"]="Guinea-Bissau";
+$lang["country_GY"]="Guyana";
+$lang["country_HK"]="Hong Kong";
+$lang["country_HM"]="Heard Island and McDonald Islands";
+$lang["country_HN"]="Honduras";
+$lang["country_HR"]="Croatia";
+$lang["country_HT"]="Haiti";
+$lang["country_HU"]="Hungary";
+$lang["country_ID"]="Indonesia";
+$lang["country_IE"]="Ireland";
+$lang["country_IL"]="Israel";
+$lang["country_IM"]="Isle of Man";
+$lang["country_IN"]="India";
+$lang["country_IO"]="British Indian Ocean Territory";
+$lang["country_IQ"]="Iraq";
+$lang["country_IR"]="Iran, Islamic Republic of";
+$lang["country_IS"]="Iceland";
+$lang["country_IT"]="Italy";
+$lang["country_JE"]="Jersey";
+$lang["country_JM"]="Jamaica";
+$lang["country_JO"]="Jordan";
+$lang["country_JP"]="Japan";
+$lang["country_KE"]="Kenya";
+$lang["country_KG"]="Kyrgyzstan";
+$lang["country_KH"]="Cambodia";
+$lang["country_KI"]="Kiribati";
+$lang["country_KM"]="Comoros";
+$lang["country_KN"]="Saint Kitts and Nevis";
+$lang["country_KP"]="Korea, Democratic People\'s Republic of";
+$lang["country_KR"]="Korea, Republic of";
+$lang["country_KW"]="Kuwait";
+$lang["country_KY"]="Cayman Islands";
+$lang["country_KZ"]="Kazakhstan";
+$lang["country_LA"]="Lao People\'s Democratic Republic";
+$lang["country_LB"]="Lebanon";
+$lang["country_LC"]="Saint Lucia";
+$lang["country_LI"]="Liechtenstein";
+$lang["country_LK"]="Sri Lanka";
+$lang["country_LR"]="Liberia";
+$lang["country_LS"]="Lesotho";
+$lang["country_LT"]="Lithuania";
+$lang["country_LU"]="Luxembourg";
+$lang["country_LV"]="Latvia";
+$lang["country_LY"]="Libyan Arab Jamahiriya";
+$lang["country_MA"]="Morocco";
+$lang["country_MC"]="Monaco";
+$lang["country_MD"]="Moldova";
+$lang["country_ME"]="Montenegro";
+$lang["country_MF"]="Saint Martin (French part)";
+$lang["country_MG"]="Madagascar";
+$lang["country_MH"]="Marshall Islands";
+$lang["country_MK"]="Macedonia, the former Yugoslav Republic of";
+$lang["country_ML"]="Mali";
+$lang["country_MM"]="Myanmar";
+$lang["country_MN"]="Mongolia";
+$lang["country_MO"]="Macao";
+$lang["country_MP"]="Northern Mariana Islands";
+$lang["country_MQ"]="Martinique";
+$lang["country_MR"]="Mauritania";
+$lang["country_MS"]="Montserrat";
+$lang["country_MT"]="Malta";
+$lang["country_MU"]="Mauritius";
+$lang["country_MV"]="Maldives";
+$lang["country_MW"]="Malawi";
+$lang["country_MX"]="Mexico";
+$lang["country_MY"]="Malaysia";
+$lang["country_MZ"]="Mozambique";
+$lang["country_NA"]="Namibia";
+$lang["country_NC"]="New Caledonia";
+$lang["country_NE"]="Niger";
+$lang["country_NF"]="Norfolk Island";
+$lang["country_NG"]="Nigeria";
+$lang["country_NI"]="Nicaragua";
+$lang["country_NL"]="Netherlands";
+$lang["country_NO"]="Norway";
+$lang["country_NP"]="Nepal";
+$lang["country_NR"]="Nauru";
+$lang["country_NU"]="Niue";
+$lang["country_NZ"]="New Zealand";
+$lang["country_OM"]="Oman";
+$lang["country_PA"]="Panama";
+$lang["country_PE"]="Peru";
+$lang["country_PF"]="French Polynesia";
+$lang["country_PG"]="Papua New Guinea";
+$lang["country_PH"]="Philippines";
+$lang["country_PK"]="Pakistan";
+$lang["country_PL"]="Poland";
+$lang["country_PM"]="Saint Pierre and Miquelon";
+$lang["country_PN"]="Pitcairn";
+$lang["country_PR"]="Puerto Rico";
+$lang["country_PS"]="Palestinian Territory, Occupied";
+$lang["country_PT"]="Portugal";
+$lang["country_PW"]="Palau";
+$lang["country_PY"]="Paraguay";
+$lang["country_QA"]="Qatar";
+$lang["country_RE"]="Reunion";
+$lang["country_RO"]="Romania";
+$lang["country_RS"]="Serbia";
+$lang["country_RU"]="Russian Federation";
+$lang["country_RW"]="Rwanda";
+$lang["country_SA"]="Saudi Arabia";
+$lang["country_SB"]="Solomon Islands";
+$lang["country_SC"]="Seychelles";
+$lang["country_SD"]="Sudan";
+$lang["country_SE"]="Sweden";
+$lang["country_SG"]="Singapore";
+$lang["country_SH"]="Saint Helena";
+$lang["country_SI"]="Slovenia";
+$lang["country_SJ"]="Svalbard and Jan Mayen";
+$lang["country_SK"]="Slovakia";
+$lang["country_SL"]="Sierra Leone";
+$lang["country_SM"]="San Marino";
+$lang["country_SN"]="Senegal";
+$lang["country_SO"]="Somalia";
+$lang["country_SR"]="Suriname";
+$lang["country_ST"]="Sao Tome and Principe";
+$lang["country_SV"]="El Salvador";
+$lang["country_SY"]="Syrian Arab Republic";
+$lang["country_SZ"]="Swaziland";
+$lang["country_TC"]="Turks and Caicos Islands";
+$lang["country_TD"]="Chad";
+$lang["country_TF"]="French Southern Territories";
+$lang["country_TG"]="Togo";
+$lang["country_TH"]="Thailand";
+$lang["country_TJ"]="Tajikistan";
+$lang["country_TK"]="Tokelau";
+$lang["country_TL"]="Timor-Leste";
+$lang["country_TM"]="Turkmenistan";
+$lang["country_TN"]="Tunisia";
+$lang["country_TO"]="Tonga";
+$lang["country_TR"]="Turkey";
+$lang["country_TT"]="Trinidad and Tobago";
+$lang["country_TV"]="Tuvalu";
+$lang["country_TW"]="Taiwan, Province of China";
+$lang["country_TZ"]="Tanzania, United Republic of";
+$lang["country_UA"]="Ukraine";
+$lang["country_UG"]="Uganda";
+$lang["country_UK"]="United Kingdom";
+$lang["country_UM"]="United States Minor Outlying Islands";
+$lang["country_US"]="United States";
+$lang["country_UY"]="Uruguay";
+$lang["country_UZ"]="Uzbekistan";
+$lang["country_VA"]="Holy See (Vatican City State)";
+$lang["country_VC"]="Saint Vincent and the Grenadines";
+$lang["country_VE"]="Venezuela";
+$lang["country_VG"]="Virgin Islands, British";
+$lang["country_VI"]="Virgin Islands, U.S.";
+$lang["country_VN"]="Viet Nam";
+$lang["country_VU"]="Vanuatu";
+$lang["country_WF"]="Wallis and Futuna";
+$lang["country_WS"]="Samoa";
+$lang["country_YE"]="Yemen";
+$lang["country_YT"]="Mayotte";
+$lang["country_ZA"]="South Africa";
+$lang["country_ZM"]="Zambia";
+$lang["country_ZW"]="Zimbabwe";
+?>
