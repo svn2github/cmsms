eb3f58c4cc001e11fdbe2b2776cc470ddcceaf2a
rolf1 <rolf1@3d254a34-79dc-0310-9e5f-be208747d8a0>
2013-01-23 18:38:21 +0000

    cleanup work
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@8654 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.12.x_calguy_templates/admin/plugins/function.syntax_area.php
+++ branches/1.12.x_calguy_templates/admin/plugins/function.syntax_area.php
@@ -1,73 +1,99 @@
 <?php
+#CMS - CMS Made Simple
+#(c)2004 by Ted Kulp (wishy@users.sf.net)
+#This project's homepage is: http://www.cmsmadesimple.org
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 function smarty_function_syntax_area($params, &$template)
 {
-  $smarty   = $template->smarty;
+	$smarty = $template->smarty;
 
-  if( !isset($params['name']) )
-    throw new CmsInvalidDataExtension('syntax_area misssing parameter: name');
+	if( !isset($params['name']) )
+		throw new CmsInvalidDataExtension('syntax_area misssing parameter: name');
 
-  $name = trim($params['name']);
-  if( isset($params['prefix']) ) {
-    $name = trim($params['prefix']).$name;
-  }
+	$name = trim($params['name']);
+	if( isset($params['prefix']) )
+	{
+		$name = trim($params['prefix']).$name;
+	}
 
-  $value = '';
-  if( isset($params['value']) ) {
-    $value = trim($params['value']);
-  }
+	$value = '';
+	if( isset($params['value']) )
+	{
+		$value = trim($params['value']);
+	}
 
-  $id = '';
-  if( isset($params['id']) ) {
-    $id = trim($params['id']);
-  }
+	$id = '';
+	if( isset($params['id']) )
+	{
+		$id = trim($params['id']);
+	}
 
-  $class = '';
-  if( isset($params['class']) ) {
-    $class = trim($params['class']);
-  }
+	$class = '';
+	if( isset($params['class']) )
+	{
+		$class = trim($params['class']);
+	}
 
-  $type = 'html';
-  if( isset($params['type']) ) {
-    $type = trim($params['type']);
-  }
+	$type = 'html';
+	if( isset($params['type']) )
+	{
+		$type = trim($params['type']);
+	}
 
-  $rows = 15;
-  $cols = 80;
-  if( isset($params['rows']) ) {
-    $rows = (int)$params['rows'];
-  }
-  if( isset($params['cols']) ) {
-    $rows = (int)$params['cols'];
-  }
+	$rows = 15;
+	$cols = 80;
+	if( isset($params['rows']) )
+	{
+		$rows = (int)$params['rows'];
+	}
+	if( isset($params['cols']) )
+	{
+		$rows = (int)$params['cols'];
+	}
+	
+	$out = null;
+	$userid = get_userid(FALSE);
+	$syntaxhighlighter = get_preference($userid, 'syntaxhighlighter');
+	if( $syntaxhighlighter )
+	{
+		$mod = cms_utils::get_module($syntaxhighlighter);
+		if( $mod )
+		{
+			// we want a syntax highlighter, we have a preferred one and could find the module
+			$out = $mod->SyntaxTextArea($name,$type,$cols,$rows,'',$value);
+		}
+	}
 
-  $out = null;
-  $userid = get_userid(FALSE);
-  $syntaxhighlighter = get_preference($userid, 'syntaxhighlighter');
-  if( $syntaxhighlighter ) {
-    $mod = cms_utils::get_module($syntaxhighlighter);
-    if( $mod ) {
-      // we want a syntax highlighter, we have a preferred one, and could find the module
-      $out = $mod->SyntaxTextArea($name,$type,$cols,$rows,'',$value);
-    }
-  }
+	if( !$out )
+	{
+		$out = '<textarea name="'.$name.'"';
+		if( $id ) $out .= ' id="'.$id.'"';
+		if( $class ) $out .= ' class="'.$class.'"';
+		if( $rows ) $out .= ' rows="'.$rows.'"';
+		if( $cols ) $out .= ' cols="'.$cols.'"';
+		$out .= '>'.$value.'</textarea>';
+	}
 
-  if( !$out ) {
-    $out = '<textarea name="'.$name.'"';
-    if( $id ) $out .= ' id="'.$id.'"';
-    if( $class ) $out .= ' class="'.$class.'"';
-    if( $rows ) $out .= ' rows="'.$rows.'"';
-    if( $cols ) $out .= ' cols="'.$cols.'"';
-    $out .= '>'.$value.'</textarea>';
-  }
-
-  if( isset($params['assign']) ) {
-    $smarty->assign(trim($params['assign']),$out);
-    return;
-  }
-  return $out;
+	if( isset($params['assign']) )
+	{
+		$smarty->assign(trim($params['assign']),$out);
+		return;
+	}
+	return $out;
 }
-
 #
 # EOF
 #
