9a17914add8ec38e24ef2e68f10c6798297bf55f
stikki <stikki@3d254a34-79dc-0310-9e5f-be208747d8a0>
2013-03-23 14:36:43 +0000

    Fixes problem where CMSMS internal parser mixed <head to <header.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@8738 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/lib/classes/class.CMSPageTemplateResource.php
+++ trunk/lib/classes/class.CMSPageTemplateResource.php
@@ -34,116 +34,134 @@
  */
 class CMSPageTemplateResource extends CMS_Fixed_Resource_Custom
 {
-  static private $_templates;
-  private $_section;
+	static private $_templates;
+	private $_section;
 
-  public function __construct($section = '')
-  {
-    if( in_array($section,array('top','head','body')) )
-      {
-	$this->_section = $section;
-      }
-  }
-
-  protected function buildUniqueResourceName(Smarty $smarty,$resource_name)
-  {
-    return parent::buildUniqueResourceName($smarty,$resource_name).'--'.$this->_section;
-  }
-
-  private function get_template($name)
-  {
-    if( !is_array(self::$_templates) || !isset(self::$_templates[$name]) )
-      {
-	if( !is_array(self::$_templates) ) self::$_templates = array();
-
-	$templateops = cmsms()->GetTemplateOperations();
-	if( $name == -1 ) {
-	  $templateobj = $templateops->LoadDefaultTemplate();
-	}
-	else {
-	  $templateobj = $templateops->LoadTemplateByID($name);
+	public function __construct($section = '')
+	{
+		if( in_array($section,array('top','head','body')) ) 
+		{
+			$this->_section = $section;
+		}
 	}
 
-	if( is_object($templateobj) && $templateobj !== FALSE )
-	  {
-	    $name = $templateobj->name;
-	    self::$_templates[$name] = $templateobj;
-	  }
-	else
-	  {
-	    debug_display('no template '.$name);
-	  }
-      }
-    if( isset(self::$_templates[$name]) )
-      {
-	return self::$_templates[$name];
-      }
-  }
+	protected function buildUniqueResourceName(Smarty $smarty,$resource_name)
+	{
+		return parent::buildUniqueResourceName($smarty,$resource_name).'--'.$this->_section;
+	}
 
-  protected function fetch($name,&$source,&$mtime)
-  {
-    if( is_sitedown() && cmsms()->is_frontend_request() ) {
-      $source = '';
-      $mtime = time();
-      if( $this->_section == 'body' ) {
-	header('HTTP/1.0 503 Service Unavailable');
-	header('Status: 503 Service Unavailable');
-	$source = get_site_preference('sitedownmessage');
-      }
-      return;
-    }
+	private function get_template($name)
+	{
+		if( !is_array(self::$_templates) || !isset(self::$_templates[$name]) )
+		{
+			if( !is_array(self::$_templates) ) self::$_templates = array();
 
-    if( $name == 'notemplate' ) {
-      $source = '{content}';
-      $mtime = time(); // never cache...
-      return;
-    }
-    else if( startswith($name,'appdata;') ) {
-      $name = substr($name,8);
-      $source = cms_utils::get_app_data($name);
-      $mtime = time();
-      return;
-    }
+			$templateops = cmsms()->GetTemplateOperations();
+			if( $name == -1 ) {
+				$templateobj = $templateops->LoadDefaultTemplate();
+			}
+			else {
+				$templateobj = $templateops->LoadTemplateByID($name);
+			}
 
-    $source = '';
-    $mtime = null;
-    
-    $tpl = $this->get_template($name);
-    if( !is_object($tpl) ) return;
+			if( is_object($templateobj) && $templateobj !== FALSE )
+			{
+				$name = $templateobj->name;
+				self::$_templates[$name] = $templateobj;
+			}
+			else {
+				debug_display('no template '.$name);
+			}
+		}
+		
+		if( isset(self::$_templates[$name]) )
+		{
+			return self::$_templates[$name];
+		}
+	}
 
-    switch( $this->_section ) {
-    case 'top':
-      $mtime = $tpl->modified_date;
-      $pos = stripos($tpl->content,'<head');
-      if( $pos === FALSE ) return;
-      $source = substr($tpl->content,0,$pos);
-      return;
+	protected function fetch($name,&$source,&$mtime)
+	{
+		if( is_sitedown() && cmsms()->is_frontend_request() ) 
+		{
+			$source = '';
+			$mtime = time();
+			if( $this->_section == 'body' ) 
+			{
+				header('HTTP/1.0 503 Service Unavailable');
+				header('Status: 503 Service Unavailable');
+				$source = get_site_preference('sitedownmessage');
+			}
+			
+			return;
+		}
 
-    case 'head':
-      $mtime = $tpl->modified_date;
-      $pos1 = stripos($tpl->content,'<head');
-      $pos2 = stripos($tpl->content,'</head>');
-      if( $pos1 === FALSE || $pos2 === FALSE ) return;
-      $source = substr($tpl->content,$pos1,$pos2-$pos1+7);
-      return;
+		if( $name == 'notemplate' ) 
+		{
+			$source = '{content}';
+			$mtime = time(); // never cache...
+			return;
+		}
+		else if( startswith($name,'appdata;') ) {
+			$name = substr($name,8);
+			$source = cms_utils::get_app_data($name);
+			$mtime = time();
+			return;
+		}
 
-    case 'body':
-      $mtime = $tpl->modified_date;
-      $pos = stripos($tpl->content,'</head>');
-      if( $pos !== FALSE ) {
-	$source = substr($tpl->content,$pos+7);
-      }
-      else {
-	$source = $tpl->content;
-      }
-      return;
+		$source = '';
+		$mtime = null;
 
-    default:
-      $source = $tpl->content;
-      $mtime = $tpl->modified_date;
-      return;
-    }
-  }
+		$tpl = $this->get_template($name);
+		if( !is_object($tpl) ) return;
+
+		// Make sure that <head or </head won't collapse
+		$compare_content = $tpl->content;
+		$compare_content = preg_replace('/<(head|\/head).+\b[^>]*>/', '##DUMMY##', $compare_content);
+		
+		// Get section, do magic.
+		switch( $this->_section ) {
+		
+			case 'top':
+			
+				$mtime = $tpl->modified_date;
+				$pos = stripos($compare_content,'<head');
+				if( $pos === FALSE ) return;
+				$source = substr($tpl->content,0,$pos);
+	
+				return;
+
+			case 'head':
+			
+				$mtime = $tpl->modified_date;
+				$pos1 = stripos($compare_content,'<head');
+				$pos2 = stripos($compare_content,'</head>');
+				if( $pos1 === FALSE || $pos2 === FALSE ) return;
+				$source = substr($tpl->content,$pos1,$pos2-$pos1+7);
+				
+				return;
+
+			case 'body':
+			
+				$mtime = $tpl->modified_date;
+				$pos = stripos($compare_content,'</head>');
+				if( $pos !== FALSE ) {
+					$source = substr($tpl->content,$pos+7);
+				}
+				else {
+					$source = $tpl->content;
+				}
+				
+				return;
+
+			default:
+			
+				$source = $tpl->content;
+				$mtime = $tpl->modified_date;
+				
+				return;
+		}
+	}
 } // end of class
 
 #
