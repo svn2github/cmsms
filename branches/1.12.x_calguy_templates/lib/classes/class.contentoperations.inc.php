651bf60237e590cd71132788c113c3ecc217c5e7
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2013-09-21 16:56:07 +0000

    minor.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@8981 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.12.x_calguy_templates/lib/classes/class.contentoperations.inc.php
+++ branches/1.12.x_calguy_templates/lib/classes/class.contentoperations.inc.php
@@ -569,7 +569,7 @@ class ContentOperations
 				for( $i = 0; $i < count($tmp); $i++ ) {
 					$content_id = $tmp[$i]['content_id'];
 					if( in_array($content_id,$child_ids) ) {
-						if( !is_array($contentprops[$content_id]) ) $contentprops[$content_id] = array();
+						if( !isset($contentprops[$content_id]) ) $contentprops[$content_id] = array();
 						$contentprops[$content_id] = $tmp[$i];
 					}
 				}
@@ -667,7 +667,7 @@ class ContentOperations
 				for( $i = 0; $i < count($tmp); $i++ ) {
 					$content_id = $tmp[$i]['content_id'];
 					if( in_array($content_id,$child_ids) ) {
-						if( !is_array($contentprops[$content_id]) ) $contentprops[$content_id] = array();
+						if( !isset($contentprops[$content_id]) ) $contentprops[$content_id] = array();
 						$contentprops[$content_id] = $tmp[$i];
 					}
 				}
