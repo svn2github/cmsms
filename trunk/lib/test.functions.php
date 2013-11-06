2ea66b5a91ca77c313ecbf4bab056f225767f04c
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2013-11-06 22:20:06 +0000

    Bump minimum and recommended versions of PHP for CMSMS 2.0.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@9048 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/lib/test.functions.php
+++ trunk/lib/test.functions.php
@@ -70,7 +70,7 @@ function getSupportedDBDriver()
 function getTestValues( $property )
 {
 	$range = array(
-		'php_version'			=> array('minimum'=>'5.3.2', 'recommended'=>'5.4'),
+		'php_version'			=> array('minimum'=>'5.4.9', 'recommended'=>'5.5.5'),
 		'gd_version'			=> array('minimum'=>2),
 		'memory_limit'			=> array('minimum'=>'16M', 'recommended'=>'24M'),
 		'max_execution_time'	=> array('minimum'=>30, 'recommended'=>60),
