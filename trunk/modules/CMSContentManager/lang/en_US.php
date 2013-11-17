bc323708a4055502d25fdcc3a178e70dcaf193c6
rolf1 <rolf1@3d254a34-79dc-0310-9e5f-be208747d8a0>
2013-11-17 18:55:28 +0000

    Just trying to rename admin settings
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@9089 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/modules/CMSContentManager/lang/en_US.php
+++ trunk/modules/CMSContentManager/lang/en_US.php
@@ -234,7 +234,7 @@ $lang['select_all'] = 'Select All';
 $lang['submit'] = 'Submit';
 
 #T
-$lang['title_contentmanager_settings'] = 'Content Manager Settings';
+$lang['title_contentmanager_settings'] = 'Settings - Content Manager';
 $lang['title_editpage_apply'] = 'Save the changes to this content page, and continue editing';
 $lang['title_editpage_cancel'] = 'Abandon all unsaved changes to this page, and return to the content list';
 $lang['title_editpage_submit'] = 'Save the changes to this content page, and return to the content list';
