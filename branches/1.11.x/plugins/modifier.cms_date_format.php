28dbe3bec6b1a9cec7a4566f27fd4ffea9056077
stikki <stikki@3d254a34-79dc-0310-9e5f-be208747d8a0>
2014-01-22 01:13:28 +0000

    Small cleanup to cms_date_format plugin
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@9207 3d254a34-79dc-0310-9e5f-be208747d8a0

--- branches/1.11.x/plugins/modifier.cms_date_format.php
+++ branches/1.11.x/plugins/modifier.cms_date_format.php
@@ -1,36 +1,61 @@
 <?php
+/**
+ * Smarty plugin
+ *
+ * @package Smarty
+ * @subpackage PluginsModifier
+ */
 
-function smarty_cms_modifier_cms_date_format($string, $format = '',
-					 $default_date = '')
+/**
+ * Smarty date_format modifier plugin
+ *
+ * Type:     modifier<br>
+ * Name:     cms_date_format<br>
+ * Purpose:  format datestamps via strftime<br>
+ * Input:<br>
+ *          - string: input date string
+ *          - format: strftime format for output
+ *          - default_date: default date if $string is empty
+ *
+ * @link http://www.smarty.net/manual/en/language.modifier.date.format.php date_format (Smarty online manual)
+ * @author Monte Ohrt <monte at ohrt dot com>
+ * @param string $string       input date string
+ * @param string $format       strftime format for output
+ * @param string $default_date default date if $string is empty
+ * @param string $formatter    either 'strftime' or 'auto'
+ * @return string |void
+ * @uses smarty_make_timestamp()
+ *
+ * Modified by Tapio Löytty <stikki@cmsmadesimple.org>
+ */
+function smarty_cms_modifier_cms_date_format($string, $format = '', $default_date = '')
 {
-  $gCms = cmsms();
-  if( $format == '' )
-    {
-      $format = get_site_preference('defaultdateformat');
-      if( $format == '' )
-	{
-	  $format = '%b %e, %Y';
-	}
-      if( !isset($gCms->variables['page_id']) )
-        {
-	  $uid = get_userid(false);
-	  if( $uid )
-	    {
-	      $tmp = get_preference($uid,'date_format_string');
-	      if( $tmp != '' )
-		{
-		  $format = $tmp;
+	if($format == '') {
+	
+		$format = get_site_preference('defaultdateformat');
+		if($format == '')		
+			$format = '%b %e, %Y';
+		
+		if(!isset(cmsms()->variables['page_id'])) {
+		
+			if($uid = get_userid(false)) {
+			
+				$tmp = get_preference($uid, 'date_format_string');
+				if($tmp != '')
+					$format = $tmp;
+			}
 		}
-	    }
 	}
-    }
 
-  $config = $gCms->GetConfig();
-  $fn = cms_join_path($config['root_path'],'lib','smarty','plugins','modifier.date_format.php');
-  if( !file_exists($fn) ) die();
-  require_once( $fn );
+	$config = cmsms()->GetConfig();
+	
+	$fn = cms_join_path(SMARTY_PLUGINS_DIR, 'modifier.date_format.php');
+	if(!file_exists($fn)) 
+		die();
+		
+	require_once( $fn );
 
-  return smarty_modifier_date_format($string,$format,$default_date);
+	return smarty_modifier_date_format($string,$format,$default_date);
 }
 // EOF
 ?>
