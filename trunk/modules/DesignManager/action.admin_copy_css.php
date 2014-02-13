f4a262262275ee194c8198446f073ad451dbc6a1
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2014-02-13 16:55:30 +0000

    minor.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@9323 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/modules/DesignManager/action.admin_copy_css.php
+++ trunk/modules/DesignManager/action.admin_copy_css.php
@@ -42,7 +42,7 @@ try {
 
       if( isset($params['submitandedit']) ) {
 				$this->SetMessage($this->Lang('msg_stylesheet_copied_edit'));
-				$this->Redirect($id,'admin_edit_template',$returnid,array('tpl'=>$new_tpl->get_id()));
+				$this->Redirect($id,'admin_edit_css',$returnid,array('tpl'=>$new_css->get_id()));
 			}
 			else {
 				$this->SetMessage($this->Lang('msg_stylesheet_copied'));
