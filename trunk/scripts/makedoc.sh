9ccc511c72b61124ffe631b0658ae175de4dd709
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2014-03-11 20:22:39 +0000

    Adds goran's cmsms customized phpdoc template, and a script to generate APIDOCS.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@9364 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ trunk/scripts/makedoc.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# NOTES
+#  - this script requires phpdoc (phpDocumentor)
+#     recommend install via pear (see the phpDocumentor website: http://www.phpdoc.org/docs/latest/getting-started/installing.html)
+#  - this script assumes the cmsms phpDocumentor template (may need te copied into the phpdoc shared templates location
+#     i.e: /usr/share/php/phpDocumentor/data/templates
+#     this template is stored in svn upder scripts/phpDocumentor Template
+#  - the phpDocumentor template relies on php's xslt extensions (in ubuntu based systems: sudo-apt-get install php5-xslt)
+#  - this script must be executed from the CMSMS root (i.e: ./scripts/makedoc.sh)
+#  - API documentation is output in the APIDOC subdirectory.
+
+_config='scripts/makedoc.xml';
+_exe=`which phpdoc`
+if [ -z $_exe ]; then
+  echo "FATAL: phpdoc must be installed";
+  exit 1
+fi
+if [ ! -r config.php -o ! -r version.php ]; then
+  echo "FATAL: This script must be executed from the CMSMS root path";
+  exit 1
+fi
+
+if [ ! -r ${_config} ]; then
+  echo "FATAL: phpdoc configuration file not found";
+  exit 1
+fi
+
+trap cleanup INT
+
+cleanup() {
+  rm phpdoc.dist.xml
+}
+
+# temporarily copy the makedoc.xml file to the root directory
+cp ${_config} phpdoc.dist.xml
+
+# execute phpdoc
+phpdoc | tee tmp/phpdoc.log
+
+# cleanup
+cleanup;
