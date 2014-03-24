92994a07cbde9193d97e58cc92b55eb4aae13480
stikki <stikki@3d254a34-79dc-0310-9e5f-be208747d8a0>
2014-03-24 21:26:05 +0000

    Upgrading cms_date_format modifier from 1.11.x series.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@9398 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/plugins/modifier.cms_date_format.php
+++ trunk/plugins/modifier.cms_date_format.php
@@ -1,41 +1,59 @@
 <?php
-#CMS - CMS Made Simple
-#(c)2004 by Ted Kulp (wishy@users.sf.net)
-#Visit our homepage at: http://www.cmsmadesimple.org
-#
-#This program is free software; you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation; either version 2 of the License, or
-#(at your option) any later version.
-#
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+/**
+ * Smarty plugin
+ *
+ * @package Smarty
+ * @subpackage PluginsModifier
+ */
 
+/**
+ * Smarty date_format modifier plugin
+ *
+ * Type:     modifier<br>
+ * Name:     cms_date_format<br>
+ * Purpose:  format datestamps via strftime<br>
+ * Input:<br>
+ *          - string: input date string
+ *          - format: strftime format for output
+ *          - default_date: default date if $string is empty
+ *
+ * @link http://www.smarty.net/manual/en/language.modifier.date.format.php date_format (Smarty online manual)
+ * @author Monte Ohrt <monte at ohrt dot com>
+ * @param string $string       input date string
+ * @param string $format       strftime format for output
+ * @param string $default_date default date if $string is empty
+ * @param string $formatter    either 'strftime' or 'auto'
+ * @return string |void
+ * @uses smarty_make_timestamp()
+ *
+ * Modified by Tapio Löytty <stikki@cmsmadesimple.org>
+ */
 function smarty_cms_modifier_cms_date_format($string, $format = '', $default_date = '')
 {
-  if( $format == '' ) {
-    $format = get_site_preference('defaultdateformat');
-    if( $format == '' ) $format = '%b %e, %Y';
+	if($format == '') {
+	
+		$format = get_site_preference('defaultdateformat');
+		if($format == '')		
+			$format = '%b %e, %Y';
+		
+		if(!cmsms()->is_frontend_request()) {
+		
+			if($uid = get_userid(false)) {
+			
+				$tmp = get_preference($uid, 'date_format_string');
+				if($tmp != '')
+					$format = $tmp;
+			}
+		}
+	}
+	
+	$fn = cms_join_path(SMARTY_PLUGINS_DIR, 'modifier.date_format.php');
+	if(!file_exists($fn)) 
+		die();
+		
+	require_once( $fn );
 
-    if( !cmsms()->get_content_id() ) {
-      $uid = get_userid(false);
-      if( $uid ) {
-	$tmp = get_preference($uid,'date_format_string');
-	if( $tmp != '' ) $format = $tmp;
-      }
-    }
-  }
-
-  $config = cmsms()->GetConfig();
-  $fn = cms_join_path($config['root_path'],'lib','smarty','plugins','modifier.date_format.php');
-
-  if( !file_exists($fn) ) die();
-  require_once( $fn );
-  return smarty_modifier_date_format($string,$format,$default_date);
+	return smarty_modifier_date_format($string,$format,$default_date);
 }
+// EOF
 ?>
