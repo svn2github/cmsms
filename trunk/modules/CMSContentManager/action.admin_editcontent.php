a3fd2c1201a09bd785d2acc754241944f47099bc
rolf1 <rolf1@3d254a34-79dc-0310-9e5f-be208747d8a0>
2014-04-04 13:16:16 +0000

    Remove not working title
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@9438 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/modules/CMSContentManager/action.admin_editcontent.php
+++ trunk/modules/CMSContentManager/action.admin_editcontent.php
@@ -244,7 +244,7 @@ foreach( $tab_names as $currenttab => $label ) {
                 // this is ugly... we should know if the type is a system type.
                 continue;
             }
-            $tmp2 .= CmsFormUtils::create_option(array('value'=>$type,'label'=>$label,'title'=>lang($label.'todo')),$content_type);  // todo: add title here.
+            $tmp2 .= CmsFormUtils::create_option(array('value'=>$type,'label'=>$label),$content_type);
         }
         $tmp2 .= '</select>';
         $tmp[] = $tmp2;
