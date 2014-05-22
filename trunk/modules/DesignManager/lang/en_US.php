7a3e2e8fd2a902e15a39a9757b8a9a7345e9ae15
rolf1 <rolf1@3d254a34-79dc-0310-9e5f-be208747d8a0>
2014-05-22 16:21:51 +0000

    missing langstring
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple@9558 3d254a34-79dc-0310-9e5f-be208747d8a0

--- trunk/modules/DesignManager/lang/en_US.php
+++ trunk/modules/DesignManager/lang/en_US.php
@@ -468,6 +468,7 @@ $lang['warn_template_used'] = '<strong>Warning:</strong> This template is in use
 $lang['warning_category_dragdrop'] = 'To change the display order of categories you can drag the rows into their correct order';
 $lang['warning_deletetemplate_attachments'] = 'This design has templates attached to it.  These templates may be in use on some page or by a module. Deleting these templates, even if they are attached to no other design, may break an otherwise working site.   Please exercise extreme caution!';
 $lang['warning_deletestylesheet_attachments'] = 'This design has stylesheets attached to it.  Some of the templates in use by the system but not necessarily attached to this design may rely on these stylesheets.  Deleting these stylesheets, even if they are attached to no other design may break an otherwise working site. Please exercise extreme caution!';
+$lang['warning_edittemplate_nostylesheets'] = 'No stylesheets attached';
 $lang['warning_no_templates_available'] = 'We could find no templates that match the selected criteria or that you have permission to edit';
 $lang['warning_no_stylesheets'] = 'We could find no stylesheets that match the selected criteria or that you have permission to edit';
 $lang['warning_set_all_pages'] = '<strong>Warning:</strong> This is a potentially destructive operation!  You are asking that all content pages be set use this template.  Please confirm this action.';
